229dc6780424f9fb459732500c418f3ed35e1dc4
Kevin Gilpin <kgilpin@gmail.com>
2016-07-22 16:30:48 -0400

    fix rewrite patterns

--- config/initializers/rewrite.rb
+++ config/initializers/rewrite.rb
@@ -1,9 +1,8 @@
 Possum::Application.config.middleware.insert 0, Rack::Rewrite do
-  rewrite %r{^/authn/users/(.*)}, '/authn/$1'
-  rewrite %r{^/authz/([^\/]+)/resources(\?.*)?}, '/resources/$1$2'
-  rewrite %r{^/authz/([^\/]+)/roles/allowed_to/([^\/]+)/([^?]*)(\?.*)?}, 'resources/$1/$3?permitted_roles&privilege=$2&$4'
-  rewrite %r{^/authz/([^\/]+)/resources/(.*)}, '/resources/$1/$2'
-  rewrite %r{^/authz/([^\/]+)/roles/(.*)},     '/roles/$1/$2'
+  rewrite %r{^/authn/users/(.*)(\?.*)?}, '/authn/$1$2'
+  rewrite %r{^/authz/([^/]+)/roles/allowed_to/([^/]+)/([^?]*)\??(.*)?}, '/resources/$1/$3?permitted_roles&privilege=$2&$4'
+  rewrite %r{^/authz/([^/]+)/resources(\/.*)?(\?.*)?}, '/resources/$1/$2$3'
+  rewrite %r{^/authz/([^/]+)/roles/(.*)(\?.*)?},     '/roles/$1/$2$3'
 end
 
 Possum::Application.config.middleware.insert 0, Rack::Rewrite do
