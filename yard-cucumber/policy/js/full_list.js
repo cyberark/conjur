5c7ef5fe104b34b03ae5114352afad41e6410b9d
Kevin Gilpin <kgilpin@gmail.com>
2016-07-26 17:10:33 -0400

    updated YARD docs

--- yard-cucumber/policy/js/full_list.js
+++ yard-cucumber/policy/js/full_list.js
@@ -1,8 +1,8 @@
-(function() {
-
-var $clicked = $(null);
-var searchTimeout = null;
+var inSearch = null;
+var searchIndex = 0;
 var searchCache = [];
+var searchString = '';
+var regexSearchString = '';
 var caseSensitiveMatch = false;
 var ignoreKeyCodeMin = 8;
 var ignoreKeyCodeMax = 46;
@@ -12,205 +12,162 @@ RegExp.escape = function(text) {
     return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
 }
 
-function escapeShortcut() {
-  $(document).keydown(function(evt) {
-    if (evt.which == 27) {
-      window.parent.postMessage('navEscape', '*');
-    }
+function fullListSearch() {
+  // generate cache
+  searchCache = [];
+  $('#full_list li').each(function() {
+    var link = $(this).find('.object_link a');
+    var fullName = link.attr('title').split(' ')[0];
+    searchCache.push({name:link.text(), fullName:fullName, node:$(this), link:link});
   });
-}
 
-function navResizer() {
-  $(window).mousemove(function(e) {
-    window.parent.postMessage({
-      action: 'mousemove', event: {pageX: e.pageX, which: e.which}
-    }, '*');
-  }).mouseup(function(e) {
-    window.parent.postMessage({action: 'mouseup'}, '*');
-  });
-  window.parent.postMessage("navReady", "*");
-}
-
-function clearSearchTimeout() {
-  clearTimeout(searchTimeout);
-  searchTimeout = null;
-}
-
-function enableLinks() {
-  // load the target page in the parent window
-  $('#full_list li').on('click', function(evt) {
-    $('#full_list li').removeClass('clicked');
-    $clicked = $(this);
-    $clicked.addClass('clicked');
-    evt.stopPropagation();
-
-    if (evt.target.tagName === 'A') return true;
-
-    var elem = $clicked.find('> .item .object_link a')[0];
-    var e = evt.originalEvent;
-    var newEvent = new MouseEvent(evt.originalEvent.type);
-    newEvent.initMouseEvent(e.type, e.canBubble, e.cancelable, e.view, e.detail, e.screenX, e.screenY, e.clientX, e.clientY, e.ctrlKey, e.altKey, e.shiftKey, e.metaKey, e.button, e.relatedTarget);
-    elem.dispatchEvent(newEvent);
-    evt.preventDefault();
-    return false;
-  });
-}
-
-function enableToggles() {
-  // show/hide nested classes on toggle click
-  $('#full_list a.toggle').on('click', function(evt) {
-    evt.stopPropagation();
-    evt.preventDefault();
-    $(this).parent().parent().toggleClass('collapsed');
-    highlight();
-  });
-}
-
-function populateSearchCache() {
-  $('#full_list li .item').each(function() {
-    var $node = $(this);
-    var $link = $node.find('.object_link a');
-    if ($link.length > 0) {
-      searchCache.push({
-        node: $node,
-        link: $link,
-        name: $link.text(),
-        fullName: $link.attr('title').split(' ')[0]
-      });
-    }
-  });
-}
-
-function enableSearch() {
   $('#search input').keyup(function(event) {
-    if (ignoredKeyPress(event)) return;
-    if (this.value === "") {
-      clearSearch();
-    } else {
-      performSearch(this.value);
+    if ((event.keyCode > ignoreKeyCodeMin && event.keyCode < ignoreKeyCodeMax)
+         || event.keyCode == commandKey)
+      return;
+    searchString = this.value;
+    caseSensitiveMatch = searchString.match(/[A-Z]/) != null;
+    regexSearchString = RegExp.escape(searchString);
+    if (caseSensitiveMatch) {
+      regexSearchString += "|" +
+        $.map(searchString.split(''), function(e) { return RegExp.escape(e); }).
+        join('.+?');
+    }
+    if (searchString === "") {
+      clearTimeout(inSearch);
+      inSearch = null;
+      $('ul .search_uncollapsed').removeClass('search_uncollapsed');
+      $('#full_list, #content').removeClass('insearch');
+      $('#full_list li').removeClass('found').each(function() {
+
+        var link = $(this).find('.object_link a');
+        link.text(link.text());
+      });
+      if (clicked) {
+        clicked.parents('ul').each(function() {
+          $(this).removeClass('collapsed').prev().removeClass('collapsed');
+        });
+      }
+      highlight();
+    }
+    else {
+      if (inSearch) clearTimeout(inSearch);
+      searchIndex = 0;
+      lastRowClass = '';
+      $('#full_list, #content').addClass('insearch');
+      $('#noresults').text('');
+      searchItem();
     }
   });
 
-  $('#full_list').after("<div id='noresults' style='display:none'></div>");
+  $('#search input').focus();
+  $('#full_list').after("<div id='noresults'></div>");
 }
 
-function ignoredKeyPress(event) {
-  if (
-    (event.keyCode > ignoreKeyCodeMin && event.keyCode < ignoreKeyCodeMax) ||
-    (event.keyCode == commandKey)
-  ) {
-    return true;
-  } else {
-    return false;
-  }
-}
-
-function clearSearch() {
-  clearSearchTimeout();
-  $('#full_list .found').removeClass('found').each(function() {
-    var $link = $(this).find('.object_link a');
-    $link.text($link.text());
-  });
-  $('#full_list, #content').removeClass('insearch');
-  $clicked.parents().removeClass('collapsed');
-  highlight();
-}
-
-function performSearch(searchString) {
-  clearSearchTimeout();
-  $('#full_list, #content').addClass('insearch');
-  $('#noresults').text('').hide();
-  partialSearch(searchString, 0);
-}
-
-function partialSearch(searchString, offset) {
-  var lastRowClass = '';
-  var i = null;
-  for (i = offset; i < Math.min(offset + 50, searchCache.length); i++) {
-    var item = searchCache[i];
+var lastRowClass = '';
+function searchItem() {
+  for (var i = 0; i < searchCache.length / 50; i++) {
+    var item = searchCache[searchIndex];
     var searchName = (searchString.indexOf('::') != -1 ? item.fullName : item.name);
-    var matchString = buildMatchString(searchString);
+    var matchString = regexSearchString;
     var matchRegexp = new RegExp(matchString, caseSensitiveMatch ? "" : "i");
     if (searchName.match(matchRegexp) == null) {
       item.node.removeClass('found');
-      item.link.text(item.link.text());
     }
     else {
-      item.node.addClass('found');
+      item.node.css('padding-left', '10px').addClass('found');
+      item.node.parents().addClass('search_uncollapsed');
       item.node.removeClass(lastRowClass).addClass(lastRowClass == 'r1' ? 'r2' : 'r1');
       lastRowClass = item.node.hasClass('r1') ? 'r1' : 'r2';
       item.link.html(item.name.replace(matchRegexp, "<strong>$&</strong>"));
     }
+
+    if (searchCache.length === searchIndex + 1) {
+      searchDone();
+      return;
+    }
+    else {
+      searchIndex++;
+    }
   }
-  if(i == searchCache.length) {
-    searchDone();
-  } else {
-    searchTimeout = setTimeout(function() {
-      partialSearch(searchString, i);
-    }, 0);
-  }
+  inSearch = setTimeout('searchItem()', 0);
 }
 
 function searchDone() {
-  searchTimeout = null;
-  highlight();
+  highlight(true);
   if ($('#full_list li:visible').size() === 0) {
     $('#noresults').text('No results were found.').hide().fadeIn();
-  } else {
-    $('#noresults').text('').hide();
+  }
+  else {
+    $('#noresults').text('');
   }
   $('#content').removeClass('insearch');
+  clearTimeout(inSearch);
+  inSearch = null;
 }
 
-function buildMatchString(searchString, event) {
-  caseSensitiveMatch = searchString.match(/[A-Z]/) != null;
-  var regexSearchString = RegExp.escape(searchString);
-  if (caseSensitiveMatch) {
-    regexSearchString += "|" +
-      $.map(searchString.split(''), function(e) { return RegExp.escape(e); }).
-      join('.+?');
-  }
-  return regexSearchString;
-}
-
-function highlight() {
-  $('#full_list li:visible').each(function(n) {
-    $(this).removeClass('even odd').addClass(n % 2 == 0 ? 'odd' : 'even');
+clicked = null;
+function linkList() {
+  $('#full_list li, #full_list li a:last').click(function(evt) {
+    if ($(this).hasClass('toggle')) return true;
+    if (this.tagName.toLowerCase() == "li") {
+      var toggle = $(this).children('a.toggle');
+      if (toggle.size() > 0 && evt.pageX < toggle.offset().left) {
+        toggle.click();
+        return false;
+      }
+    }
+    if (clicked) clicked.removeClass('clicked');
+    var win = window.top.frames.main ? window.top.frames.main : window.parent;
+    if (this.tagName.toLowerCase() == "a") {
+      clicked = $(this).parent('li').addClass('clicked');
+      win.location = this.href;
+    }
+    else {
+      clicked = $(this).addClass('clicked');
+      win.location = $(this).find('a:last').attr('href');
+    }
+    return false;
   });
 }
 
-/**
- * Expands the tree to the target element and its immediate
- * children.
- */
-function expandTo(path) {
-  var $target = $(document.getElementById('object_' + path));
-  $target.addClass('clicked');
-  $target.removeClass('collapsed');
-  $target.parentsUntil('#full_list', 'li').removeClass('collapsed');
-  if($target[0]) {
-    window.scrollTo(window.scrollX, $target.offset().top - 250);
+function collapse() {
+  if (!$('#full_list').hasClass('class')) return;
+  $('#full_list.class a.toggle').click(function() {
+    $(this).parent().toggleClass('collapsed').next().toggleClass('collapsed');
     highlight();
-  }
+    return false;
+  });
+  $('#full_list.class ul').each(function() {
+    $(this).addClass('collapsed').prev().addClass('collapsed');
+  });
+  $('#full_list.class').children().removeClass('collapsed');
+  highlight();
 }
 
-function windowEvents(event) {
-  var msg = event.data;
-  if (msg.action === "expand") {
-    expandTo(msg.path);
-  }
-  return false;
+function highlight(no_padding) {
+  var n = 1;
+  $('#full_list li:visible').each(function() {
+    var next = n == 1 ? 2 : 1;
+    $(this).removeClass("r" + next).addClass("r" + n);
+    if (!no_padding && $('#full_list').hasClass('class')) {
+      $(this).css('padding-left', (10 + $(this).parents('ul').size() * 15) + 'px');
+    }
+    n = next;
+  });
 }
 
-window.addEventListener("message", windowEvents, false);
+function escapeShortcut() {
+  $(document).keydown(function(evt) {
+    if (evt.which == 27) {
+      $('#search_frame', window.top.document).slideUp(100);
+      $('#search a', window.top.document).removeClass('active inactive');
+      $(window.top).focus();
+    }
+  });
+}
 
-$(document).ready(function() {
-  escapeShortcut();
-  navResizer();
-  enableLinks();
-  enableToggles();
-  populateSearchCache();
-  enableSearch();
-});
-
-})();
+$(escapeShortcut);
+$(fullListSearch);
+$(linkList);
+$(collapse);
