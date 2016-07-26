1f8b2c698f408558b20b0ac9578229cd94e78a78
Kevin Gilpin <kgilpin@gmail.com>
2016-07-26 16:19:11 -0400

    CON-3212 remove unused import

--- app/controllers/public_keys_controller.rb
+++ app/controllers/public_keys_controller.rb
@@ -1,7 +1,5 @@
 class PublicKeysController < ApplicationController
   def show
-    require 'slosilo'
-    
     account, kind, id = [ params[:account], params[:kind], params[:identifier] ]
 
     values = Secret.latest_public_keys account, kind, id
