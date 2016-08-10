3ff3ce7656d729669cde5f27943ffce3749d5516
Kevin Gilpin <kgilpin@gmail.com>
2016-08-10 12:41:37 -0400

    Revert "drop unused sequence"
    This reverts commit a349eacbc398362cf84750a812f16f727c0b80e1.

--- db/migrate/20160630172059_create_secrets.rb
+++ db/migrate/20160630172059_create_secrets.rb
@@ -9,9 +9,34 @@ Sequel.migration do
       
       primary_key [ :resource_id, :counter ]
     end
+
+    execute <<-COUNTER_TRIGGER
+    CREATE OR REPLACE FUNCTION secrets_next_counter() RETURNS TRIGGER
+      LANGUAGE plpgsql STABLE STRICT
+    AS $$
+    DECLARE
+      next_counter integer;
+    BEGIN
+      SELECT coalesce(max(counter), 0) + 1 INTO next_counter
+        FROM secrets 
+        WHERE resource_id = NEW.resource_id;
+
+      NEW.counter = next_counter;
+      RETURN NEW;
+    END
+    $$;
+
+    CREATE TRIGGER secrets_counter
+    BEFORE INSERT
+    ON secrets
+    FOR EACH ROW
+    EXECUTE PROCEDURE secrets_next_counter();
+    COUNTER_TRIGGER
   end
   
   down do
+    execute "DROP TRIGGER IF EXISTS secrets_counter ON secrets"
+    execute "DROP FUNCTION IF EXISTS secrets_next_counter()"
     drop_table :secrets
   end
 end
