512d6cf497447b06f9144a535282bbaa9acdff7f
Kevin Gilpin <kgilpin@gmail.com>
2016-08-11 17:27:12 -0400

    publish to 'latest' unless otherwise instructed

--- publish.sh
+++ publish.sh
@@ -1,9 +1,32 @@
 #!/bin/bash -ex
 
+VERSION=latest
+
+usage() {
+    cat <<EOF
+usage: $(basename $0) [-h] [-v <version>]
+    -h              Show help
+    -v <version>    Version to publish [default: latest]
+EOF
+}
+
+while getopts "hv:" opt; do
+    case "$opt" in
+        h) usage
+           exit 0
+           ;;
+
+        v) VERSION=$OPTARG
+           ;;
+
+        \?) usage >&2
+            exit 1
+            ;;
+    esac
+done
+
 ./build.sh
 
-docker tag -f possum registry.tld/conjurinc/possum:latest
-docker tag -f possum registry.tld/conjurinc/possum:$(cat VERSION)
+docker tag -f possum registry.tld/conjurinc/possum:${VERSION}
 
-docker push registry.tld/conjurinc/possum:latest
-docker push registry.tld/conjurinc/possum:$(cat VERSION)
+docker push registry.tld/conjurinc/possum:${VERSION}
