b44f21d624d7dde4d9249e17de77a29edf476394
Kevin Gilpin <kgilpin@gmail.com>
2017-05-03 14:10:10 -0400

    remove v4 compatibility

--- config/initializers/v4_compatibility.rb
+++
@@ -1,7 +0,0 @@
-if default_account = ENV['CONJUR_ACCOUNT']
-  $stderr.puts "Activating v4 route compatibility with account '#{default_account}'"
-  Rails.application.config.middleware.insert_before(Rack::Runtime, Rack::Rewrite) do
-    rewrite %r{/authn/users/([^/]*)/authenticate}, "/authn/#{default_account}/$1/authenticate", method: :post
-    rewrite %r{/variables/([^/]*)/value}, "/secrets/#{default_account}/variable/$1", method: :get
-  end
-end
