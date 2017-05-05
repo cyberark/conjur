d5e66bab05e2c2ada5c9390b03ad51b9bf78880c
Kevin Gilpin <kgilpin@gmail.com>
2017-05-05 10:58:45 -0400

    add Procfile and config/puma.rb

---
+++ config/puma.rb
@@ -0,0 +1,15 @@
+workers Integer(ENV['WEB_CONCURRENCY'] || 2)
+threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
+threads threads_count, threads_count
+
+preload_app!
+
+rackup      DefaultRackup
+port        ENV['PORT']     || 3000
+environment ENV['RACK_ENV'] || 'development'
+
+on_worker_boot do
+  # https://groups.google.com/forum/#!topic/sequel-talk/LBAtdstVhWQ
+  Sequel::Model.db.disconnect
+end
+
