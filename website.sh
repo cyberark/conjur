0a4dc06689c8d254e1dcf08459585a95789049d2
Alan Potter <alanjpotter@gmail.com>
2017-06-14 18:34:29 -0400

    get rid of prerelease directory, just call it docs

--- website.sh
+++ website.sh
@@ -9,7 +9,7 @@ docker run -i -v $PWD:/opt/possum --rm  \
   -e POSSUM_WEB_USER -e POSSUM_WEB_PASSWORD \
   possum-web bash -ec '
 mkdir /output
-jekyll build --source prerelease/docs --destination /output/_site
+jekyll build --source docs --destination /output/_site
 
 aws s3 ls s3://${POSSUM_WEB_CFG_BUCKET} >/dev/null || aws s3 mb s3://${POSSUM_WEB_CFG_BUCKET}
 echo "${POSSUM_WEB_USER}:$(openssl passwd -apr1 ${POSSUM_WEB_PASSWORD})" | aws s3 cp - s3://${POSSUM_WEB_CFG_BUCKET}/htpasswd
