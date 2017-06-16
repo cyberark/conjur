8173d8587ff9de9b6976801f8a92d70143fcfb2a
Alan Potter <alanjpotter@gmail.com>
2017-06-16 10:46:26 -0400

    take out S3 bucket creation

--- website.sh
+++ website.sh
@@ -11,9 +11,7 @@ docker run -i -v $PWD:/opt/conjur --rm  \
 mkdir /output
 jekyll build --source docs --destination /output/_site
 
-aws s3 ls s3://${POSSUM_WEB_CFG_BUCKET} >/dev/null || aws s3 mb s3://${POSSUM_WEB_CFG_BUCKET}
 echo "${POSSUM_WEB_USER}:$(openssl passwd -apr1 ${POSSUM_WEB_PASSWORD})" | aws s3 cp - s3://${POSSUM_WEB_CFG_BUCKET}/htpasswd
 
-aws s3 ls s3://${POSSUM_WEB_BUCKET} >/dev/null || aws s3 mb s3://${POSSUM_WEB_BUCKET}
 aws s3 sync /output/_site s3://${POSSUM_WEB_BUCKET}
 '
