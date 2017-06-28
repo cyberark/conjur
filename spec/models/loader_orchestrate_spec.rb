cd215b1a91ef2ee7e284f0a0373b329f3ddd0c15
John Tuttle <jtuttle@conjur.net>
2017-06-28 16:03:45 -0400

    spacing fix

--- spec/models/loader_orchestrate_spec.rb
+++ spec/models/loader_orchestrate_spec.rb
@@ -35,6 +35,7 @@ describe Loader::Orchestrate do
     if ENV['DUMP_DATA']
       File.write expectation_path(path), print_public
     end
+    binding.pry
     expect(print_public).to eq(File.read(expectation_path(path)))
   end
 
