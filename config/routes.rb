bebf6a963ccea0324e2679987572964ef4b5d635
Alan Potter <alanjpotter@gmail.com>
2017-07-11 10:54:16 -0400

    don't need the account constraint on the accounts resources

--- config/routes.rb
+++ config/routes.rb
@@ -10,9 +10,9 @@ end
 
 Rails.application.routes.draw do
   scope format: false do
-    constraints account: /[^\/\?]+/ do
-      resources :accounts, only: [ :create, :index, :destroy ]
+    resources :accounts, only: [ :create, :index, :destroy ]
 
+    constraints account: /[^\/\?]+/ do
       get  '/authn/:account/login' => 'credentials#login'
       put  '/authn/:account/password' => 'credentials#update_password'
       put  '/authn/:account/api_key'  => 'credentials#rotate_api_key'
