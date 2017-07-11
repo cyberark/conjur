94accda20f2e6d78ccb886122a70a710b3e1f838
Alan Potter <alanjpotter@gmail.com>
2017-07-11 10:43:41 -0400

    only apply account constraint to routes with an account

--- config/routes.rb
+++ config/routes.rb
@@ -46,12 +46,13 @@ Rails.application.routes.draw do
       post "/policies/:account/:kind/*identifier" => 'policies#post'
 
       get "/public_keys/:account/:kind/*identifier" => 'public_keys#show'
-      
-      post "/host_factories/hosts" => 'host_factories#create_host'
-      
-      post "/host_factory_tokens" => 'host_factory_tokens#create'
-      
-      delete "/host_factory_tokens/:id" => 'host_factory_tokens#destroy'
     end
+      
+    post "/host_factories/hosts" => 'host_factories#create_host'
+    
+    post "/host_factory_tokens" => 'host_factory_tokens#create'
+    
+    delete "/host_factory_tokens/:id" => 'host_factory_tokens#destroy'
   end
+
 end
