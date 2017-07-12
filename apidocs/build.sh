619d1aaf82b21600a37eb8e4fa8eb8d87e68ff82
Ryan Prior <ryanprior@gmail.com>
2017-07-12 14:04:05 -0500

    Streamlines building and tagging docker image

--- apidocs/build.sh
+++ apidocs/build.sh
@@ -1,5 +1,3 @@
 #!/bin/bash -ex
 
-docker build -t possum-apidocs -f apidocs/Dockerfile.apidocs apidocs
-
-docker tag possum-apidocs conjurinc/possum-apidocs || docker tag -f possum-apidocs conjurinc/possum-apidocs
+docker build -t conjurinc/possum-apidocs -f apidocs/Dockerfile.apidocs apidocs
