4c35b3f6fb6a9e2143e4b93d946eeb8f5e42ebbe
Jason Vanderhoof <jvanderhoof@gmail.com>
2017-07-18 16:02:08 -0600

    Update readme & name running possum container

--- docs/dev.sh
+++ docs/dev.sh
@@ -2,4 +2,11 @@
 
 (cd ..; docker build -t possum-web -f docs/Dockerfile .)
 
-docker run --rm -it -v $PWD/..:/opt/conjur -p 4000:4000 possum-web ${@-jekyll serve -H 0.0.0.0 --source docs}
+# /node_modules/.bin/aglio -i apidocs/src/api.md -o docs/apidocs/doc.html
+docker run \
+  --rm \
+  -it \
+  -v $PWD/..:/opt/conjur \
+  -p 4000:4000 \
+  --name possum-web \
+  possum-web ${@-jekyll serve -H 0.0.0.0 --source docs}
