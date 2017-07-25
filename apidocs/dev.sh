3c147a1f173c342ccb782c9a901b9d89180510dc
Jason Vanderhoof <jvanderhoof@gmail.com>
2017-07-25 22:09:29 +0300

    Adding dev.sh file to run api docs in apidocs (#133)

---
+++ apidocs/dev.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -ex
+
+source build.sh
+
+docker run \
+        -it \
+        --rm \
+        -v $PWD/:/apidocs \
+        -p 4000:4000 \
+        -w /apidocs \
+        --name possum-apidocs \
+        conjurinc/possum-apidocs \
+        /home/node/node_modules/.bin/aglio -i src/api.md -s -h 0.0.0.0 -p 4000
