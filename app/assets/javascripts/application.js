cbe8926eba21849f613f7ecd6c43a6b84ec99593
Tammy Shipps <mizziness@gmail.com>
2017-07-26 11:23:26 -0400

    Adding possum, removing jquery_ujs and turbolinks

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,4 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery_ujs
-//= require turbolinks
 //= require_tree .
