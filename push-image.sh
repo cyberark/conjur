924b1668984e9bd68013497e61a30ea9ecee5cd1
Dustin Collins <dustinrcollins@gmail.com>
2017-08-01 16:45:36 -0500

    Push possum image to internal, dockerhub and quay repos

--- push-image.sh
+++ push-image.sh
@@ -1,16 +1,30 @@
 #!/bin/bash -ex
 
-tag_with() {
-  echo -n registry.tld/possum:$(< VERSION)-$1
+function main() {
+  local version="$(< VERSION)-$(git rev-parse --short HEAD)"
+
+  push_to_registries $version
+
+  if [ "$BRANCH_NAME" = "master" ]; then
+    local stable_version="$(< VERSION)-stable"
+    push_to_registries $stable_version
+  fi
 }
 
-branch_tag=$(tag_with $(git rev-parse --short HEAD))
-docker tag possum $branch_tag
-docker push $branch_tag
+function push_to_registries() {
+  local version="$1"
 
-if [ "$BRANCH_NAME" = "master" ]; then
-  stable_tag=$(tag_with stable)
-  docker tag possum $stable_tag
-  docker push $stable_tag
-fi
-  
+  local internal_tag="registry.tld/possum:$version"
+  docker tag possum $internal_tag
+  docker push $internal_tag
+
+  local dockerhub_tag="conjurinc/possum:$version"
+  docker tag possum $dockerhub_tag
+  docker push $dockerhub_tag
+
+  local quay_tag="quay.io/conjur/possum:$version"
+  docker tag possum $quay_tag
+  docker push $quay_tag
+}
+
+main
