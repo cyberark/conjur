76bfe3163c7b6f41515098924a33c44ba76272d6
Dustin Collins <dustinrcollins@gmail.com>
2017-08-02 16:23:02 -0500

    Ignore '/api.html#authentication' in checklinks as well, for now

--- checklinks.sh
+++ checklinks.sh
@@ -3,8 +3,9 @@
 # Run html-proofer to check links on the docs site
 
 docker-compose build --pull docs
+
 docker run --rm possum-docs htmlproofer \
   --check-external-hash \
   --enforce-https \
-  --url-ignore '/public/favicon.ico,/apidocs.html' \
+  --url-ignore '/public/favicon.ico,/apidocs.html,/api.html#authentication' \
   ./_site
