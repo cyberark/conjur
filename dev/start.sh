b33a3b9edfe689992a226e5f7916b7d89d307fc6
John Tuttle <jtuttle@conjur.net>
2017-08-04 15:43:10 -0400

    rename all dev containers to possum, update volumes and links

--- dev/start.sh
+++ dev/start.sh
@@ -1,18 +1,18 @@
 #!/bin/bash -ex
 
-export COMPOSE_PROJECT_NAME=possumdev
+export COMPOSE_PROJECT_NAME=conjurdev
 
 docker-compose build
 
 if [ ! -f data_key ]; then
 	echo "Generating data key"
-	docker-compose run --no-deps --rm --entrypoint conjurctl possum data-key generate > data_key
+	docker-compose run --no-deps --rm --entrypoint conjurctl conjur data-key generate > data_key
 fi
 
 export POSSUM_DATA_KEY="$(cat data_key)"
 
 docker-compose up -d
-docker-compose exec possum conjurctl db migrate
-docker-compose exec possum conjurctl account create cucumber || true
-# docker-compose exec possum bash
-docker exec -it --detach-keys 'ctrl-\' $(docker-compose ps -q possum) bash
+docker-compose exec conjur conjurctl db migrate
+docker-compose exec conjur conjurctl account create cucumber || true
+# docker-compose exec conjur bash
+docker exec -it --detach-keys 'ctrl-\' $(docker-compose ps -q conjur) bash
