695e887a505d1e5fa8ab33970770cf313b12a415
John Tuttle <jtuttle@conjur.net>
2017-08-10 15:41:21 -0400

    Rename docker builds  217 (#221)
    * Issue #217 Started.
    
    * rename docker images, update test script
    
    * fix image push script
    
    * tag image with both possum and conjur
    
    * use server subdirectory
    
    * use product name for opt folder
    
    * delete unused dockerfile, change registry names

--- push-image.sh
+++ push-image.sh
@@ -14,16 +14,20 @@ function main() {
 function push_to_registries() {
   local version="$1"
 
-  local internal_tag="registry.tld/possum:$version"
-  docker tag possum $internal_tag
+  local internal_tag="registry.tld/conjur:$version"
+  docker tag conjur $internal_tag
   docker push $internal_tag
+  
+  local legacy_tag="registry.tld/possum:$version"
+  docker tag conjur $legacy_tag
+  docker push $legacy_tag
 
-  local dockerhub_tag="conjurinc/possum:$version"
-  docker tag possum $dockerhub_tag
+  local dockerhub_tag="conjurinc/conjur:$version"
+  docker tag conjur $dockerhub_tag
   docker push $dockerhub_tag
 
-  local quay_tag="quay.io/conjur/possum:$version"
-  docker tag possum $quay_tag
+  local quay_tag="quay.io/conjur/conjur:$version"
+  docker tag conjur $quay_tag
   docker push $quay_tag
 }
 
