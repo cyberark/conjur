357aa7e5d299bfa6d878f76c5bcc1182fab16fc0
John Tuttle <jtuttle@conjur.net>
2017-08-15 16:49:19 -0400

    store cookie on success, display creds when cookie data found

--- docs/javascript/conjur-account.js
+++ docs/javascript/conjur-account.js
@@ -2,28 +2,29 @@
 ---
 //
 
-var cookies = document.cookie.split('; ');
-var account = null;
+function displayAccountCredentials(email, account, api_key) {
+  $("#credentials-email").text(email);
+  $("#credentials-account").text(account);
+  $("#credentials-api-key").text(api_key);
 
-cookies.forEach(function(c) {
-  var name, value;
-  [name,value] = c.split('=');
-  if (name == 'email') {
-    account = value;
-  }
-});
-
-if (account) {
-  var spans = document.querySelectorAll('span');
-
-  spans.forEach(function(s) {
-    if (s.innerText == '"your-conjur-account-id"') {
-      s.innerText = '"' + account + '"';
-    }
+  $('.hosted-account-signup').fadeOut("normal", function(){
+    $('.hosted-account-signup').next(".hosted-confirmation").slideDown("normal");
   });
 }
 
 $(document).ready(function() {
+  document.cookie.split('; ').forEach(function(c) {
+    var name, value;
+    [name,value] = c.split('=');
+
+    if(name == "account") {
+      account_data = JSON.parse(value);
+      displayAccountCredentials(account_data.account,
+                                account_data.account,
+                                account_data.api_key);
+    }
+  });
+
   $('.hosted-account-signup').validator().on('submit', function(e) {
     if(!e.isDefaultPrevented()) {
       e.preventDefault();
@@ -38,13 +39,10 @@ $(document).ready(function() {
         url: "http://localhost:3000/api/accounts",
         {% endif %}
         success: function(response) {
-          $("#credentials-email").text(response.account);
-          $("#credentials-account").text(response.account);
-          $("#credentials-api-key").text(response.api_key);
-
-          $('.hosted-conjur-signup').fadeOut("normal", function(){
-            $(this).next(".hosted-confirmation").slideDown("normal");
-          });
+          document.cookie = "account="+JSON.stringify(response);
+          displayAccountCredentials(response.account,
+                                    response.account,
+                                    response.api_key);
         },
         error: function(xhr, ajaxOptions, thrownError) {
           // Manually inserting the error here because there doesn't seem to
