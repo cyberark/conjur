f92e7e833e41ff66d66ac69986ae261e475b7292
John Tuttle <jtuttle@conjur.net>
2017-08-15 18:14:39 -0400

    Account info visible when user returns 4277 (#254)
    * store cookie on success, display creds when cookie data found
    
    * clean up cookie functionality, replace other fields on page
    
    * add expiration date and path to cookie
    
    * Updated class names to reference the entire form element

--- docs/javascript/conjur-account.js
+++ docs/javascript/conjur-account.js
@@ -2,28 +2,55 @@
 ---
 //
 
-var cookies = document.cookie.split('; ');
-var account = null;
+function getAccountCookie() {
+  var cookies = document.cookie.split('; ');
 
-cookies.forEach(function(c) {
-  var name, value;
-  [name,value] = c.split('=');
-  if (name == 'email') {
-    account = value;
+  for(var i = 0; i < cookies.length; i++) {
+    var name, value;
+    [name, value] = cookies[i].split('=');
+
+    if(name == "account") {
+      return JSON.parse(value);
+    }
   }
-});
+  return null;
+}
 
-if (account) {
+function setAccountCookie(value) {
+  var date = new Date();
+  date.setTime(date.getTime()+(120*24*60*60*1000));
+  var expires = "; expires="+date.toGMTString();
+
+  document.cookie = 'account=' + value + '; expires=' + expires + '; path=/';
+}
+
+function displayAccountCredentials(email, account, api_key) {
+  $("#credentials-email").text(email);
+  $("#credentials-account").text(account);
+  $("#credentials-api-key").text(api_key);
+
+  $('.hosted-conjur-signup').fadeOut("normal", function() {
+    $('.hosted-conjur-signup').next(".hosted-confirmation").slideDown("normal");
+  });
+
   var spans = document.querySelectorAll('span');
 
   spans.forEach(function(s) {
-    if (s.innerText == '"your-conjur-account-id"') {
+    if(s.innerText == '"your-conjur-account-id"') {
       s.innerText = '"' + account + '"';
     }
   });
 }
 
 $(document).ready(function() {
+  var accountCookie = getAccountCookie();
+
+  if(accountCookie !== null) {
+    displayAccountCredentials(accountCookie.account,
+                              accountCookie.account,
+                              accountCookie.api_key);
+  }
+
   $('.hosted-account-signup').validator().on('submit', function(e) {
     if(!e.isDefaultPrevented()) {
       e.preventDefault();
@@ -38,13 +65,10 @@ $(document).ready(function() {
         url: "http://localhost:3000/api/accounts",
         {% endif %}
         success: function(response) {
-          $("#credentials-email").text(response.account);
-          $("#credentials-account").text(response.account);
-          $("#credentials-api-key").text(response.api_key);
-
-          $('.hosted-conjur-signup').fadeOut("normal", function(){
-            $(this).next(".hosted-confirmation").slideDown("normal");
-          });
+          setAccountCookie(JSON.stringify(response));
+          displayAccountCredentials(response.account,
+                                    response.account,
+                                    response.api_key);
         },
         error: function(xhr, ajaxOptions, thrownError) {
           // Manually inserting the error here because there doesn't seem to
