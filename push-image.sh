c830f76471a00c4b1a4b9bf49c1c28f214a39597
Dustin Collins <dustinrcollins@gmail.com>
2017-08-22 11:59:52 -0500

    Push images to docker registries

--- push-image.sh
+++ push-image.sh
@@ -1,34 +1,43 @@
-#!/bin/bash -ex
+#!/bin/bash -e
+
+# Push the 'conjur' image to various Docker registries
+# Push stable images on master branch
+# Release images can be created by passing the desired tag to this script
+# Ex: ./push-image 4.9.5.1
+
+TAG="${1:-$(< VERSION)-$(git rev-parse --short HEAD)}"
+
+SOURCE_IMAGE='conjur'
+INTERNAL_IMAGE='registry.tld/conjur'
+DOCKERHUB_IMAGE='cyberark/conjur'
+QUAY_IMAGE='quay.io/cyberark/conjur'
 
 function main() {
-  local version="$(< VERSION)-$(git rev-parse --short HEAD)"
+  echo "TAG = $TAG"
 
-  push_to_registries $version
+  tag_and_push $INTERNAL_IMAGE $TAG
 
   if [ "$BRANCH_NAME" = "master" ]; then
-    local stable_version="$(< VERSION)-stable"
-    push_to_registries $stable_version
+    local stable_tag="$(< VERSION)-stable"
+
+    echo "TAG = $stable_tag, stable image"
+
+    tag_and_push $INTERNAL_IMAGE $stable_tag
+
+    tag_and_push $DOCKERHUB_IMAGE $TAG
+    tag_and_push $DOCKERHUB_IMAGE $stable_tag
+
+    tag_and_push $QUAY_IMAGE $TAG
+    tag_and_push $QUAY_IMAGE $stable_tag
   fi
 }
 
-function push_to_registries() {
-  local version="$1"
+function tag_and_push() {
+  local image="$1"
+  local tag="$2"
 
-  local internal_tag="registry.tld/conjur:$version"
-  docker tag conjur $internal_tag
-  docker push $internal_tag
-  
-  local legacy_tag="registry.tld/possum:$version"
-  docker tag conjur $legacy_tag
-  docker push $legacy_tag
-
-  local dockerhub_tag="conjurinc/conjur:$version"
-  docker tag conjur $dockerhub_tag
-  docker push $dockerhub_tag
-
-  local quay_tag="quay.io/conjur/conjur:$version"
-  docker tag conjur $quay_tag
-  docker push $quay_tag
+  docker tag "$SOURCE_IMAGE" "$image:$tag"
+  docker push "$image:$tag"
 }
 
 main
