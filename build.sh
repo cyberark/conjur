519684cd0e44cb4f13573db07c75ba5085a6314f
Dustin Collins <dustinmm80@users.noreply.github.com>
2017-08-30 15:27:21 -0500

    Split internal and external docker image pushes (#327)
    * start to split up internal and external docker image pushing
    
    * build.sh now uses TAG, tags image right after build
    
    * build.sh needs no $1 arg

--- build.sh
+++ build.sh
@@ -1,13 +1,14 @@
 #!/bin/bash -ex
 
-run_dev=true
+TAG="$(< VERSION)-$(git rev-parse --short HEAD)"
+RUN_DEV=true
 
 while [[ $# -gt 0 ]]
 do
 key="$1"
 case $key in
     -j|--jenkins)
-    run_dev=false
+    RUN_DEV=false
     ;;
     *)
     ;;
@@ -16,11 +17,10 @@ shift # past argument or value
 done
 
 docker build -t conjur .
+docker tag conjur "conjur:$TAG"
 
 docker build -t conjur-test -f Dockerfile.test .
 
-if [[ $run_dev = true ]]; then
+if [[ $RUN_DEV = true ]]; then
   docker build -t conjur-dev -f Dockerfile.dev .
 fi
-
-docker tag conjur conjurinc/conjur || docker tag -f conjur conjurinc/conjur
