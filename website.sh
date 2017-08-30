480593035ae6adb8cd3da2e1b52cd542c705fc48
Ryan Prior <ryanprior@gmail.com>
2017-08-30 11:42:50 -0500

    Uses conjur-docs and conjur-apidocs images in website.sh

--- website.sh
+++ website.sh
@@ -1,20 +1,15 @@
 #!/bin/bash -ex
 
-docker build -t conjur-web -f docs/Dockerfile .
+docker-compose run --rm apidocs > docs/_includes/api.html
 
-docker run -i -v $PWD:/opt/conjur --rm  \
+docker-compose run -i --rm  \
   -e AWS_ACCESS_KEY_ID \
   -e AWS_SECRET_ACCESS_KEY \
   -e POSSUM_WEB_BUCKET -e POSSUM_WEB_CFG_BUCKET \
   -e POSSUM_WEB_USER -e POSSUM_WEB_PASSWORD \
   -e CPANEL_URL \
-  conjur-web bash -ec '
+  docs bash -ec '
 mkdir /output
-
-/node_modules/.bin/aglio --theme-template apidocs/templates/index.jade --theme-style apidocs/templates/css/layout-conjur.less -i apidocs/src/api.md -o docs/_includes/api.html
-
-cd docs
-
 jekyll build --destination /output/_site
 
 echo "${POSSUM_WEB_USER}:$(openssl passwd -apr1 ${POSSUM_WEB_PASSWORD})" | aws s3 cp - s3://${POSSUM_WEB_CFG_BUCKET}/htpasswd
