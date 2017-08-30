b7d802534095a5786b265fe9918a5ea5ad2072af
Ryan Prior <ryanprior@gmail.com>
2017-08-30 12:13:47 -0500

    Updating website.sh to reflect different CWD

--- website.sh
+++ website.sh
@@ -8,8 +8,8 @@ docker-compose run --rm  \
   -e POSSUM_WEB_BUCKET -e POSSUM_WEB_CFG_BUCKET \
   -e POSSUM_WEB_USER -e POSSUM_WEB_PASSWORD \
   docs bash -ec '
-mkdir /output
-jekyll build --source docs --destination /output/_site
+mkdir -p /output
+jekyll build --destination /output/_site
 
 echo "${POSSUM_WEB_USER}:$(openssl passwd -apr1 ${POSSUM_WEB_PASSWORD})" | aws s3 cp - s3://${POSSUM_WEB_CFG_BUCKET}/htpasswd
 
