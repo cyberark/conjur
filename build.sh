f9c96f0906fa18ecced3a15e4ed3a62e9348f758
Dustin Collins <dustinrcollins@gmail.com>
2017-08-30 15:06:46 -0500

    build.sh now uses TAG, tags image right after build

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -ex
 
+TAG="${1:-$(< VERSION)-$(git rev-parse --short HEAD)}"
+
 run_dev=true
 
 while [[ $# -gt 0 ]]
@@ -16,11 +18,10 @@ shift # past argument or value
 done
 
 docker build -t conjur .
+docker tag conjur "conjur:$TAG"
 
 docker build -t conjur-test -f Dockerfile.test .
 
 if [[ $run_dev = true ]]; then
   docker build -t conjur-dev -f Dockerfile.dev .
 fi
-
-docker tag conjur conjurinc/conjur || docker tag -f conjur conjurinc/conjur
