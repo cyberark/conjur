0851a2778376a91458629c441f0563e6a88d99aa
Joe Garcia, CISSP <infamousjoeg@users.noreply.github.com>
2017-09-07 12:02:45 -0400

    Create build.ps1

---
+++ build.ps1
@@ -0,0 +1,16 @@
+$tag = "$(git rev-parse --short HEAD)"
+$run_dev = $true
+
+Write-Host "Building Conjur Docker image"
+& docker build -t conjur .
+
+Write-Host "Tagging conjur:${tag}"
+& docker tag conjur "conjur:${tag}"
+
+Write-Host "Building test container"
+& docker build -t conjur-test -f Dockerfile.test . 
+
+if ($run_dev) {
+    Write-Host "Building dev container"
+    & docker build -t conjur-dev -f Dockerfile.dev . 
+}
