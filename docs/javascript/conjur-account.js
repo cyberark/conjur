0a02413af3bfc924b75b02c093a8c99592b6b43c
John Tuttle <jtuttle@conjur.net>
2017-09-12 13:32:22 -0400

    working reset form

--- docs/javascript/conjur-account.js
+++ docs/javascript/conjur-account.js
@@ -42,6 +42,18 @@ function displayAccountCredentials(email, account_id, api_key) {
   });
 }
 
+function displayResetApiKey(email) {
+  $("#reset-api-key-email").text(email);
+
+  $(".hosted-api-key-reset").show();
+  
+  $('.hosted-conjur-signup').fadeOut("normal", function() {
+    $('.hosted-conjur-signup').next(".hosted-api-key-reset").slideDown("normal");
+  });
+
+  $(".hosted-confirmation").hide();
+}
+
 function displayError(formField, message) {
   // Manually inserting the error here because there doesn't seem to
   // be a way to make bootstrap-validator display an error on-demand.
@@ -55,6 +67,78 @@ function displayError(formField, message) {
   formField.siblings(".help-block.with-errors").first().html(errorElement);
 }
 
+function submitAccountSignup() {
+  var recaptchaToken = grecaptcha.getResponse();
+    
+  if(recaptchaToken === "") {
+    displayError($("#recaptcha").first(), "Please complete reCAPTCHA.");
+    return;
+  }
+
+  var payload =
+      "email=" + $("#email-address").val() +
+      "&organization=" + $("#organization").val() +
+      "&recaptcha_token=" + recaptchaToken;
+  
+  $.ajax({
+    context: this,
+    type: "POST",
+    data: payload,
+    url: "{{site.cpanel_url}}/api/accounts",
+    success: function(response) {
+      setAccountCookie(JSON.stringify(response));
+      displayAccountCredentials(response.email,
+                                response.account_id,
+                                response.api_key);
+    },
+    error: function(xhr, ajaxOptions, thrownError) {
+      if(xhr.responseJSON === undefined) { return; }
+      
+      var error = xhr.responseJSON.error;
+      var formField;
+
+      if(error.code == "duplicate_email") {
+        displayResetApiKey($("#email-address").val());
+      } else {
+        if(error.target == "email") {
+          formField = $("#email-address").first();
+        } else if(error.target == "organization") {
+          formField = $("#organization").first();
+        } else if(error.target == "recaptcha") {
+          formField = $("#recaptcha").first();
+        }
+
+        if(formField !== undefined && error !== undefined) {
+          displayError(formField, error.message);
+        }
+      }
+    }
+  });
+}
+
+function submitApiKeyReset(email) {
+  payload = "email=" + email;
+  
+  $.ajax({
+    context: this,
+    type: "POST",
+    data: payload,
+    url: "{{site.cpanel_url}}/api/accounts/reset_api_key",
+    success: function(response) {
+      setAccountCookie(JSON.stringify(response));
+
+      $(".hosted-api-key-reset").hide();
+      
+      displayAccountCredentials(response.email,
+                                response.account_id,
+                                response.api_key);
+    },
+    error: function(xhr, ajaxOptions, thrownError) {
+      if(xhr.responseJSON === undefined) { return; }
+    }
+  });
+}
+
 $(document).ready(function() {
   var accountCookie = getAccountCookie();
 
@@ -63,53 +147,18 @@ $(document).ready(function() {
                               accountCookie.account_id,
                               accountCookie.api_key);
   }
-
+  
   $('.hosted-account-signup').validator().on('submit', function(e) {
     if(!e.isDefaultPrevented()) {
       e.preventDefault();
+      submitAccountSignup();
+    }
+  });
 
-      var recaptchaToken = grecaptcha.getResponse();
-      
-      if(recaptchaToken === "") {
-        displayError($("#recaptcha").first(), "Please complete reCAPTCHA.");
-        return;
-      }
-      
-      var payload =
-          "email=" + $("#email-address").val() +
-          "&organization=" + $("#organization").val() +
-          "&recaptcha_token=" + recaptchaToken;
-      
-      $.ajax({
-        context: this,
-        type: "POST",
-        data: payload,
-        url: "{{site.cpanel_url}}/api/accounts",
-        success: function(response) {
-          setAccountCookie(JSON.stringify(response));
-          displayAccountCredentials(response.email,
-                                    response.account_id,
-                                    response.api_key);
-        },
-        error: function(xhr, ajaxOptions, thrownError) {
-          if(xhr.responseJSON === undefined) { return; }
-          
-          var error = xhr.responseJSON.error;
-          var formField;
-          
-          if(error.target == "email") {
-            formField = $("#email-address").first();
-          } else if(error.target == "organization") {
-            formField = $("#organization").first();
-          } else if(error.target == "recaptcha") {
-            formField = $("#recaptcha").first();
-          }
-
-          if(formField !== undefined && error !== undefined) {
-            displayError(formField, error.message);
-          }
-        }
-      });
+  $('.hosted-account-api-key-reset').on('submit', function(e) {
+    if(!e.isDefaultPrevented()) {
+      e.preventDefault();
+      submitApiKeyReset($("#email-address").val());
     }
   });
 });
