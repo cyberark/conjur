64d8e8799758aa03bee3a978a60beb3c0fc6021b
Ryan Prior <ryan.prior@cyberark.com>
2017-11-14 15:06:24 -0600

    Increases maximum POST request size to 10MB
    This allows loading of large policies. Previous (Rails default) max size was
    4kb.

--- config/initializers/limits.rb
+++ config/initializers/limits.rb
@@ -2,3 +2,7 @@
 def secrets_version_limit
   ( ENV['SECRETS_VERSION_LIMIT'] || 20 ).to_i
 end
+
+# Max size of policies that can be loaded (or data sent to POST endpoints in
+# general)
+Rack::Utils.key_space_limit = 1048576 * 10 # 10 MiB
