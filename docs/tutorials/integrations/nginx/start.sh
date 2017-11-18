043b0de86ee70e2e0821c2545db1daa03ab0c799
Ryan Prior <ryan.prior@cyberark.com>
2017-11-17 21:59:28 -0600

    Automates logging in to demo account

--- docs/tutorials/integrations/nginx/start.sh
+++ docs/tutorials/integrations/nginx/start.sh
@@ -3,6 +3,9 @@
 # docker-compose pull
 # docker pull svagi/openssl
 
+rm -f tls/nginx.key tls/nginx.crt
+docker-compose down
+
 docker run --rm -it \
        -w /home -v $PWD/tls:/home \
        svagi/openssl req\
@@ -18,3 +21,11 @@ docker run --rm -it \
 docker-compose run --no-deps --rm conjur data-key generate > data_key
 export CONJUR_DATA_KEY="$(< data_key)"
 docker-compose up -d
+
+sleep 6
+
+docker-compose exec conjur conjurctl account create test | tee test.out
+api_key="$(grep API test.out | cut -d: -f2 | tr -d ' \r\n')"
+
+docker-compose exec client bash -c "echo yes | conjur init -u https://proxy -a test"
+docker-compose exec client conjur authn login -u admin -p "$api_key"
