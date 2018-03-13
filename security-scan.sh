c7adf93b4448f0128c31623767a43e0bc8881d99
Jason Vanderhoof <jvanderhoof@gmail.com>
2018-03-13 12:57:09 -0600

    Add update to audit & remove generated brakeman file

--- security-scan.sh
+++ security-scan.sh
@@ -17,9 +17,9 @@ shift # past argument or value
 done
 
 if [[ $RUN_BRAKEMAN = true ]]; then
-  docker run -v "$(pwd):/tmp/" -w /tmp/  codeclimate/codeclimate-brakeman brakeman -A -o brakeman-output.tabs
+  docker run -v "$(pwd):/tmp/" -w /tmp/  codeclimate/codeclimate-brakeman brakeman -A
 fi
 
 if [[ $RUN_GEM_AUDIT = true ]]; then
-  docker run -v "$(pwd):/tmp/" -w /tmp/ codeclimate/codeclimate-bundler-audit bundle audit check
+  docker run -v "$(pwd):/tmp/" -w /tmp/ codeclimate/codeclimate-bundler-audit bundle audit check --update
 fi
