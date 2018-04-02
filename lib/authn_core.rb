3df7ad826333a8c2cda78e1008d5542ec5261d4e
Geri Jennings <geri.jennings@cyberark.com>
2018-04-02 09:24:05 -0400

    updates to authn_core

--- lib/authn_core.rb
+++ lib/authn_core.rb
@@ -2,23 +2,38 @@ require 'conjur-api'
 
 class AuthnCore
 
+  class BadRequestError < RuntimeError
+  end
+
+  class NotFoundError < RuntimeError
+  end
+
+  class AuthenticationError < RuntimeError
+  end
+
   # required info:
   # service_id, authn_type, user_id (which might be host/host_id)
   class << self
     def authorized? authn_type, service_id, user_id
+      @authn_type = authn_type
+      @service_id = service_id
+      @user_id = user_id
+
+      raise BadRequestError, "Invalid authn configuration" if @authn_type.nil? || @service_id.nil? || @user_id.nil?
+
       enabled? && service_exists? && user_exists? && user_authorized?    
     end
 
     def enabled?
       authenticators = (ENV['CONJUR_AUTHENTICATORS'] || '').split(',').map(&:strip)
-      unless authenticators.include?("authn-#{authn_type}/#{service_id}")
-        raise NotFoundError, "authn-#{authn_type}/#{service_id} not whitelisted in CONJUR_AUTHENTICATORS"
+      unless authenticators.include?("authn-#{@authn_type}/#{@service_id}")
+        raise NotFoundError, "authn-#{@authn_type}/#{@service_id} not whitelisted in CONJUR_AUTHENTICATORS"
       end
     end
 
     def service_exists?
-      @service ||= user_api_client.resource("webservice:conjur/authn-#{authn_type}/#{service_id}")
-    raise NotFoundError, "Service #{service_id} not found" unless @service.exists?
+      @service ||= user_api_client.resource("webservice:conjur/authn-#{@authn_type}/#{@service_id}")
+    raise NotFoundError, "Service #{@service_id} not found" unless @service.exists?
     end
 
     def user_exists?
@@ -30,7 +45,7 @@ class AuthnCore
     end
 
     def user
-      @user ||= user_api_client.user(user_id)
+      @user ||= user_api_client.user(@user_id)
     end
 
     def user_api_client
@@ -38,7 +53,7 @@ class AuthnCore
     end
 
     def user_token
-      @user_token ||= Conjur::API.authenticate_local "#{user_id}"
+      @user_token ||= Conjur::API.authenticate_local "#{@user_id}"
     end
   end
 end
