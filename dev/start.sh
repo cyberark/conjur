a9ceb400550e29dc98f55ced91833fb3106db027
Jason Vanderhoof <jvanderhoof@gmail.com>
2018-04-09 12:13:29 -0600

    Updates to integrate authn-ldap with dev workflow (#519)

--- dev/start.sh
+++ dev/start.sh
@@ -1,12 +1,11 @@
 #!/bin/bash -ex
 
 export COMPOSE_PROJECT_NAME=conjurdev
-
 docker-compose build
 
 if [ ! -f data_key ]; then
 	echo "Generating data key"
-	docker-compose run --no-deps --rm --entrypoint conjurctl conjur data-key generate > data_key
+	openssl rand -base64 32 > data_key
 fi
 
 export CONJUR_DATA_KEY="$(cat data_key)"
@@ -14,7 +13,6 @@ export CONJUR_DATA_KEY="$(cat data_key)"
 docker-compose up -d
 docker-compose exec conjur bundle
 docker-compose exec conjur conjurctl db migrate
-docker-compose exec conjur conjurctl account create cucumber || true
-docker-compose exec -d conjur conjurctl server
-# docker-compose exec conjur bash
-# docker exec -it --detach-keys 'ctrl-\' $(docker-compose ps -q conjur) bash
+docker-compose exec conjur conjurctl account create cucumber
+
+docker exec -it --detach-keys 'ctrl-\' $(docker-compose ps -q conjur) bash
