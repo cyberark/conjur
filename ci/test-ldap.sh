8317b2326b71e4bd20e5f409928fa5f1cc055d7a
Jason Vanderhoof <jvanderhoof@gmail.com>
2018-04-18 14:38:44 -0600

    run cukes

--- ci/test-ldap.sh
+++ ci/test-ldap.sh
@@ -13,7 +13,6 @@ export COMPOSE_PROJECT_NAME="$(openssl rand -hex 3)"
 
 # Generate a data key
 export CONJUR_DATA_KEY="$(openssl rand -base64 32)"
-echo "export COMPOSE_PROJECT_NAME=$COMPOSE_PROJECT_NAME"
 
 # Grab the build tag so we launch the correct version of Conjur
 cd ..
@@ -33,4 +32,4 @@ api_key=$(docker-compose exec conjur conjurctl \
   role retrieve-key cucumber:user:admin | tr -d '\r')
 
 docker-compose run --rm -e CONJUR_AUTHN_API_KEY=$api_key cucumber -c \
-  'bundle exec rake jenkins:core:cucumber-api'
+  'bundle exec rake jenkins:authn_ldap:cucumber'
