9169078325a0ef483bcda18e0383c24599ae78d4
Jonah <jonah@moocho.com>
2018-04-18 12:12:14 -0400

    Remove redundant work from merge

--- cucumber/authn-ldap/features/support/hooks.rb
+++
@@ -1,6 +0,0 @@
-require 'aruba'
-require 'aruba/cucumber'
-require 'conjur-api'
-
-Conjur.configuration.appliance_url = ENV['CONJUR_APPLIANCE_URL'] || 'http://possum'
-Conjur.configuration.account = ENV['CONJUR_ACCOUNT'] || 'cucumber'
