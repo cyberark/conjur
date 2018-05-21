1a4576da7bf4ca6aa94ceb8989599d3fd0a0c85b
Rafał Rzepecki <divided.mind@gmail.com>
2018-05-21 19:16:12 +0200

    Whitespace fix
    Someone went a lil' bit overboard with newlines.

--- config/routes.rb
+++ config/routes.rb
@@ -28,41 +28,28 @@ Rails.application.routes.draw do
 
       get "/roles/:account/:kind/*identifier" => "roles#all_memberships", :constraints => QueryParameterActionRecognizer.new("all")
       get "/roles/:account/:kind/*identifier" => "roles#direct_memberships", :constraints => QueryParameterActionRecognizer.new("memberships")
-
       get "/roles/:account/:kind/*identifier" => "roles#show"
 
       get "/resources/:account/:kind/*identifier" => 'resources#check_permission', :constraints => QueryParameterActionRecognizer.new("check")
-
       get "/resources/:account/:kind/*identifier" => 'resources#permitted_roles', :constraints => QueryParameterActionRecognizer.new("permitted_roles")
-
       get "/resources/:account/:kind/*identifier" => "resources#show"
-
       get "/resources/:account/:kind" => "resources#index"
-
       get "/resources/:account" => "resources#index"
-
       get "/resources" => "resources#index"
 
       get "/secrets/:account/:kind/*identifier" => 'secrets#show'
-
       post "/secrets/:account/:kind/*identifier" => 'secrets#create'
-
       get "/secrets" => 'secrets#batch'
 
       put "/policies/:account/:kind/*identifier" => 'policies#put'
-
       patch "/policies/:account/:kind/*identifier" => 'policies#patch'
-
       post "/policies/:account/:kind/*identifier" => 'policies#post'
 
       get "/public_keys/:account/:kind/*identifier" => 'public_keys#show'
     end
 
     post "/host_factories/hosts" => 'host_factories#create_host'
-
     post "/host_factory_tokens" => 'host_factory_tokens#create'
-
     delete "/host_factory_tokens/:id" => 'host_factory_tokens#destroy'
   end
-
 end
