7cb67485a65184986b89bd739ff7fa4615d98cd6
Rafał Rzepecki <divided.mind@gmail.com>
2018-05-29 09:54:55 +0200

    Ensure lib is in load path

--- config/boot.rb
+++ config/boot.rb
@@ -1,3 +1,6 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
+
+# in circumstances unexplained sometimes this ends up not in the load path
+$: << File.expand_path('../../lib', __FILE__)
