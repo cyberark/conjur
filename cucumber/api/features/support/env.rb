19c4fb7ec6117498219d510fefaa7bd29fd6ad8d
Rafał Rzepecki <divided.mind@gmail.com>
2018-06-17 15:41:28 +0200

    Add audit steps and support code for cuke testing
    Note, currently this only works when running cukes locally.

--- cucumber/api/features/support/env.rb
+++ cucumber/api/features/support/env.rb
@@ -1,7 +1,11 @@
+require_relative 'utils'
+
 ENV['CONJUR_ACCOUNT'] = 'cucumber'
-ENV['CONJUR_APPLIANCE_URL'] ||= 'http://localhost:3000'
+ENV['RAILS_ENV'] ||= 'test'
 
 require ::File.expand_path('../../../../../config/environment', __FILE__)
+
+ENV['CONJUR_APPLIANCE_URL'] ||= Utils.start_local_server
 
 require 'json_spec/cucumber'
 
---
+++ lib/test/audit_sink.rb
@@ -0,0 +1,34 @@
+module Test
+  class AuditSink
+    def initialize
+      @socket = UNIXServer.new ''
+      @messages = []
+      listen
+    end
+
+    def listen backlog = 1
+      socket.listen backlog
+      Thread.new { loop { handle socket.accept } }
+        .abort_on_exception = true # to ease debugging
+    end
+
+    def handle sock
+      sock.each_line(&messages.method(:push))
+    end
+
+    def address
+      socket.addr[1]
+    end
+
+    attr_reader :socket, :messages
+
+    class << self
+      def instance
+        @instance ||= new
+      end
+      
+      extend Forwardable
+      def_delegators :instance, :messages
+    end
+  end
+end
