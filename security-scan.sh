dce992b4986e5ecf746d81fb9c0a0f5e85565ab4
Jason Vanderhoof <jvanderhoof@gmail.com>
2018-06-20 13:35:38 -0600

    Pinned version of Brakeman to prevent runtime error

--- security-scan.sh
+++ security-scan.sh
@@ -18,7 +18,7 @@ done
 if [[ $RUN_BRAKEMAN = true ]]; then
   mkdir -p brakeman/reports
   chmod -R 0777 brakeman/reports
-  docker run -v "$(pwd):/tmp/" --rm -w /tmp/ codeclimate/codeclimate-brakeman brakeman -o brakeman/reports/brakeman-output.html
+  docker run -v "$(pwd):/tmp/" --rm -w /tmp/ codeclimate/codeclimate-brakeman:b804 brakeman -o brakeman/reports/brakeman-output.html
 fi
 
 
