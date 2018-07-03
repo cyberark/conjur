36e0e97384e9246066763c0efb181fcb26d848d7
Rafał Rzepecki <divided.mind@gmail.com>
2018-07-03 21:03:38 +0200

    Fix rubocop's Style/NestedParenthesizedCalls offenses

--- config/initializers/audit_socket.rb
+++ config/initializers/audit_socket.rb
@@ -2,7 +2,7 @@ require 'logger'
 require 'logger/formatter/rfc5424_formatter'
 
 if path = Rails.application.config.try(:audit_socket)
-  Audit.logger = Logger.new(UNIXSocket.open path).tap do |logger|
+  Audit.logger = Logger.new(UNIXSocket.open(path)).tap do |logger|
     logger.formatter = Logger::Formatter::RFC5424Formatter
   end
 end
