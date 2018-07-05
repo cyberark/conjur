7d9341c9f8d1ea7b0f8174af16f1ddc9938fd63a
John Tuttle <jtuttle@conjur.net>
2018-07-05 13:34:08 -0400

    fix entitelemtn yml extension

--- ci/authn-k8s/test_gke_entrypoint.sh
+++ ci/authn-k8s/test_gke_entrypoint.sh
@@ -133,7 +133,6 @@ function launchConjurMaster() {
 
   wait_for_it 300 "kubectl describe po $pod_name | grep Status: | grep -q Running"
   
-#  kubectl exec $pod_name -- conjurctl db migrate
   export API_KEY=$(kubectl exec $pod_name -- conjurctl account create cucumber | tail -n 1 | awk '{ print $NF }')
   
 #  kubectl cp $pod_name:/opt/conjur/etc/authn-k8s.conf ./dev/tmp/authn-k8s.conf
