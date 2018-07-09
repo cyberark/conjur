e8a2aa77c19849d1a73b5312016c1d1ccdec94fc
Jonah <jonah@moocho.com>
2018-07-09 13:54:18 -0400

    Require rails environment from project root

--- cucumber/api/features/support/env.rb
+++ cucumber/api/features/support/env.rb
@@ -1,11 +1,17 @@
 # frozen_string_literal: true
 
-require_relative 'utils'
+# so that we can require relative to the project root
+$LOAD_PATH.unshift(Dir.pwd)
+require 'config/environment'
+
 require 'json_spec/cucumber'
-require ::File.expand_path('../../../../../config/environment', __FILE__)
+require_relative 'utils'
 
 ENV['CONJUR_ACCOUNT'] = 'cucumber'
 ENV['RAILS_ENV'] ||= 'test'
+
+# This line is here to support running these tests outside a container.
+# It could be deleted were it not for that.
 ENV['CONJUR_APPLIANCE_URL'] ||= Utils.start_local_server
 
 Slosilo["authn:cucumber"] ||= Slosilo::Key.new
--- cucumber/authenticators/features/support/env.rb
+++ cucumber/authenticators/features/support/env.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
+$LOAD_PATH.unshift(Dir.pwd)
+require 'config/environment'
 
 require 'rest-client'
 require 'aruba'
 require 'aruba/cucumber'
 require 'conjur-api'
 require 'json_spec/cucumber'
-
-require ::File.expand_path('../../../../../config/environment', __FILE__)
 
 Conjur.configuration.appliance_url = ENV['CONJUR_APPLIANCE_URL'] || 'http://possum'
 Conjur.configuration.account = ENV['CONJUR_ACCOUNT'] || 'cucumber'
