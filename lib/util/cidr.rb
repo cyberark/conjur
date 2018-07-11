ae507bb7b9023057b508f9fcb9c120efdcf6831a
Micah Lee <micah.lee@cyberark.com>
2018-07-11 09:23:40 -0400

    Rename instance variable to clarify decorator vs decorated

--- lib/util/cidr.rb
+++ lib/util/cidr.rb
@@ -7,23 +7,23 @@ module Util
   class CIDR
     extend Forwardable
 
-    def_delegators :@cidr, :include?
+    def_delegators :@ip_addr, :include?
 
     def initialize(ip_addr)
-      @cidr = ip_addr
+      @ip_addr = ip_addr
     end
 
     def to_s
-      cidr.is_a?(IPAddr) ? "#{cidr}/#{mask}" : cidr.to_s
+      ip_addr.is_a?(IPAddr) ? "#{ip_addr}/#{mask}" : ip_addr.to_s
     end
 
     private 
 
-    attr_reader :cidr
+    attr_reader :ip_addr
 
     def mask
-      mask = cidr.instance_variable_get(:@mask_addr).to_s(2)[/\A(1*)0*\z/, 1]
-      raise ArgumentError, "invalid IP mask in #{cidr.inspect}" if mask.nil?
+      mask = ip_addr.instance_variable_get(:@mask_addr).to_s(2)[/\A(1*)0*\z/, 1]
+      raise ArgumentError, "invalid IP mask in #{ip_addr.inspect}" if mask.nil?
       mask.length
     end
   end
