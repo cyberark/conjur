86348e75b49329ba4fb8af1111cdad3dd05f4a7e
Rafał Rzepecki <divided.mind@gmail.com>
2018-07-19 04:13:47 +0200

    Fixes for API cuke environment config
    - Set RAILS_ENV before the environment is actually loaded.
    - Resolve project root relative to the file instead of using pwd.

--- cucumber/api/features/support/env.rb
+++ cucumber/api/features/support/env.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
 
+ENV['CONJUR_ACCOUNT'] = 'cucumber'
+ENV['RAILS_ENV'] ||= 'test'
+
 # so that we can require relative to the project root
-$LOAD_PATH.unshift(Dir.pwd)
+$LOAD_PATH.unshift File.expand_path '../../../..', __dir__
 require 'config/environment'
 
 require 'json_spec/cucumber'
 require_relative 'utils'
-
-ENV['CONJUR_ACCOUNT'] = 'cucumber'
-ENV['RAILS_ENV'] ||= 'test'
 
 # This line is here to support running these tests outside a container,
 # per Rafal's request.  It could be deleted were it not for that.
