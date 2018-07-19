d11c72eafda8fae7011fff4dab425c06b2d1fe71
Jonah <jonah@moocho.com>
2018-07-19 12:02:57 -0400

    WIP: checkin to test on jenkins

--- app/controllers/authenticate_controller.rb
+++ app/controllers/authenticate_controller.rb
@@ -7,9 +7,9 @@
 #       It's purpose it to load all the pluggable authenticator files into 
 #       memory, so we can determine which authenticators are available
 #
-Dir[File.expand_path("../domain/authentication/**/*.rb", __dir__)].each do |f|
-  require f
-end
+# Dir[File.expand_path("../domain/authentication/**/*.rb", __dir__)].each do |f|
+#   require f
+# end
 
 class AuthenticateController < ApplicationController
 
