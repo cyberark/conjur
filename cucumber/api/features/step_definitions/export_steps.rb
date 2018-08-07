1fcd911d2f131854a73d83b442ecf17743e89e31
Micah Lee <micah.lee@cyberark.com>
2018-08-07 15:43:43 -0400

    Use `Dir.mktmpdir` to create temporary directory for test

--- cucumber/api/features/step_definitions/export_steps.rb
+++ cucumber/api/features/step_definitions/export_steps.rb
@@ -9,22 +9,21 @@ end
 
 Then (/^the accounts file contains "([^"]*)"$/) do |contents|
   key_file = 'cuke_export/key'.freeze
-  temp_dir = "#{Dir.tmpdir()}/#{SecureRandom.hex(5)}".freeze
-  plain_backup_file = temp_dir + '/backup.tar.xz'
   backup_file = Dir['cuke_export/*.tar.xz.gpg'].sort().first
 
-  # Decrypt export archive
-  FileUtils.mkpath(temp_dir)
-  system %(gpg --quiet --batch --no-use-agent --passphrase-file #{key_file} -o #{plain_backup_file} #{backup_file})
-  
-  Dir.chdir temp_dir do
-    # Extract archive contents
-    system %(tar -v -x -f #{plain_backup_file})
+  Dir.mktmpdir do |temp_dir|
+    plain_backup_file = temp_dir + '/backup.tar.xz'
 
-    # Check accounts contents
-    accounts = File.read("#{temp_dir}/opt/conjur/backup/accounts")
-    fail('Exported accounts does not contain expected content') unless accounts.include?(contents)
-  end 
-ensure
-  FileUtils.rm_rf(temp_dir)
+    # Decrypt export archive
+    system %(gpg --quiet --batch --no-use-agent --passphrase-file #{key_file} -o #{plain_backup_file} #{backup_file})
+
+    Dir.chdir temp_dir do
+      # Extract archive contents
+      system %(tar -v -x -f #{plain_backup_file})
+  
+      # Check accounts contents
+      accounts = File.read("#{temp_dir}/opt/conjur/backup/accounts")
+      fail('Exported accounts does not contain expected content') unless accounts.include?(contents)
+    end 
+  end
 end
