c72e41a6cf4e2155e35b49ba6a1e42f37c88463a
Jonah <jonah@moocho.com>
2018-09-13 14:46:07 -0400

    Fix CA problem

--- app/domain/repos/conjur_ca.rb
+++ app/domain/repos/conjur_ca.rb
@@ -3,35 +3,21 @@ module Repos
 
     # Generates a CA certificate and key and store them in Conjur variables.  
     def self.create(service_id)
-      Rails.logger.debug("jonah0 creating CA for #{service_id}")
-      cr = cert_resource(service_id)
-      Rails.logger.debug(cr.inspect)
-      Rails.logger.debug(cr.cert_id)
-      ca = ::Util::OpenSsl::CA.from_subject(cr.cert_subject)
-      Secret.create(resource_id: cr.cert_id, value: ca.cert.to_pem)
-      Secret.create(resource_id: cr.key_id, value: ca.key.to_pem)
+      ca_info = ::Conjur::CaInfo.new(service_id)
+      ca = ::Util::OpenSsl::CA.from_subject(ca_info.cert_subject)
+      Secret.create(resource_id: ca_info.cert_id, value: ca.cert.to_pem)
+      Secret.create(resource_id: ca_info.key_id, value: ca.key.to_pem)
     end
 
-    # Initialize CA from Conjur variables
-    #
-    # Note: This repo accepts a domain object (a resource model object)
-    #       and returns a domain object.
+    # Initialize stored CA from Conjur resource_id
     #
     def self.ca(service_id)
-      Rails.logger.debug("jonah1 creating CA for #{service_id}")
-      cr = cert_resource(service_id)
-      Rails.logger.debug("jonah1 #{cr.inspect}")
-      Rails.logger.debug("jonah1 #{cr.cert_id}")
-      stored_cert = Resource[cr.cert_id.to_s].last_secret.value
-      stored_key = Resource[cr.key_id.to_s].last_secret.value
+      ca_info = ::Conjur::CaInfo.new(service_id)
+      stored_cert = Resource[ca_info.cert_id].last_secret.value
+      stored_key = Resource[ca_info.key_id].last_secret.value
       ca_cert = OpenSSL::X509::Certificate.new(stored_cert)
       ca_key = OpenSSL::PKey::RSA.new(stored_key)
       ::Util::OpenSsl::CA.new(ca_cert, ca_key)
-    end
-
-    def self.cert_resource(service_id)
-      resource = Resource[service_id]
-      ::Conjur::CertificateResource.new(resource)
     end
 
   end
