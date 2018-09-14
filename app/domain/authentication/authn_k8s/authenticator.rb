ea3b0f8a17bad9fbbdc0ce9a65c5a27d3d01b208
Jonah <jonah@moocho.com>
2018-09-14 13:03:35 -0400

    Comment stuff out

--- app/domain/authentication/authn_k8s/authenticator.rb
+++ app/domain/authentication/authn_k8s/authenticator.rb
@@ -110,11 +110,11 @@ module Authentication
 
       def pod_request
         #TODO:
-        Rails.logger.debug("service_id #{service_id}")
-        Rails.logger.debug("account #{account}")
-        Rails.logger.debug("cert.san #{cert.san}")
-        Rails.logger.debug("spiffe_id #{spiffe_id}")
-        Rails.logger.debug("jonah #{header_cert_str}")
+        # Rails.logger.debug("service_id #{service_id}")
+        # Rails.logger.debug("account #{account}")
+        # Rails.logger.debug("cert.san #{cert.san}")
+        # Rails.logger.debug("spiffe_id #{spiffe_id}")
+        # Rails.logger.debug("jonah #{header_cert_str}")
         PodRequest.new(
           service_id: service_id,
           k8s_host: K8sHost.from_cert(
