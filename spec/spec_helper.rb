ba0c2d82f63b4c06ac2201f2afc526667128e696
John Tuttle <jtuttle@conjur.net>
2018-09-17 10:40:33 -0400

    remove spec require

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,8 +5,6 @@ ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 
-require 'websocket-eventmachine-server'
-
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
