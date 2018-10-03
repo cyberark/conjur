566df6a4b091ab1c50f8e9e60fe99bc402f7a331
Jonah <jonahx@gmail.com>
2018-10-03 00:33:06 -0400

    Refactor Authenticator itself

--- app/domain/authentication/authn_oidc/authenticator.rb
+++ app/domain/authentication/authn_oidc/authenticator.rb
@@ -1,3 +1,5 @@
+require 'command_class'
+
 module Authentication
   module AuthnOidc
     class AuthenticationError < RuntimeError; end
@@ -5,57 +7,28 @@ module Authentication
     class OIDCConfigurationError < RuntimeError; end
     class OIDCAuthenticationError < RuntimeError; end
 
-    class Authenticator
+    # TODO: Should really have a verb name "Authenticate" since it's a command
+    # object but we'll leave it like this for now for consistency
+    #
+    Authenticator = CommandClass.new(
+      dependencies: {env: ENV},
+      inputs: [:input, :user_details]
+    ) do
 
-      def initialize(env:)
-        @env = env
-      end
-
-      def valid?(input)
-        @authenticator_name = input.authenticator_name
-        @service_id = input.service_id
-        @conjur_account = input.account
-        @request_body = input.request.body.read
-
-        verify_service_enabled
-
-        # TODO update name to GetUserDetails
-        oidc_authn_service = AuthenticationService.new(service.identifier, @conjur_account)
-        user_details = oidc_authn_service.user_details(@request_body)
-
-        # validate id_token claims - if not raise error
-        validate_id_token_claims(user_details.id_token, oidc_authn_service.client_id, oidc_authn_service.issuer)
-
-        validate_user_info(user_details.user_info, user_details.id_token.sub)
-
-        username = user_details.user_info.preferred_username
-        input.instance_variable_set(:@username, username)
-
+      def call
+        validate_id_token_claims
+        validate_user_info
         true
       end
 
       private
 
-      def service
-        @service ||= Resource["#{@conjur_account}:webservice:conjur/#{@authenticator_name}/#{@service_id}"]
-      end
-
-      def verify_service_enabled
-        verify_service_exist
-
-        raise OIDCConfigurationError, "#{@authenticator_name}/#{@service_id} not whitelisted in CONJUR_AUTHENTICATORS" unless authenticator_available?
-      end
-
-      def verify_service_exist
-        raise OIDCConfigurationError, "Webservice [conjur/#{@authenticator_name}/#{@service_id}] not found in Conjur" unless service
-      end
-
       def validate_id_token_claims(id_token, client_id, issuer)
         expected = { client_id: client_id, issuer: issuer } # , nonce: 'nonce'}
-        id_token.verify! expected
+        @user_details.id_token.verify!(expected)
       end
 
-      def validate_user_info(user_info, id_token_subject)
+      def validate_user_info
         unless user_info.sub == id_token_subject
           raise OIDCAuthenticationError, "User info subject [#{user_info.sub}] and id token subject [#{id_token_subject}] are not equal"
         end
@@ -66,11 +39,51 @@ module Authentication
         end
       end
 
-      def authenticator_available?
-        conjur_authenticators = (@env['CONJUR_AUTHENTICATORS'] || '').split(',').map(&:strip)
-
-        conjur_authenticators.include?("#{@authenticator_name}/#{@service_id}")
+      def user_info
+        @user_details.user_info
       end
+
+      def id_token_subject
+        @user_details.id_token.sub
+      end
+
+      def authenticator_name
+        @input.authenticator_name
+      end
+
+      def service_id
+        @input.service_id
+      end
+
+      def conjur_account
+        @input.account
+      end
+
+      def request_body
+        @request_body ||= @input.request.body.read
+      end
+
+      def service
+        @service ||= Resource["#{conjur_account}:webservice:conjur/#{authenticator_name}/#{service_id}"]
+      end
+
+      # NOTE: These can be removed since they're now handled by
+      # `validate_security` in strategy
+      #
+      # def verify_service_enabled
+      #   verify_service_exists
+
+      #   raise OIDCConfigurationError, "#{authenticator_name}/#{service_id} not whitelisted in CONJUR_AUTHENTICATORS" unless authenticator_available?
+      # end
+
+      # def verify_service_exists
+      #   raise OIDCConfigurationError, "Webservice [conjur/#{authenticator_name}/#{service_id}] not found in Conjur" unless service
+      # end
+
+      # def authenticator_available?
+      #   conjur_authenticators = (@env['CONJUR_AUTHENTICATORS'] || '').split(',').map(&:strip)
+      #   conjur_authenticators.include?("#{authenticator_name}/#{service_id}")
+      # end
     end
   end
 end
