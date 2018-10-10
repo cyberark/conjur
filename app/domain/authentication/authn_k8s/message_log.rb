40a78ead0ac17803cde0a555d95fe7548ab6f246
Srdjan Grubor <sgnn7@sgnn7.org>
2018-10-10 15:32:39 -0500

    Extracted MessageLog class to a separate file
    We don't want to co-mingle these classes as it really makes it hard to
    maintain the codebase.

---
+++ app/domain/authentication/authn_k8s/message_log.rb
@@ -0,0 +1,27 @@
+# Utility class for storing messages received during websocket communication.
+module Authentication
+  module AuthnK8s
+    class MessageLog
+      attr_reader :messages
+
+      def initialize
+        @messages = Hash.new { |hash,key| hash[key] = [] }
+      end
+
+      def save_message(wsmsg)
+        channel_name = wsmsg.channel_name
+
+        unless channel_name
+          raise "Unexpected channel: #{wsmsg.channel_number}"
+        end
+
+        @messages[channel_name.to_sym] << wsmsg.data
+      end
+
+      def save_error_string(str)
+        @messages[:error] << str
+      end
+
+    end
+  end
+end
