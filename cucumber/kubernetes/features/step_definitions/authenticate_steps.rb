b286ceb4719ff8f4e87ea1b0f0b37b8ec6c3c1c4
Srdjan Grubor <sgnn7@sgnn7.org>
2018-10-10 12:07:23 -0500

    Improved certificate fetching logic and messaging for k8s
    We now have appropriate messages when there's problems and better retry
    logic for these steps. Code was cleaned up too to fix a few codeclimate
    issues.

--- cucumber/kubernetes/features/step_definitions/authenticate_steps.rb
+++ cucumber/kubernetes/features/step_definitions/authenticate_steps.rb
@@ -16,6 +16,16 @@ def gen_cert(host_id)
   ::Repos::ConjurCA.create(webservice_resource_id)
 end
 
+def authenticate_k8s(host, cert, key, conjur_id)
+  RestClient::Resource.new(
+    host,
+    ssl_ca_file: './nginx.crt',
+    ssl_client_cert: cert,
+    ssl_client_key: key,
+    verify_ssl: OpenSSL::SSL::VERIFY_PEER
+  )["#{ENV['CONJUR_ACCOUNT']}/#{CGI.escape conjur_id}/authenticate?request_ip=#{@request_ip}"].post('')
+end
+
 Given(/^I use the IP address of(?: a pod in)? "([^"]*)"$/) do |objectid|
   @request_ip = find_matching_pod(objectid)
 end
@@ -25,17 +35,16 @@ Then(/^I( can)? authenticate with authn-k8s as "([^"]*)"( without cert and key)?
 
   conjur_id = conjur_resource_id(namespace, objectid)
 
-  cert = nocertkey ? nil : OpenSSL::X509::Certificate.new(@cert)
+  cert = nil
+  unless nocertkey
+    expect(@cert.to_s).not_to be_empty, "ERROR: Certificate fetched was empty or nil but was expected to be present!"
+    cert = OpenSSL::X509::Certificate.new(@cert)
+  end
+
   key = nocertkey ? nil : @pkey
 
   begin
-    response = RestClient::Resource.new(
-      authn_k8s_host,
-      ssl_ca_file: './nginx.crt',
-      ssl_client_cert: cert,
-      ssl_client_key: key,
-      verify_ssl: OpenSSL::SSL::VERIFY_PEER
-    )["#{ENV['CONJUR_ACCOUNT']}/#{CGI.escape conjur_id}/authenticate?request_ip=#{@request_ip}"].post('')
+    response = authenticate_k8s(authn_k8s_host, cert, key, conjur_id)
   rescue
     raise if success
     @error = $!
@@ -51,18 +60,12 @@ Then(/^I( can)? authenticate pod matching "([^"]*)" with authn-k8s as "([^"]*)"(
   @request_ip ||= detect_request_ip(objectid)
 
   conjur_id = conjur_resource_id(namespace, hostid)
-  
+
   cert = nocertkey ? nil : OpenSSL::X509::Certificate.new(@cert)
   key = nocertkey ? nil : @pkey
-  
+
   begin
-    response = RestClient::Resource.new(
-      authn_k8s_host,
-      ssl_ca_file: './nginx.crt',
-      ssl_client_cert: cert,
-      ssl_client_key: key,
-      verify_ssl: OpenSSL::SSL::VERIFY_PEER
-    )["#{ENV['CONJUR_ACCOUNT']}/#{CGI.escape conjur_id}/authenticate?request_ip=#{@request_ip}"].post('')
+    response = authenticate_k8s(authn_k8s_host, cert, key, conjur_id)
   rescue
     raise if success
     @error = $!
@@ -76,19 +79,13 @@ end
 
 Then(/^I cannot authenticate with pod matching "([^"]*)" as "([^"]*)" using a cert signed by a different CA?$/) do |object_id, host_id|
   @request_ip ||= detect_request_ip(object_id)
-  
-  cert = gen_cert(host_id)
 
   conjur_id = conjur_resource_id(namespace, host_id)
 
+  cert = gen_cert(host_id)
+
   begin
-    RestClient::Resource.new(
-      authn_k8s_host,
-      ssl_ca_file: './nginx.crt',
-      ssl_client_cert: cert,
-      ssl_client_key: @pkey,
-      verify_ssl: OpenSSL::SSL::VERIFY_PEER
-    )["#{ENV['CONJUR_ACCOUNT']}/#{CGI.escape conjur_id}/authenticate?request_ip=#{@request_ip}"].post('')
+    authenticate_k8s(authn_k8s_host, cert, @pkey, conjur_id)
   rescue RestClient::Exception
     @error = $!
   end
