a4bff8909bbfad9984f4a158530324d0495baaf1
“Nessi <Nessi.Lahav@cyberark.com>
2018-11-07 14:49:49 +0200

    delete oidc UTs

--- spec/app/domain/authentication/strategy_spec.rb
+++ spec/app/domain/authentication/strategy_spec.rb
@@ -194,52 +194,6 @@ RSpec.describe 'Authentication::Strategy' do
     end
   end
 
-  context "An available oidc authenticator" do
-    context "that receives valid credentials" do
-      context "that fails Security checks" do
-        subject do
-          Authentication::Strategy.new(
-            authenticators: authenticators,
-            security: failing_security,
-            env: two_authenticator_env,
-            token_factory: token_factory,
-            audit_log: nil,
-            role_cls: nil
-          )
-        end
-        it "raises an error" do
-          allow(subject).to receive(:oidc_user_details) { oidc_user_details }
-          allow(subject).to receive(:oidc_validate_credentials) { true }
-          input_ = input(authenticator_name: 'authn-always-pass')
-          expect{ subject.conjur_token_oidc(input_) }.to raise_error(
-            /FAKE_SECURITY_ERROR/
-          )
-        end
-      end
-
-      context "that passes Security checks" do
-        subject do
-          Authentication::Strategy.new(
-            authenticators: authenticators,
-            security: passing_security,
-            env: two_authenticator_env,
-            token_factory: token_factory,
-            audit_log: nil,
-            role_cls: nil
-          )
-        end
-        it "returns a new token" do
-          allow(subject).to receive(:oidc_user_details) { oidc_user_details }
-          allow(subject).to receive(:oidc_validate_credentials) { true }
-          allow(subject).to receive(:validate_origin) { true }
-          input_ = input(authenticator_name: 'authn-always-pass')
-          expect(subject.conjur_token_oidc(input_)).to equal(a_new_token)
-        end
-      end
-    end
-
-  end
-
   context "An available authenticator" do
     context "that passes Security checks" do
       subject do
