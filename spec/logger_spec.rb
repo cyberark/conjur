ea0fb3f080db611c084062c83752c98f4e71962d
Micah Lee <micah.lee@cyberark.com>
2018-11-28 15:45:43 -0500

    Add test for syslog character escaping

--- spec/logger_spec.rb
+++ spec/logger_spec.rb
@@ -8,5 +8,27 @@ describe Logger::Formatter::RFC5424Formatter do
     expect(formatter.call(3, Time.now, nil, msg)).to start_with '<43>'
   end
 
+  describe "with structured data" do
+    describe "escapes" do
+      it "quote" do
+        params = { quote: '"' }
+        formatted = described_class::Format.sd_parameters params
+        expect(formatted).to eq ["quote=\"\\\"\""]
+      end
+
+      it "backslash" do
+        params = { backslash: '\\' }
+        formatted = described_class::Format.sd_parameters params
+        expect(formatted).to eq ["backslash=\"\\\\\""]
+      end
+
+      it "bracket" do
+        params = { bracket: ']' }
+        formatted = described_class::Format.sd_parameters params
+        expect(formatted).to eq ["bracket=\"\\]\""]
+      end
+    end
+  end
+
   subject(:formatter) { described_class }
 end
