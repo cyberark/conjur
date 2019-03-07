7d004019fe27d7cf4ebd842bd7118150e91bf395
orenbm <oren.benmeir@cyberark.com>
2019-03-07 13:20:51 +0200

    Removed boolean parameter from method

--- cucumber/api/features/support/authz_helpers.rb
+++ cucumber/api/features/support/authz_helpers.rb
@@ -3,21 +3,34 @@
 # Utility methods for Authz steps
 #
 module AuthzHelpers
-  def i_have_a_resource(kind, identifier, must_be_new: false)
-    kind ||= "test-resource"
-    identifier ||= random_hex
-    identifier = denormalize identifier
+  def i_have_a_resource(kind, identifier)
+    kind        ||= "test-resource"
+    identifier  ||= random_hex
+    identifier  = denormalize identifier
     resource_id = "cucumber:#{kind}:#{identifier}"
 
     @resources ||= {}
 
-    if Resource[resource_id: resource_id] && !must_be_new
-      @current_resource = Resource[resource_id: resource_id]
-    else
-      @current_resource =
-        Resource.create(resource_id: resource_id,
-                        owner: @current_user || admin_user)
-    end
+    @current_resource = if Resource[resource_id: resource_id] &&
+      Resource[resource_id: resource_id]
+                        else
+                          Resource.create(resource_id: resource_id,
+                                          owner:       @current_user || admin_user)
+                        end
+
+    @resources[resource_id] = @current_resource
+  end
+
+  def i_have_a_new_resource(kind, identifier)
+    kind        ||= "test-resource"
+    identifier  ||= random_hex
+    identifier  = denormalize identifier
+    resource_id = "cucumber:#{kind}:#{identifier}"
+
+    @resources ||= {}
+
+    @current_resource = Resource.create(resource_id: resource_id,
+                                        owner:       @current_user || admin_user)
 
     @resources[resource_id] = @current_resource
   end
