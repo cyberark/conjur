2110d0393f2655efd309fde727b0d52f4f7d2c54
orenbm <oren.benmeir@cyberark.com>
2019-03-14 18:44:30 +0200

    Refactored `oidc_secrets` to improve readability

--- app/domain/authentication/authn_oidc/fetch_oidc_secrets.rb
+++ app/domain/authentication/authn_oidc/fetch_oidc_secrets.rb
@@ -4,22 +4,15 @@ require 'conjur/fetch_required_secrets'
 module Authentication
   module AuthnOidc
 
-    # TODO: (later version) Fix CommandClass so we can add errors directly
-    # inside of it
-    #
-    # TODO: list any OIDC or other errors here.  The errors are part of the
-    # API.
-    #
     # Errors from FetchRequiredSecrets
-    #
     RequiredResourceMissing = ::Conjur::RequiredResourceMissing
-    RequiredSecretMissing   = ::Conjur::RequiredSecretMissing
+    RequiredSecretMissing = ::Conjur::RequiredSecretMissing
 
     FetchOidcSecrets = CommandClass.new(
       dependencies: {
         fetch_secrets: ::Conjur::FetchRequiredSecrets.new
       },
-      inputs:       %i(conjur_account service_id required_variable_names)
+      inputs: %i(conjur_account service_id required_variable_names)
     ) do
 
       def call
@@ -28,27 +21,22 @@ module Authentication
 
       private
 
-      def required_secrets
-        @required_secrets ||= @fetch_secrets.(resource_ids: required_resource_ids)
-      end
-
       def required_resource_ids
-        @required_variable_names.map { |var_name| variable_id(var_name) }
+        @required_variable_names.map { |var_name| full_variable_name(var_name) }
       end
 
-      def variable_id(var_name)
+      def full_variable_name(var_name)
         "#{@conjur_account}:variable:conjur/authn-oidc/#{@service_id}/#{var_name}"
-      end
-
-      def secret_value(var_name)
-        required_secrets[variable_id(var_name)]
       end
 
       def oidc_secrets
         secrets = {}
 
+        required_secrets = @fetch_secrets.(resource_ids: required_resource_ids)
+
         @required_variable_names.each do |variable_name|
-          secrets[variable_name] = secret_value(variable_name)
+          full_variable_name = full_variable_name(variable_name)
+          secrets[variable_name] = required_secrets[full_variable_name]
         end
 
         secrets
