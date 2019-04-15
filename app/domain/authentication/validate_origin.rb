97f998fc9693f46ad90a82e7dbd42ac9898a8bbc
orenbm <oren.benmeir@cyberark.com>
2019-04-15 12:31:03 +0300

    Removed redundant get_login_by_id

--- app/domain/authentication/validate_origin.rb
+++ app/domain/authentication/validate_origin.rb
@@ -3,20 +3,19 @@
 module Authentication
   ValidateOrigin = CommandClass.new(
     dependencies: {
-      get_role_by_login: GetRoleByLogin.new
+      role_cls: ::Role
     },
     inputs:       %i(input)
   ) do
 
     def call
-      authn_role = role(@input.username, @input.account)
-      raise InvalidOrigin unless authn_role.valid_origin?(@input.origin)
+      raise InvalidOrigin unless role.valid_origin?(@input.origin)
     end
 
     private
 
-    def role(username, account)
-      @get_role_by_login.(username: username, account: account)
+    def role
+      @role_cls.by_login(@input.username, account: @input.account)
     end
   end
 end
