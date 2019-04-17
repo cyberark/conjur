aeef6d131e87691c710e9309907e76bc92b496c3
orenbm <oren.benmeir@cyberark.com>
2019-04-17 17:02:32 +0300

    Errors are presented in top of command classes

--- app/domain/authentication/authn_k8s/inject_client_cert.rb
+++ app/domain/authentication/authn_k8s/inject_client_cert.rb
@@ -1,141 +1,148 @@
 require 'command_class'
+require 'authentication/errors'
 
 module Authentication
   module AuthnK8s
+    class InjectClientCert
+      extend CommandClass::Include
 
-    InjectClientCert = CommandClass.new(
-      dependencies: {
-        logger: Rails.logger,
-        resource_repo: Resource,
-        conjur_ca_repo: Repos::ConjurCA,
-        kubectl_exec: KubectlExec,
-        validate_pod_request: ValidatePodRequest.new
-      },
-      inputs: %i(conjur_account service_id csr)
-    ) do
+      CSRIsMissingSpiffeId = Authentication::AuthnK8s::CSRIsMissingSpiffeId
+      CSRNamespaceMismatch = Authentication::AuthnK8s::CSRNamespaceMismatch
+      CertInstallationError = Authentication::AuthnK8s::CertInstallationError
 
-      def call
-        validate
-        install_signed_cert
-      end
+      command_class(
+        dependencies: {
+          logger: Rails.logger,
+          resource_repo: Resource,
+          conjur_ca_repo: Repos::ConjurCA,
+          kubectl_exec: KubectlExec,
+          validate_pod_request: ValidatePodRequest.new
+        },
+        inputs: %i(conjur_account service_id csr)
+      ) do
 
-      private
+        def call
+          validate
+          install_signed_cert
+        end
 
-      def validate
-        # We validate the CSR first since the pod_request uses its values
-        validate_csr
+        private
 
-        @validate_pod_request.(pod_request: pod_request)
-      end
+        def validate
+          # We validate the CSR first since the pod_request uses its values
+          validate_csr
 
-      def install_signed_cert
-        pod_namespace = spiffe_id.namespace
-        pod_name = spiffe_id.name
-        @logger.debug "Copying SSL cert to #{pod_namespace}/#{pod_name}"
+          @validate_pod_request.(pod_request: pod_request)
+        end
 
-        resp = @kubectl_exec.new.copy(
-          k8s_object_lookup: k8s_object_lookup,
-          pod_namespace: pod_namespace,
-          pod_name: pod_name,
-          container: container_name,
-          path: "/etc/conjur/ssl/client.pem",
-          content: cert_to_install.to_pem,
-          mode: 0o644
-        )
-        validate_cert_installation(resp)
-      end
+        def install_signed_cert
+          pod_namespace = spiffe_id.namespace
+          pod_name = spiffe_id.name
+          @logger.debug "Copying SSL cert to #{pod_namespace}/#{pod_name}"
 
-      def pod_request
-        PodRequest.new(
-          service_id: @service_id,
-          k8s_host: k8s_host,
-          spiffe_id: spiffe_id
-        )
-      end
+          resp = @kubectl_exec.new.copy(
+            k8s_object_lookup: k8s_object_lookup,
+            pod_namespace: pod_namespace,
+            pod_name: pod_name,
+            container: container_name,
+            path: "/etc/conjur/ssl/client.pem",
+            content: cert_to_install.to_pem,
+            mode: 0o644
+          )
+          validate_cert_installation(resp)
+        end
 
-      def k8s_host
-        @k8s_host ||= Authentication::AuthnK8s::K8sHost.from_csr(
-          account: @conjur_account,
-          service_name: @service_id,
-          csr: @csr
-        )
-      end
+        def pod_request
+          PodRequest.new(
+            service_id: @service_id,
+            k8s_host: k8s_host,
+            spiffe_id: spiffe_id
+          )
+        end
 
-      def host_id
-        k8s_host.conjur_host_id
-      end
+        def k8s_host
+          @k8s_host ||= Authentication::AuthnK8s::K8sHost.from_csr(
+            account: @conjur_account,
+            service_name: @service_id,
+            csr: @csr
+          )
+        end
 
-      def spiffe_id
-        @spiffe_id ||= SpiffeId.new(smart_csr.spiffe_id)
-      end
+        def host_id
+          k8s_host.conjur_host_id
+        end
 
-      def pod
-        @pod ||= k8s_object_lookup.pod_by_name(
-          spiffe_id.name, spiffe_id.namespace
-        )
-      end
+        def spiffe_id
+          @spiffe_id ||= SpiffeId.new(smart_csr.spiffe_id)
+        end
 
-      def host
-        @host ||= @resource_repo[host_id]
-      end
+        def pod
+          @pod ||= k8s_object_lookup.pod_by_name(
+            spiffe_id.name, spiffe_id.namespace
+          )
+        end
 
-      def container_name
-        name = 'kubernetes/authentication-container-name'
-        annotation = host.annotations.find { |a| a.values[:name] == name }
-        annotation[:value] || 'authenticator'
-      end
+        def host
+          @host ||= @resource_repo[host_id]
+        end
 
-      def validate_csr
-        raise Authentication::AuthnK8s::CSRIsMissingSpiffeId unless smart_csr.spiffe_id
+        def container_name
+          name = 'kubernetes/authentication-container-name'
+          annotation = host.annotations.find { |a| a.values[:name] == name }
+          annotation[:value] || 'authenticator'
+        end
 
-        spiffe_namespace = spiffe_id.namespace
-        cn_namespace = common_name.namespace
-        raise Authentication::AuthnK8s::CSRNamespaceMismatch.new(cn_namespace, spiffe_namespace) unless cn_namespace == spiffe_namespace
-      end
+        def validate_csr
+          raise CSRIsMissingSpiffeId unless smart_csr.spiffe_id
 
-      def smart_csr
-        @smart_csr ||= Util::OpenSsl::X509::SmartCsr.new(@csr)
-      end
+          spiffe_namespace = spiffe_id.namespace
+          cn_namespace = common_name.namespace
+          raise CSRNamespaceMismatch.new(cn_namespace, spiffe_namespace) unless cn_namespace == spiffe_namespace
+        end
 
-      def common_name
-        @common_name ||= CommonName.new(smart_csr.common_name)
-      end
+        def smart_csr
+          @smart_csr ||= Util::OpenSsl::X509::SmartCsr.new(@csr)
+        end
 
-      def validate_cert_installation(resp)
-        error_stream = resp[:error]
-        return if error_stream.nil? || error_stream.empty?
-        raise Authentication::AuthnK8s::CertInstallationError, cert_error(error_stream)
-      end
+        def common_name
+          @common_name ||= CommonName.new(smart_csr.common_name)
+        end
 
-      # In case there's a blank error message...
-      def cert_error(msg)
-        return 'The server returned a blank error message' if msg.blank?
-        msg.to_s
-      end
+        def validate_cert_installation(resp)
+          error_stream = resp[:error]
+          return if error_stream.nil? || error_stream.empty?
+          raise CertInstallationError, cert_error(error_stream)
+        end
 
-      def ca_for_webservice
-        @conjur_ca_repo.ca(webservice.resource_id)
-      end
+        # In case there's a blank error message...
+        def cert_error(msg)
+          return 'The server returned a blank error message' if msg.blank?
+          msg.to_s
+        end
 
-      def webservice
-        ::Authentication::Webservice.new(
-          account: @conjur_account,
-          authenticator_name: 'authn-k8s',
-          service_id: @service_id
-        )
-      end
+        def ca_for_webservice
+          @conjur_ca_repo.ca(webservice.resource_id)
+        end
 
-      def k8s_object_lookup
-        @k8s_object_lookup ||= K8sObjectLookup.new(webservice)
-      end
+        def webservice
+          ::Authentication::Webservice.new(
+            account: @conjur_account,
+            authenticator_name: 'authn-k8s',
+            service_id: @service_id
+          )
+        end
 
-      def cert_to_install
-        ca_for_webservice.signed_cert(
-          @csr,
-          subject_altnames: [ spiffe_id.to_altname ]
-        )
+        def cert_to_install
+          ca_for_webservice.signed_cert(
+            @csr,
+            subject_altnames: [spiffe_id.to_altname]
+          )
+        end
+
+        def k8s_object_lookup
+          @k8s_object_lookup ||= K8sObjectLookup.new(webservice)
+        end
       end
     end
-
   end
 end
--- app/domain/authentication/authn_oidc/authenticate_id_token/fetch_provider_certificate.rb
+++ app/domain/authentication/authn_oidc/authenticate_id_token/fetch_provider_certificate.rb
@@ -1,46 +1,55 @@
 require 'uri'
 require 'json'
+require 'authentication/errors'
 
 module Authentication
   module AuthnOidc
     module AuthenticateIdToken
-      FetchProviderCertificate = CommandClass.new(
-        dependencies: {
-          logger: Rails.logger
-        },
-        inputs: %i(provider_uri)
-      ) do
+      class FetchProviderCertificate
+        extend CommandClass::Include
 
-        def call
-          # provider discovery might throw exception. Let it propagate upward
-          discover_provider
-          fetch_certs
-        end
+        ProviderDiscoveryTimeout = Authentication::AuthnOidc::ProviderDiscoveryTimeout
+        ProviderDiscoveryFailed = Authentication::AuthnOidc::ProviderDiscoveryFailed
+        ProviderFetchCertificateFailed = Authentication::AuthnOidc::ProviderFetchCertificateFailed
 
-        private
+        command_class(
+          dependencies: {
+            logger: Rails.logger
+          },
+          inputs: %i(provider_uri)
+        ) do
 
-        def discover_provider
-          @logger.debug("[OIDC] Discovering provider '#{@provider_uri}'")
+          def call
+            # provider discovery might throw exception. Let it propagate upward
+            discover_provider
+            fetch_certs
+          end
 
-          @discovered_provider = OpenIDConnect::Discovery::Provider::Config.discover!(@provider_uri)
-        rescue HTTPClient::ConnectTimeoutError => e
-          raise_error(Authentication::AuthnOidc::ProviderDiscoveryTimeout, e)
-        rescue => e
-          raise_error(Authentication::AuthnOidc::ProviderDiscoveryFailed, e)
-        end
+          private
 
-        def fetch_certs
-          @logger.debug("[OIDC] Fetching provider certificate from '#{@provider_uri}'")
+          def discover_provider
+            @logger.debug("[OIDC] Discovering provider '#{@provider_uri}'")
 
-          jwks = @discovered_provider.jwks
-          @logger.debug("[OIDC] Provider certificate was fetched successfully from '#{@provider_uri}'")
-          jwks
-        rescue => e
-          raise_error(Authentication::AuthnOidc::ProviderFetchCertificateFailed, e)
-        end
+            @discovered_provider = OpenIDConnect::Discovery::Provider::Config.discover!(@provider_uri)
+          rescue HTTPClient::ConnectTimeoutError => e
+            raise_error(ProviderDiscoveryTimeout, e)
+          rescue => e
+            raise_error(ProviderDiscoveryFailed, e)
+          end
 
-        def raise_error(error_class, original_error)
-          raise error_class.new(@provider_uri, original_error.inspect)
+          def fetch_certs
+            @logger.debug("[OIDC] Fetching provider certificate from '#{@provider_uri}'")
+
+            jwks = @discovered_provider.jwks
+            @logger.debug("[OIDC] Provider certificate was fetched successfully from '#{@provider_uri}'")
+            jwks
+          rescue => e
+            raise_error(ProviderFetchCertificateFailed, e)
+          end
+
+          def raise_error(error_class, original_error)
+            raise error_class.new(@provider_uri, original_error.inspect)
+          end
         end
       end
     end
