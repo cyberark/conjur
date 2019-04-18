4bca35eed9e1f0a2cc2f62b3544704c356d39949
orenbm <oren.benmeir@cyberark.com>
2019-04-18 10:30:09 +0300

    caching classes so they won't be loaded multiple times. this is done to fix the issue where errors aren't caught due to rail's auto-loading

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,7 +6,7 @@ Rails.application.configure do
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
-  config.cache_classes = false
+  config.cache_classes = true
 
   # eager_load needed to make authentication work without the hacky
   # loading code...
