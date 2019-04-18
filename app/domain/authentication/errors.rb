c8fcd3a2ddbc19e59b92c1788a96ac9905f23a13
orenbm <oren.benmeir@cyberark.com>
2019-04-18 15:24:08 +0300

    Creating authn errors with error codes

--- app/domain/authentication/errors.rb
+++ app/domain/authentication/errors.rb
@@ -4,166 +4,201 @@ require 'util/error_class'
 
 module Authentication
 
-  AuthenticatorNotFound = ::Util::ErrorClass.new(
-    "'{0}' wasn't in the available authenticators"
+  AuthenticatorNotFound = ::Util::ErrorClassWithCode.new(
+    msg: "'{0}' wasn't in the available authenticators",
+    code: "CONJ00001E"
   )
 
-  InvalidCredentials = ::Util::ErrorClass.new(
-    "Invalid credentials"
+  InvalidCredentials = ::Util::ErrorClassWithCode.new(
+    msg: "Invalid credentials",
+    code: "CONJ00002E"
   )
 
-  InvalidOrigin = ::Util::ErrorClass.new(
-    "Invalid origin"
+  InvalidOrigin = ::Util::ErrorClassWithCode.new(
+    msg: "Invalid origin",
+    code: "CONJ00003E"
   )
 
   module Security
 
-    NotWhitelisted = ::Util::ErrorClass.new(
-      "'{0}' not whitelisted in CONJUR_AUTHENTICATORS"
+    NotWhitelisted = ::Util::ErrorClassWithCode.new(
+      msg: "'{0}' not whitelisted in CONJUR_AUTHENTICATORS",
+      code: "CONJ00004E"
     )
 
-    ServiceNotDefined = ::Util::ErrorClass.new(
-      "Webservice '{0}' is not defined in the Conjur policy"
+    ServiceNotDefined = ::Util::ErrorClassWithCode.new(
+      msg: "Webservice '{0}' is not defined in the Conjur policy",
+      code: "CONJ00005E"
     )
 
-    UserNotAuthorizedInConjur = ::Util::ErrorClass.new(
-      "User '{0}' is not authorized in the Conjur policy"
+    UserNotAuthorizedInConjur = ::Util::ErrorClassWithCode.new(
+      msg: "User '{0}' is not authorized in the Conjur policy",
+      code: "CONJ00006E"
     )
 
-    UserNotDefinedInConjur = ::Util::ErrorClass.new(
-      "User '{0}' is not defined in Conjur"
+    UserNotDefinedInConjur = ::Util::ErrorClassWithCode.new(
+      msg: "User '{0}' is not defined in Conjur",
+      code: "CONJ00007E"
     )
 
-    AccountNotDefined = ::Util::ErrorClass.new(
-      "account '{0}' is not defined in Conjur"
+    AccountNotDefined = ::Util::ErrorClassWithCode.new(
+      msg: "account '{0}' is not defined in Conjur",
+      code: "CONJ00008E"
     )
 
   end
 
   module RequestBody
 
-    MissingRequestParam = ::Util::ErrorClass.new(
-      "field '{0}' is missing or empty in request body"
+    MissingRequestParam = ::Util::ErrorClassWithCode.new(
+      msg: "field '{0}' is missing or empty in request body",
+      code: "CONJ00009E"
     )
 
   end
 
   module AuthnOidc
 
-    ProviderDiscoveryTimeout = ::Util::ErrorClass.new(
-      "OIDC provider discovery failed with timeout error (provider_uri='{0}'). Reason: '{1}'"
+    ProviderDiscoveryTimeout = ::Util::ErrorClassWithCode.new(
+      msg: "OIDC provider discovery failed with timeout error (provider_uri='{0}'). Reason: '{1}'",
+      code: "CONJ00010E"
     )
 
-    ProviderDiscoveryFailed = ::Util::ErrorClass.new(
-      "OIDC provider discovery failed (provider_uri='{0}'). Reason: '{1}'"
+    ProviderDiscoveryFailed = ::Util::ErrorClassWithCode.new(
+      msg: "OIDC provider discovery failed (provider_uri='{0}'). Reason: '{1}'",
+      code: "CONJ00011E"
     )
 
-    ProviderFetchCertificateFailed = ::Util::ErrorClass.new(
-      "Failed to fetch certificate from OIDC provider (provider_uri='{0}'). Reason: '{1}'"
+    ProviderFetchCertificateFailed = ::Util::ErrorClassWithCode.new(
+      msg: "Failed to fetch certificate from OIDC provider (provider_uri='{0}'). Reason: '{1}'",
+      code: "CONJ00012E"
     )
 
-    IdTokenFieldNotFoundOrEmpty = ::Util::ErrorClass.new(
-      "Field '{0}' not found or empty in ID Token"
+    IdTokenFieldNotFoundOrEmpty = ::Util::ErrorClassWithCode.new(
+      msg: "Field '{0}' not found or empty in ID Token",
+      code: "CONJ00013E"
     )
 
-    IdTokenInvalidFormat = ::Util::ErrorClass.new(
-      "Invalid ID Token Format (3rdPartyError ='{0}')"
+    IdTokenInvalidFormat = ::Util::ErrorClassWithCode.new(
+      msg: "Invalid ID Token Format (3rdPartyError ='{0}')",
+      code: "CONJ00014E"
     )
 
-    IdTokenVerifyFailed = ::Util::ErrorClass.new(
-      "ID Token verification failed (3rdPartyError ='{0}')"
+    IdTokenVerifyFailed = ::Util::ErrorClassWithCode.new(
+      msg: "ID Token verification failed (3rdPartyError ='{0}')",
+      code: "CONJ00015E"
     )
 
-    IdTokenExpired = ::Util::ErrorClass.new(
-      "ID Token Expired"
+    IdTokenExpired = ::Util::ErrorClassWithCode.new(
+      msg: "ID Token Expired",
+      code: "CONJ00016E"
     )
 
-    AdminAuthenticationDenied = ::Util::ErrorClass.new(
-      "admin user is not allowed to authenticate with OIDC"
+    AdminAuthenticationDenied = ::Util::ErrorClassWithCode.new(
+      msg: "admin user is not allowed to authenticate with OIDC",
+      code: "CONJ00017E"
     )
 
   end
 
   module AuthnIam
 
-    InvalidAWSHeaders = ::Util::ErrorClass.new(
-      "'Invalid or Expired AWS Headers: {0}"
+    InvalidAWSHeaders = ::Util::ErrorClassWithCode.new(
+      msg: "'Invalid or Expired AWS Headers: {0}",
+      code: "CONJ00018E"
     )
 
   end
 
   module AuthnK8s
 
-    WebserviceNotFound = ::Util::ErrorClass.new(
-      "Webservice '{0}' wasn't found"
+    WebserviceNotFound = ::Util::ErrorClassWithCode.new(
+      msg: "Webservice '{0}' wasn't found",
+      code: "CONJ00019E"
     )
 
-    HostNotFound = ::Util::ErrorClass.new(
-      "Host '{0}' wasn't found"
+    HostNotFound = ::Util::ErrorClassWithCode.new(
+      msg: "Host '{0}' wasn't found",
+      code: "CONJ00020E"
     )
 
-    HostNotAuthorized = ::Util::ErrorClass.new(
-      "'{0}' does not have 'authenticate' privilege on {1}"
+    HostNotAuthorized = ::Util::ErrorClassWithCode.new(
+      msg: "'{0}' does not have 'authenticate' privilege on {1}",
+      code: "CONJ00021E"
     )
 
-    CSRIsMissingSpiffeId = ::Util::ErrorClass.new(
-      'CSR must contain SPIFFE ID SAN'
+    CSRIsMissingSpiffeId = ::Util::ErrorClassWithCode.new(
+      msg: 'CSR must contain SPIFFE ID SAN',
+      code: "CONJ00022E"
     )
 
-    CSRNamespaceMismatch = ::Util::ErrorClass.new(
-      "Namespace in SPIFFE ID '{0}' must match namespace implied by common name ('{1}')"
+    CSRNamespaceMismatch = ::Util::ErrorClassWithCode.new(
+      msg: "Namespace in SPIFFE ID '{0}' must match namespace implied by common name ('{1}')",
+      code: "CONJ00023E"
     )
 
-    PodNotFound = ::Util::ErrorClass.new(
-      "No Pod found for podname '{0}' in namespace '{1}'"
+    PodNotFound = ::Util::ErrorClassWithCode.new(
+      msg: "No Pod found for podname '{0}' in namespace '{1}'",
+      code: "CONJ00024E"
     )
 
-    ScopeNotSupported = ::Util::ErrorClass.new(
-      "Resource type '{0}' identity scope is not supported in this version " +
-        "of authn-k8s"
+    ScopeNotSupported = ::Util::ErrorClassWithCode.new(
+      msg: "Resource type '{0}' identity scope is not supported in this version " +
+        "of authn-k8s",
+      code: "CONJ00025E"
     )
 
-    ControllerNotFound = ::Util::ErrorClass.new(
-      "Kubernetes {0} {1} not found in namespace {2}"
+    ControllerNotFound = ::Util::ErrorClassWithCode.new(
+      msg: "Kubernetes {0} {1} not found in namespace {2}",
+      code: "CONJ00026E"
     )
 
-    CertInstallationError = ::Util::ErrorClass.new(
-      "Cert could not be copied to pod: {0}"
+    CertInstallationError = ::Util::ErrorClassWithCode.new(
+      msg: "Cert could not be copied to pod: {0}",
+      code: "CONJ00027E"
     )
 
-    ContainerNotFound = ::Util::ErrorClass.new(
-      "Container {0} was not found for requesting pod"
+    ContainerNotFound = ::Util::ErrorClassWithCode.new(
+      msg: "Container {0} was not found for requesting pod",
+      code: "CONJ00028E"
     )
 
-    MissingClientCertificate = ::Util::ErrorClass.new(
-      "The client SSL cert is missing from the header"
+    MissingClientCertificate = ::Util::ErrorClassWithCode.new(
+      msg: "The client SSL cert is missing from the header",
+      code: "CONJ00029E"
     )
 
-    UntrustedClientCertificate = ::Util::ErrorClass.new(
-      "Client certificate cannot be verified by certification authority"
+    UntrustedClientCertificate = ::Util::ErrorClassWithCode.new(
+      msg: "Client certificate cannot be verified by certification authority",
+      code: "CONJ00030E"
     )
 
-    CommonNameDoesntMatchHost = ::Util::ErrorClass.new(
-      "Client certificate CN must match host name. Cert CN: {0}. " +
-        "Host name: {1}. "
+    CommonNameDoesntMatchHost = ::Util::ErrorClassWithCode.new(
+      msg: "Client certificate CN must match host name. Cert CN: {0}. " +
+        "Host name: {1}. ",
+      code: "CONJ00031E"
     )
 
-    ClientCertificateExpired = ::Util::ErrorClass.new(
-      "Client certificate expired"
+    ClientCertificateExpired = ::Util::ErrorClassWithCode.new(
+      msg: "Client certificate expired",
+      code: "CONJ00032E"
     )
 
-    CommandTimedOut = ::Util::ErrorClass.new(
-      "Command timed out in container '{0}' of pod '{1}'"
+    CommandTimedOut = ::Util::ErrorClassWithCode.new(
+      msg: "Command timed out in container '{0}' of pod '{1}'",
+      code: "CONJ00033E"
     )
 
     module KubeClientFactory
 
-      MissingServiceAccountDir = ::Util::ErrorClass.new(
-        "Kubernetes serviceaccount dir '{0}' does not exist"
+      MissingServiceAccountDir = ::Util::ErrorClassWithCode.new(
+        msg: "Kubernetes serviceaccount dir '{0}' does not exist",
+        code: "CONJ00034E"
       )
 
-      MissingEnvVar = ::Util::ErrorClass.new(
-        "Expected ENV variable '{0}' is not set"
+      MissingEnvVar = ::Util::ErrorClassWithCode.new(
+        msg: "Expected ENV variable '{0}' is not set",
+        code: "CONJ00035E"
       )
 
     end
