580eb280b5d9ced3c6560a465581dd5006f0b2f7
“Nessi <Nessi.Lahav@cyberark.com>
2019-04-25 12:18:15 +0300

    Refactored logs validations steps to be more readable

--- cucumber/api/features/step_definitions/logs_steps.rb
+++ cucumber/api/features/step_definitions/logs_steps.rb
@@ -1,9 +1,16 @@
 # frozen_string_literal: true
 
+Given(/^I save the log data from bookmark$/) do
+  save_log_data_from_bookmark()
+end
+
 Given(/^I save the log data from bookmark "([^"]*)"$/) do |bookmark|
   save_log_data_from_bookmark(bookmark)
 end
 
+And(/^The log filtered from bookmark contains messages:$/) do |message|
+  expect(occurences_in_log_filtered_from_bookmark(message)).to eq 1
+end
 
 And(/^The log filtered from bookmark "([^"]*)" contains "([^"]*)" messages:$/) do |bookmark, occurrences, message|
   expect(occurences_in_log_filtered_from_bookmark(bookmark, message)).to eq occurrences.to_i
