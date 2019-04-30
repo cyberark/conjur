41885cc2815ea51176ffe6a931f27404ed34298e
orenbm <oren.benmeir@cyberark.com>
2019-04-30 13:34:22 +0300

    reverted usages of command classes to the older version syntax

--- app/domain/authentication/audit_event.rb
+++ app/domain/authentication/audit_event.rb
@@ -1,42 +1,39 @@
 # frozen_string_literal: true
 
 module Authentication
-  class AuditEvent
-    extend CommandClass::Include
 
-    command_class(
-      dependencies: {
-        role_cls: ::Role,
-        audit_log:         ::Authentication::AuditLog
-      },
-      inputs:       %i(input success message)
-    ) do
+  AuditEvent = CommandClass.new(
+    dependencies: {
+      role_cls: ::Role,
+      audit_log: ::Authentication::AuditLog
+    },
+    inputs: %i(input success message)
+  ) do
 
-      def call
-        @audit_log.record_authn_event(
-          role:               role,
-          webservice_id:      @input.webservice.resource_id,
-          authenticator_name: @input.authenticator_name,
-          success:            @success,
-          message:            @message
-        )
-      end
+    def call
+      @audit_log.record_authn_event(
+        role: role,
+        webservice_id: @input.webservice.resource_id,
+        authenticator_name: @input.authenticator_name,
+        success: @success,
+        message: @message
+      )
+    end
 
-      private
+    private
 
-      def role
-        return nil if username.nil?
+    def role
+      return nil if username.nil?
 
-        @role_cls.by_login(username, account: account)
-      end
+      @role_cls.by_login(username, account: account)
+    end
 
-      def username
-        @input.username
-      end
+    def username
+      @input.username
+    end
 
-      def account
-        @input.account
-      end
+    def account
+      @input.account
     end
   end
 end
--- app/domain/authentication/authn_oidc/authenticate_oidc_conjur_token/validate_and_decrypt_oidc_conjur_token.rb
+++ app/domain/authentication/authn_oidc/authenticate_oidc_conjur_token/validate_and_decrypt_oidc_conjur_token.rb
@@ -12,45 +12,42 @@ module Authentication
       # - Validate input
       # - Validate JWT signing token
       # - Decrypt ID token
-      class ValidateAndDecryptOidcConjurToken
-        extend CommandClass::Include
 
-        command_class(
-          dependencies: {},
-          inputs: %i(request_body)
-        ) do
+      ValidateAndDecryptOidcConjurToken = CommandClass.new(
+        dependencies: {},
+        inputs: %i(request_body)
+      ) do
 
-          def call
-            oidc_conjur_token
-            # validate_signing
-            # decrypt_token
-          end
+        def call
+          oidc_conjur_token
+          # validate_signing
+          # decrypt_token
+        end
 
-          private
+        private
 
-          def oidc_conjur_token
-            ::Authentication::AuthnOidc::ConjurToken.new(
-              id_token_encrypted: id_token_encrypted,
-              user_name: user_name,
-              expiration_time: expiration_time
-            )
-          end
+        def oidc_conjur_token
+          ::Authentication::AuthnOidc::ConjurToken.new(
+            id_token_encrypted: id_token_encrypted,
+            user_name: user_name,
+            expiration_time: expiration_time
+          )
+        end
 
-          def decoded_body
-            @decoded_body ||= URI.decode_www_form(@request_body)
-          end
+        def decoded_body
+          @decoded_body ||= URI.decode_www_form(@request_body)
+        end
 
-          def id_token_encrypted
-            @id_token_encrypted ||= decoded_body.assoc('id_token_encrypted').last
-          end
+        def id_token_encrypted
+          @id_token_encrypted ||= decoded_body.assoc('id_token_encrypted').last
+        end
 
-          def user_name
-            @user_name ||= decoded_body.assoc('user_name').last
-          end
+        def user_name
+          @user_name ||= decoded_body.assoc('user_name').last
+        end
 
-          def expiration_time
-            @expiration_time ||= decoded_body.assoc('expiration_time').last
-          end
+        def expiration_time
+          @expiration_time ||= decoded_body.assoc('expiration_time').last
         end
       end
     end
