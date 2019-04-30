f6514ea28940ac1344f0e041d94a36f367e77694
orenbm <oren.benmeir@cyberark.com>
2019-04-30 14:26:31 +0300

    explicitly calling error classes

--- app/domain/authentication/authenticate.rb
+++ app/domain/authentication/authenticate.rb
@@ -38,11 +38,11 @@ module Authentication
     end
 
     def validate_authenticator_exists
-      raise AuthenticatorNotFound, @authenticator_input.authenticator_name unless authenticator
+      raise Authentication::AuthenticatorNotFound, @authenticator_input.authenticator_name unless authenticator
     end
 
     def validate_credentials
-      raise InvalidCredentials unless authenticator.valid?(@authenticator_input)
+      raise Authentication::InvalidCredentials unless authenticator.valid?(@authenticator_input)
     end
 
     def validate_security
