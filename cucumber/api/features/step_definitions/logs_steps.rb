c761a9abc8e9f3c60a8640f0c882aeecff64a37e
“Nessi <Nessi.Lahav@cyberark.com>
2019-05-02 13:50:16 +0300

    Fixes according to PR comments - improve the bookmark approach

--- cucumber/api/features/step_definitions/logs_steps.rb
+++ cucumber/api/features/step_definitions/logs_steps.rb
@@ -1,17 +1,13 @@
 # frozen_string_literal: true
 
 Given(/^I save the log data from bookmark$/) do
-  lines_amount_in_bookmark()
-end
-
-Given(/^I save the log data from bookmark "([^"]*)"$/) do |bookmark|
-  lines_amount_in_bookmark(bookmark)
+  save_num_log_lines()
 end
 
 And(/^The log filtered from bookmark contains messages:$/) do |message|
-  expect(occurences_in_log_filtered_from_bookmark(message)).to eq 1
+  expect(num_matches_since_savepoint(message)).to eq 1
 end
 
-And(/^The log filtered from bookmark "([^"]*)" contains "([^"]*)" messages:$/) do |bookmark, occurrences, message|
-  expect(occurences_in_log_filtered_from_bookmark(bookmark, message)).to eq occurrences.to_i
+And(/^The log filtered from bookmark "([^"]*)" contains "([^"]*)" messages:$/) do |occurrences, message|
+  expect(num_matches_since_savepoint(message)).to eq occurrences.to_i
 end
--- cucumber/api/features/support/logs_helpers.rb
+++ cucumber/api/features/support/logs_helpers.rb
@@ -2,34 +2,44 @@
 
 # Utility methods for Logs steps
 #
-require 'open3'
-
+# In order to be able validating messages in the conjur server log,
+# we chose to filter the log by save points instead of zeroing out the log,
+# due to the following reasons:
+# 1. save the log content for troubleshooting
+# 2. be able to parallelize the tests in the future
+#
 module LogsHelpers
   LOG_LOCATION = "/src/conjur-server/log/development.log"
 
-  def lines_amount_in_bookmark(bookmark = "bookmark")
-    @bookmarks ||= Hash.new
-
-    lines_cnt = File.new(LOG_LOCATION).readlines.size
-    raise "Log lines amount '#{lines_cnt}' should be a positive number" unless lines_cnt > 0
-
-    @bookmarks[bookmark] = lines_cnt
+  def num_log_lines
+    File.new(LOG_LOCATION).readlines.size
   end
 
-  def occurences_in_log_filtered_from_bookmark(bookmark = "bookmark", msg)
-    raise "Bookmark #{bookmark} doesn't exists" unless @bookmarks[bookmark].present?
+  # NOTE: Client code should explicitly call this to save current log size,
+  # rather than making it a side affect of getting the current size
+  def save_num_log_lines
+    @saved_num_lines = num_log_lines
+  end
 
-    lines_cnt = File.new(LOG_LOCATION).readlines.size
-    raise "Log lines amount '#{lines_cnt}' should be a positive number" unless lines_cnt > 0
-    raise "Current logs lines amount '#{lines_cnt}' is smaller then bookmark '#{bookmark}' value '#{@bookmarks[bookmark]}'" unless lines_cnt >= @bookmarks[bookmark]
+  def num_matches_since_savepoint(msg)
+    cur_num_lines = num_log_lines
 
-    num_lines_matching(start_line: @bookmarks[bookmark], end_line: lines_cnt, text: msg)
+    validate_savepoint_exists
+    validate_log_didnt_shrink(cur_num_lines)
+
+    start_line, end_line = @saved_num_lines, cur_num_lines
+    (File.readlines(LOG_LOCATION)[start_line..end_line].grep(/#{msg}/i)).count
   end
 
   private
 
-  def num_lines_matching(start_line:, end_line:, text:)
-    (File.readlines(LOG_LOCATION)[start_line..end_line].grep /#{text}/i).count
+  def validate_savepoint_exists
+    raise "No savepoint exists.  'save_num_log_lines' must be called before this method" unless @saved_num_lines
+  end
+
+  def validate_log_didnt_shrink(cur_num_lines)
+    return if cur_num_lines >= @saved_num_lines
+    raise "The log has fewer lines (#{cur_num_lines}) than it did before (#{@saved_num_lines})"
   end
 end
 
