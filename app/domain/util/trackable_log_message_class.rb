54b08e47469800dec390e1df68b682195dbc9eb9
orenbm <oren.benmeir@cyberark.com>
2019-05-02 13:35:15 +0300

    refactored LogMessageWithCode & ErrorClassWithCode class name

--- app/domain/util/trackable_log_message_class.rb
+++ app/domain/util/trackable_log_message_class.rb
@@ -5,7 +5,7 @@ require 'util/log_message_class'
 # A factory for creating a LogMessage with a code prefix
 #
 module Util
-  class LogMessageWithCodeClass
+  class TrackableLogMessageClass
     def self.new(msg:, code:)
       LogMessageClass.new("#{code} #{msg}")
     end
