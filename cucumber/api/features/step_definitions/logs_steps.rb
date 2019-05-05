9ca929145ea5ebd9e955ab772a4033aeea7128d7
“Nessi <Nessi.Lahav@cyberark.com>
2019-05-05 09:45:41 +0300

    Fixes according to PR comments

--- cucumber/api/features/step_definitions/logs_steps.rb
+++ cucumber/api/features/step_definitions/logs_steps.rb
@@ -1,13 +1,9 @@
 # frozen_string_literal: true
 
-Given(/^I save the log data from bookmark$/) do
+Given(/^I save my place in the log file$/) do
   save_num_log_lines()
 end
 
-And(/^The log filtered from bookmark contains messages:$/) do |message|
+And(/^The following appears in the log after my savepoint:$/) do |message|
   expect(num_matches_since_savepoint(message)).to eq 1
-end
-
-And(/^The log filtered from bookmark "([^"]*)" contains "([^"]*)" messages:$/) do |occurrences, message|
-  expect(num_matches_since_savepoint(message)).to eq occurrences.to_i
 end
