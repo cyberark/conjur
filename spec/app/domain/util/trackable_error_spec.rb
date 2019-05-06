4f337629ab672dd15f4892adc162a41c7867fa24
orenbm <oren.benmeir@cyberark.com>
2019-05-06 13:30:42 +0300

    - TrackableErrorClass composes ErrorClass instead of inheritance - Added UTs for ErrorClass & LogMessageClass

---
+++ spec/app/domain/util/trackable_error_spec.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe 'Util::TrackableErrorClass' do
+  context 'object' do
+    let(:error_code) { "ABC123" }
+    let(:error_message) { "An error occured" }
+
+    let(:trackable_error_message) { "#{error_code} #{error_message}" }
+
+    subject(:trackable_error_class) {
+      Util::TrackableErrorClass.new(
+        msg: error_message,
+        code: error_code
+      )
+    }
+
+    it 'has the expected messaged error' do
+      expect { raise trackable_error_class.new }.to raise_error(trackable_error_message)
+    end
+  end
+end
