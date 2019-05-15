3a6b5b99caecd65ee6f733b1567d2472e98423a5
Jonah <jonahx@gmail.com>
2019-05-14 20:42:11 -0400

    Introduce RateLimitedCache. Should still work.

--- app/domain/authentication/authn_oidc/authenticate_id_token/fetch_provider_certificate.rb
+++ app/domain/authentication/authn_oidc/authenticate_id_token/fetch_provider_certificate.rb
@@ -8,36 +8,27 @@ module Authentication
       Log = LogMessages::Authentication::AuthnOidc
       Err = Errors::Authentication::AuthnOidc
       # Possible Errors Raised:
-      # ProviderDiscoveryTimeout, ProviderDiscoveryFailed, ProviderFetchCertificateFailed
+      #   ProviderDiscoveryTimeout
+      #   ProviderDiscoveryFailed
+      #   ProviderFetchCertificateFailed
 
       FetchProviderCertificate = CommandClass.new(
         dependencies: {
           logger: Rails.logger,
-          cert_cache: lambda {::Authentication::AuthnOidc::CertificateCache.instance}
+          open_id_discovery_service: OpenIDConnect::Discovery::Provider::Config
         },
-        inputs: %i(provider_uri force_read_from_provider)
+        inputs: %i(provider_uri)
       ) do
 
         def call
-          @logger.debug(Log::OIDCProviderUri.new(@provider_uri).to_s)
-
-          # provider discovery might throw exception. Let it propagate upward
-          fetch_cert_via_cache
+          discover_provider
+          fetch_certs
         end
 
         private
 
-        def fetch_cert_via_cache
-          cert_cache_instance = @cert_cache.call
-          cert_cache_instance.fetch @provider_uri, @force_read_from_provider do
-            discover_provider
-            fetch_certs
-          end
-        end
-
         def discover_provider
-          @discovered_provider = OpenIDConnect::Discovery::Provider::Config.discover!(@provider_uri)
-
+          @discovered_provider = @open_id_discovery_service.discover!(@provider_uri)
           @logger.debug(Log::OIDCProviderDiscoverySuccess.new.to_s)
         rescue HTTPClient::ConnectTimeoutError => e
           raise_error(Err::ProviderDiscoveryTimeout, e)
@@ -46,9 +37,9 @@ module Authentication
         end
 
         def fetch_certs
-          jwks = @discovered_provider.jwks
-          @logger.debug(Log::FetchProviderCertsSuccess.new.to_s)
-          jwks
+          @discovered_provider.jwks.tap do
+            @logger.debug(Log::FetchProviderCertsSuccess.new.to_s)
+          end
         rescue => e
           raise_error(Err::ProviderFetchCertificateFailed, e)
         end
