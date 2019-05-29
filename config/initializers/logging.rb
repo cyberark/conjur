d319f8677d384da28fc26847d1528e905e7e29fc
orenbm <oren.benmeir@cyberark.com>
2019-05-29 13:16:16 +0300

    fixed tid & pid

--- config/initializers/logging.rb
+++ config/initializers/logging.rb
@@ -1,10 +1,8 @@
 Rails.application.configure do
 
   config.log_tags = [
-    "pid:#{$$}",
-    "tid:#{Thread.current.object_id}",
-    lambda { |request| "origin:#{request.ip}" },
-    lambda { |request| "req_id:#{request.uuid}" }
+    lambda { |request| "origin=#{request.ip}" },
+    lambda { |request| "req_id=#{request.uuid}" }
   ]
 
 end
