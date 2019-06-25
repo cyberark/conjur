6861ca8dbb2e58a074051bf9545d8ed619810b72
Alan Potter <alanjpotter@gmail.com>
2019-06-25 06:46:46 -0400

    Update validate_security_spec per code review
    * Improve self documentation of +user_role+ by changing +is_authorized+
    into a kwarg
    
    * Change "role lookups are not cached across validations" example so
    it uses the injected role and resource classes, rather than using
    explicit references to +Role+ and +Resource+

--- spec/app/domain/authentication/validate_security_spec.rb
+++ spec/app/domain/authentication/validate_security_spec.rb
@@ -17,7 +17,7 @@ RSpec.describe Authentication::Security::ValidateSecurity do
   end
 
   # generates user_role authorized for all or no services
-  def user_role(is_authorized)
+  def user_role(is_authorized:)
     double('user_role').tap do |role|
       allow(role).to receive(:allowed_to?).and_return(is_authorized)
     end
@@ -32,24 +32,18 @@ RSpec.describe Authentication::Security::ValidateSecurity do
     end
   end
 
-  def role_class_instance(returned_role)
-    double('role_class_instance').tap do |inst|
-      allow(inst).to receive(:roleid_from_username).and_return('some-role-id')
-      allow(inst).to receive(:[]).and_return(returned_role)
-      
-      allow(inst).to receive(:[])
-        .with(/#{test_account}:user:admin/)
-        .and_return(user_role(true))
-      
-      allow(inst).to receive(:[])
-        .with(/#{non_existing_account}:user:admin/)
-        .and_return(nil)
-    end
-  end
-  
   def role_class(returned_role)
     double('role_class').tap do |role|
-      allow(role).to receive(:new).and_return(role_class_instance(returned_role))
+      allow(role).to receive(:roleid_from_username).and_return('some-role-id')
+      allow(role).to receive(:[]).and_return(returned_role)
+      
+      allow(role).to receive(:[])
+        .with(/#{test_account}:user:admin/)
+        .and_return(user_role(is_authorized: true))
+      
+      allow(role).to receive(:[])
+        .with(/#{non_existing_account}:user:admin/)
+        .and_return(nil)
     end
   end
 
@@ -70,19 +64,13 @@ RSpec.describe Authentication::Security::ValidateSecurity do
     end
   end
 
-  let(:random_authenticator_mock) do
-    double('authenticator').tap do |authenticator|
-      allow(authenticator).to receive(:authenticator_name).and_return(fake_authenticator_name)
-    end
-  end
-
   let (:full_access_resource_class) { resource_class('some random resource') }
   let (:no_access_resource_class) { resource_class(nil) }
 
   let (:nil_user_role_class) { role_class(nil) }
   let (:non_existing_account_role_class) { role_class(nil) }
-  let (:full_access_role_class) { role_class(user_role(true)) }
-  let (:no_access_role_class) { role_class(user_role(false)) }
+  let (:full_access_role_class) { role_class(user_role(is_authorized: true)) }
+  let (:no_access_role_class) { role_class(user_role(is_authorized: false)) }
 
   context "A whitelisted, authorized webservice and authorized user" do
     subject do
@@ -257,39 +245,45 @@ RSpec.describe Authentication::Security::ValidateSecurity do
     end
   end
 
-  context "with the default role class" do
-    let(:admin_roleid) { [test_account, 'user', 'admin'].join(':') }
-    let(:admin_role_double) { double('admin_role_double') }
-
-    let(:ws_name) { 'service1' }
-    let(:ws_id) { ['conjur', fake_authenticator_name, ws_name].join('/') }
-    let(:ws_resourceid) { [test_account, 'webservice', ws_id].join(':') }
-    let(:ws_double) { double('ws_resource_double') }
-    
+  context "when validating the same role a second time" do
     let(:user_id) { 'some-user' }
     let(:user_roleid) { [test_account, 'user', user_id].join(':') }
-    let(:user_role_double) { double('user_role_double') }
+    let(:user_role_double) {
+      double('user_role_double').tap { |ur|
+        allow(ur).to receive(:allowed_to?).and_return(true)
+      }
+    }
 
-    before do 
-      allow(Role).to receive(:[]).with(admin_roleid).and_return(admin_role_double)
-      allow(Role).to receive(:roleid_from_username).and_return(user_roleid)
-      
-      allow(Resource).to receive(:[]).with(ws_resourceid).and_return(ws_double)
+    # We can't use the double returned by the +role_class+ function
+    # above, because it doesn't constrain the arguments to +:[]+.
+    let(:role_class) {
+      double('role_class').tap {|rc|
+        allow(rc).to receive(:roleid_from_username).and_return(user_roleid)
+        allow(rc).to receive(:[])
+          .with("#{test_account}:user:admin")
+          .and_return(user_role(is_authorized: true))
+      }
+    }
 
-      allow(user_role_double).to receive(:allowed_to?).with('authenticate', ws_double).and_return(true)
+    subject do
+      described_class
+        .new(
+          role_class: role_class,
+          webservice_resource_class: full_access_resource_class
+        )
     end
     
-    it "role lookups are not cached across validations" do
+    it "role lookups are not cached" do
 
       # Simulate two validations. For the first, the role should not
       # be found.
-      allow(Role).to receive(:[]).with(user_roleid).and_return(nil)
+      allow(role_class).to receive(:[]).with(user_roleid).and_return(nil)
       expect { subject.(
-                   webservice: webservice(ws_name),
-                   account: test_account,
-                   user_id: user_id,
-                   enabled_authenticators: two_authenticator_env
-                 )
+                 webservice: webservice('service1'),
+                 account: test_account,
+                 user_id: user_id,
+                 enabled_authenticators: two_authenticator_env
+               )
       }.to raise_error(Errors::Authentication::Security::UserNotDefinedInConjur)
 
       # For the second, the role should be found, and validation
@@ -297,14 +291,13 @@ RSpec.describe Authentication::Security::ValidateSecurity do
 
       # Note that, because the arguments are the same, this +allow+
       # overwrites the previous one.
-      allow(Role).to receive(:[]).with(user_roleid).and_return(user_role_double)
-      
+      allow(role_class).to receive(:[]).with(user_roleid).and_return(user_role_double)
       expect { subject.(
-                   webservice: webservice(ws_name),
-                   account: test_account,
-                   user_id: user_id,
-                   enabled_authenticators: two_authenticator_env
-                 )
+                 webservice: webservice('service1'),
+                 account: test_account,
+                 user_id: user_id,
+                 enabled_authenticators: two_authenticator_env
+               )
       }.not_to raise_error
       
     end
