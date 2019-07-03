58b7181db8cf2f38a1a58694431b4feb52228921
orenbm <oren.benmeir@cyberark.com>
2019-07-03 12:39:09 +0300

    add OIDC status checks

--- app/domain/authentication/authn_oidc/validate_status.rb
+++ app/domain/authentication/authn_oidc/validate_status.rb
@@ -1,17 +1,22 @@
 module Authentication
   module AuthnOidc
 
+    Err = Errors::Authentication::AuthnOidc
+    # Possible Errors Raised:
+    #   ProviderDiscoveryTimeout
+    #   ProviderDiscoveryFailed
+
     ValidateStatus = CommandClass.new(
       dependencies: {
-        fetch_oidc_secrets: AuthnOidc::Util::FetchOidcSecrets.new
+        fetch_oidc_secrets: AuthnOidc::Util::FetchOidcSecrets.new,
+        open_id_discovery_service: OpenIDConnect::Discovery::Provider::Config
       },
       inputs: %i(account service_id)
     ) do
 
       def call
         validate_secrets
-
-        # validate OIDC provider is responsive
+        validate_provider_is_responsive
       end
 
       private
@@ -30,6 +35,22 @@ module Authentication
 
       def required_variable_names
         @required_variable_names ||= %w(provider-uri id-token-user-property)
+      end
+
+      def validate_provider_is_responsive
+        @open_id_discovery_service.discover!(provider_uri)
+      rescue HTTPClient::ConnectTimeoutError => e
+        raise_error(Err::ProviderDiscoveryTimeout, e)
+      rescue => e
+        raise_error(Err::ProviderDiscoveryFailed, e)
+      end
+
+      def raise_error(error_class, original_error)
+        raise error_class.new(provider_uri, original_error.inspect)
+      end
+
+      def provider_uri
+        oidc_secrets["provider-uri"]
       end
     end
   end
