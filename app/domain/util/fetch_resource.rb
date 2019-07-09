04616c618835f27d23131e9be028125bd682ccaa
orenbm <oren.benmeir@cyberark.com>
2019-07-09 12:43:53 +0300

    Code refactors
    - StatusWebservice composes Webservice instead of the other way around
    - Added a `FetchResource` Command Class so Dry::Struct object don't communicate directly with the DB

---
+++ app/domain/util/fetch_resource.rb
@@ -0,0 +1,19 @@
+module Util
+  FetchResource = CommandClass.new(
+    dependencies: {
+      resource_class: ::Resource
+    },
+    inputs: %i(resource_id)
+  ) do
+
+    def call
+      resource
+    end
+
+    private
+
+    def resource
+      @resource_class[@resource_id]
+    end
+  end
+end
