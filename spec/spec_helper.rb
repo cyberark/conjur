1b48033e423512a06000f351eacc441ef8d85603
orenbm <oren.benmeir@cyberark.com>
2019-07-25 10:57:40 +0300

    Set log level to debug in tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,7 @@
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
+ENV["CONJUR_LOG_LEVEL"] ||= 'debug'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 
