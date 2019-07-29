d003bc6d381a003a92a851bc5fc5d3f65dbfdb7e
orenbm <oren.benmeir@cyberark.com>
2019-07-29 17:14:13 +0300

    Refactor `assert_conjur_log_level` to `assert_valid_conjur_log_level` so it is more descriptive

--- config/environment.rb
+++ config/environment.rb
@@ -3,7 +3,7 @@
 LOG_LEVELS = ['debug', 'info', 'warn', 'error', 'fatal', 'unknown']
 LOG_LEVEL_ERROR = "Environment variable 'CONJUR_LOG_LEVEL' must be a valid Rails log level: #{LOG_LEVELS.inspect}"
 
-def assert_conjur_log_level
+def assert_valid_conjur_log_level
   log_level_env = ENV['CONJUR_LOG_LEVEL']
   if log_level_env
     raise LOG_LEVEL_ERROR unless LOG_LEVELS.include? log_level_env
@@ -13,7 +13,7 @@ end
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
-assert_conjur_log_level
+assert_valid_conjur_log_level
 
 # Initialize the Rails application.
 Rails.application.initialize!
