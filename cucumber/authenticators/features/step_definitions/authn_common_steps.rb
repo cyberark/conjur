5d39265d33d6e1f21ef6a0331c99e8b517407165
orenbm <oren.benmeir@cyberark.com>
2019-08-06 10:42:43 +0300

    Not using `.test` TLD for an unreachable hostname so the response is 504

--- cucumber/authenticators/features/step_definitions/authn_common_steps.rb
+++ cucumber/authenticators/features/step_definitions/authn_common_steps.rb
@@ -7,21 +7,21 @@ Then(/^login response token is valid$/) do
 end
 
 Then(/it is a bad request/) do
-  expect(bad_request?).to be true
+  expect(bad_request?).to be(true), "http status is #{http_status}"
 end
 
 Then(/it is unauthorized/) do
-  expect(unauthorized?).to be true
+  expect(unauthorized?).to be(true), "http status is #{http_status}"
 end
 
 Then(/it is forbidden/) do
-  expect(forbidden?).to be true
+  expect(forbidden?).to be(true), "http status is #{http_status}"
 end
 
 Then(/it is gateway timeout/) do
-  expect(gateway_timeout?).to be true
+  expect(gateway_timeout?).to be(true), "http status is #{http_status} & rest client error is #{rest_client_error.class}"
 end
 
 Then(/it is bad gateway/) do
-  expect(bad_gateway?).to be true
+  expect(bad_gateway?).to be(true), "http status is #{http_status}"
 end
