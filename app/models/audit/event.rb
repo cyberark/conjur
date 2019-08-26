f01779162b2a9517c44a5ce1afbe4b43099bc6ab
Dvir Levanon <dvirl@cyberark.com>
2019-08-26 14:09:01 +0300

    add audit to db method

--- app/models/audit/event.rb
+++ app/models/audit/event.rb
@@ -10,8 +10,22 @@ module Audit
     progname 'conjur'
 
     def log_to logger
+      log_to_db
       logger.log logger_severity, self, progname
     end
+
+    def log_to_db
+      ConjurAudit::Message.create({facility: facility,
+                                   severity: severity,
+                                   timestamp: Time.now,
+                                   hostname: Socket.gethostname,
+                                   appname: progname,
+                                   procid: "procid",
+                                   msgid: message_id,
+                                   sdata: Sequel.pg_jsonb(structured_data),
+                                   message: message})
+    end
+
 
     def to_s
       message
