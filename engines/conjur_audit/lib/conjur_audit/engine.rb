a40c5ecf599a9b06bcaa77e0b1a8f8293749c394
Dvir Levanon <50704381+uCatu@users.noreply.github.com>
2019-10-10 17:51:04 +0300

    Revert "Remove redundant env variable AUDIT_DATABASE_URL"
    This reverts commit de3157b60da8cfe07de61fdeb2dc99275575fb4f.

--- engines/conjur_audit/lib/conjur_audit/engine.rb
+++ engines/conjur_audit/lib/conjur_audit/engine.rb
@@ -3,10 +3,11 @@
 module ConjurAudit
   class Engine < ::Rails::Engine
     isolate_namespace ConjurAudit
+    config.audit_database = ENV['AUDIT_DATABASE_URL']
 
     initializer :connect_audit_database do
-      if config.try(:audit_database)
-        db = Sequel.connect config.audit_database
+      if (db = config.audit_database)
+        db = Sequel.connect db
         db.extension :pg_json
         Message.db.extension :pg_json
         Message.set_dataset db[:messages]
