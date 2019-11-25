fe894041fd418fed4c559b786c818925c5c4b79a
Srdjan Grubor <sgnn7@sgnn7.org>
2019-11-25 09:07:16 -0600

    Added flattening to conjur-oss image
    This will reduce vulnerability profile and reduce image size.

--- build.sh
+++ build.sh
@@ -19,8 +19,30 @@ esac
 shift # past argument or value
 done
 
+# Flatten resulting image.
+function flatten() {
+  local image="$1"
+  echo "Flattening image '$image'..."
+
+  # Since `--squash` is still experimental, we have to flatten the image
+  # by exporting and importing a container based on the source image. By
+  # doing this though, we lose a lot of the Dockerfile variables that are
+  # required for running the image (ENV, EXPOSE, WORKDIR, etc) so we
+  # manually rebuild them.
+  # See here for more details: https://github.com/moby/moby/issues/8334
+  local container=`docker create $image`
+  docker export $container | docker import \
+    --change "EXPOSE 80" \
+    --change "ENV RAILS_ENV=production" \
+    --change "WORKDIR /opt/conjur-server" \
+    --change 'ENTRYPOINT ["conjurctl"]' \
+    - $image
+  docker rm $container
+}
+
 echo "Building image conjur:$TAG"
 docker build -t "conjur:$TAG" .
+flatten "conjur:$TAG"
 
 echo "Building image conjur-test:$TAG container"
 docker build --build-arg "VERSION=$TAG" -t "conjur-test:$TAG" -f Dockerfile.test .
