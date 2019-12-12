b9601ea9ed79b12b924e5c88a3ec7af471697221
Micah Lee <micah.lee@cyberark.com>
2019-12-12 14:19:49 -0500

    Make `service_id` path parameter optional for authenticator status

--- config/routes.rb
+++ config/routes.rb
@@ -21,7 +21,7 @@ Rails.application.routes.draw do
 
     constraints account: /[^\/\?]+/ do
       constraints authenticator: /authn-?[^\/]*/, id: /[^\/\?]+/ do
-        get '/:authenticator/:service_id/:account/status' => 'authenticate#status'
+        get '/:authenticator(/:service_id)/:account/status' => 'authenticate#status'
 
         get '/:authenticator(/:service_id)/:account/login' => 'authenticate#login'
         # authn-oidc login & authenticate are currently for future use only
