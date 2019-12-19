e14b6f261d989a27326134f932256cd9e1e3408f
John Tuttle <jtuttle@conjur.net>
2019-12-19 13:38:22 -0500

    Fix authenticator config cukes
    The dependency being used to get the configured authenticator list was being
    cached when the class was loaded and had the incorrect value when the dependency
    actually needed to be evaluated.

--- app/domain/authentication/security/validate_webservice_is_authenticator.rb
+++ app/domain/authentication/security/validate_webservice_is_authenticator.rb
@@ -2,7 +2,7 @@ module Authentication
   module Security
     ValidateWebserviceIsAuthenticator = CommandClass.new(
       dependencies: {
-        configured_authenticators: Authentication::InstalledAuthenticators.configured_authenticators
+        installed_authenticators_class: Authentication::InstalledAuthenticators
       },
       inputs: %i(webservice)
     ) do
@@ -15,7 +15,7 @@ module Authentication
 
       def validate_webservice_is_configured_authenticator
         raise Errors::Authentication::AuthenticatorNotFound, webservice_id \
-          unless @configured_authenticators.include?(webservice_id)
+          unless @installed_authenticators_class.configured_authenticators.include?(webservice_id)
       end
 
       def webservice_id
