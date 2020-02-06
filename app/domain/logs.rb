ed335f66003c22ef5100806c41486ab1974988f1
orenbm <oren.benmeir@cyberark.com>
2020-02-06 20:25:14 +0200

    Prepare azure skeleton for development

--- app/domain/logs.rb
+++ app/domain/logs.rb
@@ -133,6 +133,10 @@ unless defined? LogMessages::Authentication::OriginValidated
           code: "CONJ00028D"
         )
       end
+
+      module AuthnAzure
+
+      end
     end
 
     module Util
