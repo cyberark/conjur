206459fda53424768e42fce719aa73cac8cff086
John Tuttle <jtuttle@conjur.net>
2020-02-14 15:58:21 -0500

    Unsatisfactory secret_key_base solution

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-Possum::Application.config.secret_key_base = Object.new.tap do |o|
-  def o.to_str
-    fail "secret_key_base is intentionally not set for this application"
-  end
-end
