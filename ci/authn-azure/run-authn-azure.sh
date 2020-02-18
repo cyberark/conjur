4d156b6e37c600ebc200de182cc041ca5eae9346
orenbm <oren.benmeir@cyberark.com>
2020-02-18 17:10:34 +0200

    Add scripts for authenticating with Azure
    These scripts start a Conjur OSS cluster with the
    local code, loads policies for authn-azure and runs an
    authn-azure request from an Azure VM.

---
+++ ci/authn-azure/run-authn-azure.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -ex
+
+# Get an Azure access token
+azure_access_token=$(curl \
+  'http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=https%3A%2F%2Fmanagement.azure.com%2F' \
+  -H Metadata:true -s | jq -r '.access_token')
+
+# Get an authn-azure Conjur access token for host azure-apps/test-app
+authn_azure_response=$(curl -k -X POST \
+  -H "Content-Type: application/x-www-form-urlencoded" \
+  --data "jwt=$azure_access_token" \
+  https://"$CONJUR_SERVER_DNS":8443/authn-azure/test/cucumber/host%2Fazure-apps%2Ftest-app/authenticate)
+authn_azure_access_token=$(echo -n "$authn_azure_response" | base64 | tr -d '\r\n')
+
+# Retrieve a Conjur secret using the authn-azure Conjur access token
+curl -k -H "Authorization: Token token=\"$authn_azure_access_token\"" \
+  https://"$CONJUR_SERVER_DNS":8443/secrets/cucumber/variable/secrets/test-variable
