c87df9edd61506bafd009ed125c31fe5c7ceba9c
orenbm <oren.benmeir@cyberark.com>
2020-02-19 13:38:40 +0200

    Add some echoes to authn_azure script

--- ci/authn-azure/run-authn-azure.sh
+++ ci/authn-azure/run-authn-azure.sh
@@ -1,10 +1,12 @@
 #!/bin/bash -ex
 
+echo "Retieving Azure access token"
 # Get an Azure access token
 azure_access_token=$(curl \
   'http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=https%3A%2F%2Fmanagement.azure.com%2F' \
   -H Metadata:true -s | jq -r '.access_token')
 
+echo "Get Conjur access token using an Azure access token"
 # Get an authn-azure Conjur access token for host azure-apps/test-app
 authn_azure_response=$(curl -k -X POST \
   -H "Content-Type: application/x-www-form-urlencoded" \
@@ -12,6 +14,9 @@ authn_azure_response=$(curl -k -X POST \
   https://"$CONJUR_SERVER_DNS":8443/authn-azure/test/cucumber/host%2Fazure-apps%2Ftest-app/authenticate)
 authn_azure_access_token=$(echo -n "$authn_azure_response" | base64 | tr -d '\r\n')
 
+echo "Retrieve a secret using the Conjur access token"
 # Retrieve a Conjur secret using the authn-azure Conjur access token
-curl -k -H "Authorization: Token token=\"$authn_azure_access_token\"" \
-  https://"$CONJUR_SERVER_DNS":8443/secrets/cucumber/variable/secrets/test-variable
+secret=$(curl -k -H "Authorization: Token token=\"$authn_azure_access_token\"" \
+  https://"$CONJUR_SERVER_DNS":8443/secrets/cucumber/variable/secrets/test-variable)
+
+echo "Retrieved secret ${secret} from Conjur!!!"
