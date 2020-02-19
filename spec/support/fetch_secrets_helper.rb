58ab767d43d2ae21aa053796e1075f5ba0b39529
orenbm <oren.benmeir@cyberark.com>
2020-02-19 17:11:04 +0200

    Extract security and secrets specs from oidc
    We would like to use these helpers in other specs than oidc.
    For this we need to extract them into their own helpers.

--- spec/support/fetch_secrets_helper.rb
+++ spec/support/fetch_secrets_helper.rb
@@ -2,7 +2,25 @@
 
 shared_context "fetch secrets" do
 
-  let (:test_fetch_secrets_error) { "test-fetch-secrets-error" }
+  let(:test_fetch_secrets_error) { "test-fetch-secrets-error" }
+
+  let(:mocked_secret) do
+    double('Secret').tap do |secret|
+      allow(secret).to receive(:value).and_return("mocked-secret")
+    end
+  end
+
+  let(:mocked_resource) do
+    double('Resource').tap do |resource|
+      allow(resource).to receive(:secret).and_return(mocked_secret)
+    end
+  end
+
+  let(:resource_without_value) do
+    double('Resource').tap do |resource|
+      allow(resource).to receive(:secret).and_return(nil)
+    end
+  end
 
   def mock_fetch_secrets(is_successful:, fetched_secrets:)
     double('fetch_secrets').tap do |fetch_secrets|
@@ -14,5 +32,23 @@ shared_context "fetch secrets" do
                                   .and_raise(test_fetch_secrets_error)
       end
     end
+  end
+end
+
+shared_examples_for "it fails when variable is missing or has no value" do |variable_name|
+  it "fails when variable is missing" do
+    allow(Resource).to receive(:[])
+                         .with(/#{account}:variable:conjur\/#{authenticator_name}\/#{service}\/#{variable_name}/)
+                         .and_return(nil)
+
+    expect { subject }.to raise_error(Errors::Conjur::RequiredResourceMissing)
+  end
+
+  it "fails when variable has no value" do
+    allow(Resource).to receive(:[])
+                         .with(/#{account}:variable:conjur\/#{authenticator_name}\/#{service}\/#{variable_name}/)
+                         .and_return(resource_without_value)
+
+    expect { subject }.to raise_error(Errors::Conjur::RequiredSecretMissing)
   end
 end
