bdd949b32aa972eb4b6cf8169900c7f59c19c172
orenbm <oren.benmeir@cyberark.com>
2020-03-03 11:53:00 +0200

    Improve xms_mirid parse and validation
    This commit has some improvements:
    1. Parse the xms_mirid separately than using it so it's clearer where
    the error was raised from if it does.
    2. Validate that the "providers" section has 3 parts
    3. Split the general error message to separate ones to enhance supportability

--- app/domain/errors.rb
+++ app/domain/errors.rb
@@ -292,16 +292,25 @@ unless defined? Errors::Authentication::AuthenticatorNotFound
           code: "CONJ00050E"
         )
 
-        ClaimInInvalidFormat = ::Util::TrackableErrorClass.new(
-          msg:  "xms_mirid claim has been received in an invalid format. Reason: {0}",
+        TokenFieldNotFoundOrEmpty = ::Util::TrackableErrorClass.new(
+          msg:  "Field '{0-field-name}' not found or empty in token",
           code: "CONJ00051E"
         )
 
-        TokenFieldNotFoundOrEmpty = ::Util::TrackableErrorClass.new(
-          msg:  "Field '{0-field-name}' not found or empty in token",
+        XmsMiridParseError = ::Util::TrackableErrorClass.new(
+          msg:  "Failed to parse xms_mirid {0}. Reason: {1}",
           code: "CONJ00052E"
         )
 
+        MissingRequiredFieldsInXmsMirid = ::Util::TrackableErrorClass.new(
+          msg:  "Required fields {0} are missing in xms_mirid {1}",
+          code: "CONJ00053E"
+        )
+
+        MissingProviderFieldsInXmsMirid = ::Util::TrackableErrorClass.new(
+          msg:  "Provider fields are missing in xms_mirid {1}",
+          code: "CONJ00054E"
+        )
       end
     end
 
