f63e17c52da7b5d264ddefbe175c1883b6f0305b
Jonah <jonahx@gmail.com>
2020-03-06 15:22:03 -0500

    Don't rely on ENV var for the micah policy fix

--- cucumber/policy/features/support/policy_helpers.rb
+++ cucumber/policy/features/support/policy_helpers.rb
@@ -77,9 +77,11 @@ module PolicyHelpers
   end
 
   def admin_password
-    ENV['CONJUR_MICAH_PW'] || begin
-      raise StandardError.new('Environment variable `CONJUR_MICAH_PW` must be set.')
-    end
+    'SEcret12!!!!'
+    # TODO: see note in hooks
+    # ENV['CONJUR_MICAH_PW'] || begin
+    #   raise StandardError.new('Environment variable `CONJUR_MICAH_PW` must be set.')
+    # end
   end
 
   def login_as_role login, api_key = nil
