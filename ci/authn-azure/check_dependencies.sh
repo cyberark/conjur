11ed3f527720057cc3c5b577c3400f347d2c7353
orenbm <oren.benmeir@cyberark.com>
2020-03-15 11:02:39 +0200

    Azure Authenticator tests can run as part of ci/test
    Add a new flag to the `ci/test` script for running the Azure
    Authenticator tests. We will need some variables in the environment
    in order to run the tests so we added the script check_dependencies.sh
    that verifies all the variables are in the env. This file defines the
    origin of each variable.

---
+++ ci/authn-azure/check_dependencies.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -eo pipefail
+
+check_env_var() {
+  if [[ -z "${!1+x}" ]]; then
+    # where ${var+x} is a parameter expansion which evaluates to nothing if var is unset, and substitutes the string x otherwise.
+    # https://stackoverflow.com/questions/3601515/how-to-check-if-a-variable-is-set-in-bash/13864829#13864829
+    echo "You must set $1 before running these scripts."
+    exit 1
+  fi
+}
+
+# These variables should come from Conjur via summon
+check_env_var "AZURE_TENANT_ID"
+check_env_var "AZURE_SUBSCRIPTION_ID"
+check_env_var "AZURE_RESOURCE_GROUP"
+check_env_var "AZURE_AUTHN_INSTANCE_USERNAME"
+check_env_var "AZURE_AUTHN_INSTANCE_PASSWORD"
+check_env_var "USER_ASSIGNED_IDENTITY"
+
+# These variables should come from Jenkins
+check_env_var "AZURE_AUTHN_INSTANCE_IP"
+#check_env_var "SYSTEM_ASSIGNED_IDENTITY" # TODO: add this once available
