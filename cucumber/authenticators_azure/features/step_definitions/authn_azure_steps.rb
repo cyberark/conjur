a3e0426709c0fe219e233b688bb1855695cb62e7
orenbm <oren.benmeir@cyberark.com>
2020-03-17 14:38:37 +0200

    Add ability to set a specific Azure annotation

--- cucumber/authenticators_azure/features/step_definitions/authn_azure_steps.rb
+++ cucumber/authenticators_azure/features/step_definitions/authn_azure_steps.rb
@@ -29,3 +29,21 @@ Given(/^I set Azure annotations to host "([^"]*)"$/) do |hostname|
   set_annotation_to_resource("authn-azure/subscription-id", azure_subscription_id)
   set_annotation_to_resource("authn-azure/resource-group", azure_resource_group)
 end
+
+Given(/^I set (subscription-id|resource-group|user-assigned-identity) annotation (with incorrect value )?to host "([^"]*)"$/) do |annotation_name, incorrect_value, hostname|
+  i_have_a_resource "host", hostname
+
+  case annotation_name
+  when "subscription-id"
+    annotation_correct_value = azure_subscription_id
+  when "resource-group"
+    annotation_correct_value = azure_resource_group
+  when "user-assigned-identity"
+    annotation_correct_value = user_assigned_identity
+  else
+    raise "incorrect annotation name #{annotation_name}"
+  end
+
+  annotation_value = incorrect_value ? "some-incorrect-value" : annotation_correct_value
+  set_annotation_to_resource("authn-azure/#{annotation_name}", annotation_value)
+end
