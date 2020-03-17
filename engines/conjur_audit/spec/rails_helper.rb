efb9e0879ada7497e2efc0f6e511d8870f7364d1
Micah Lee <micah.lee@cyberark.com>
2020-03-17 12:45:35 -0400

    Update audit specs for Rails 5

--- engines/conjur_audit/spec/rails_helper.rb
+++ engines/conjur_audit/spec/rails_helper.rb
@@ -19,4 +19,5 @@ RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
   config.include_context "engine routes", type: :controller
+  config.include ConjurAudit::Engine.routes.url_helpers
 end
