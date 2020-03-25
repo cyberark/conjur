708c5d956364ad74fed3c04105800d512518963d
Hadar Artzi <hadar.artzi@gmail.com>
2020-03-25 13:51:31 -0400

    remove unused environemnt variable

--- config/initializers/status.rb
+++ config/initializers/status.rb
@@ -1,4 +1,3 @@
 # frozen_string_literal: true
 
-ENV["CONJUR_VERSION_APPLIANCE"] = File.read(File.expand_path("../../VERSION_APPLIANCE", File.dirname(__FILE__)))
 ENV["CONJUR_VERSION_DISPLAY"] = File.read(File.expand_path("../../VERSION", File.dirname(__FILE__)))
