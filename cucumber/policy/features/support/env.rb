3d891c01f026e9c2e8d6d7530e8444bf81b5197b
Jonah <jonahx@gmail.com>
2020-03-26 13:13:13 -0400

    Update secrets cucumber test
    Also:
    - Add step def transforms to the files required in the api profile
    - Simplify path naming

--- cucumber/policy/features/support/env.rb
+++ cucumber/policy/features/support/env.rb
@@ -6,3 +6,7 @@ require 'conjur-api'
 
 Conjur.configuration.appliance_url = ENV['CONJUR_APPLIANCE_URL'] || 'http://conjur'
 Conjur.configuration.account = ENV['CONJUR_ACCOUNT'] || 'cucumber'
+
+# so that we can require relative to the project root
+$LOAD_PATH.unshift File.expand_path '../../../..', __dir__
+require 'config/environment'
