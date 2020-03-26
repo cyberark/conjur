2e43c0db95deafa6de72dc697293301d1b67ca55
Jonah <jonahx@gmail.com>
2020-03-26 12:44:44 -0400

    Reset the DB between each test
    Prior to this commit, our tests had hidden coupling between them.  This
    may be considered a bug fix as well as an improvement.

--- cucumber/policy/features/support/hooks.rb
+++ cucumber/policy/features/support/hooks.rb
@@ -1,9 +1,35 @@
 # frozen_string_literal: true
 
+# Generates random unique names
+#
+require 'haikunator'
+require 'fileutils'
+
 Before do |scenario|
   @scenario_name = scenario.name
 end
 
 Before "@echo" do |scenario|
   @echo = true
+end
+
+
+Before do
+  @user_index = 0
+
+  Role.truncate(cascade: true)
+  Secret.truncate
+  Credentials.truncate
+
+  Slosilo.each do |k,v|
+    unless %w(authn:rspec authn:cucumber).member?(k)
+      Slosilo.send(:keystore).adapter.model[k].delete
+    end
+  end
+  
+  Account.find_or_create_accounts_resource
+  admin_role = Role.create(role_id: "cucumber:user:admin")
+  creds = Credentials.new(role: admin_role)
+  creds.password = ENV['CONJUR_MICAH_PW']
+  creds.save(raise_on_save_failure: true)
 end
