281b2d1da982eace14fd974c74df7e272a72a882
Nahum Cohen <nahum.cohen@cyberark.com>
2020-04-13 12:39:46 +0300

    1. prevent warnings 2. use new slosilo build

--- config/initializers/fips.rb
+++ config/initializers/fips.rb
@@ -1,15 +1,29 @@
+# Supress warning messages.
 require "openssl"
 require "digest"
-require "digest/sha1"
 require "digest/md5"
+require "digest/sha1"
 require 'sprockets'
 require 'openid_connect'
+
+original_verbose, $VERBOSE = $VERBOSE, nil
+# Remove pre-existing constants if they do exist to reduce the
+# amount of log spam and warnings.
+Digest.send(:remove_const, "MD5") if Digest.const_defined?("MD5")
+Digest.const_set("MD5", OpenSSL::Digest::MD5)
+Digest.send(:remove_const, "SHA1") if Digest.const_defined?("SHA1")
+Digest.const_set("SHA1", OpenSSL::Digest::SHA1)
+Digest.send(:remove_const, "SHA256") if Digest.const_defined?("SHA256")
+Digest.const_set("SHA256", OpenSSL::Digest::SHA256)
 
 # override the default Digest with OpenSSL::Digest
 Digest::SHA256 = OpenSSL::Digest::SHA256
 Digest::SHA1 = OpenSSL::Digest::SHA1
+# Activate warning messages again.
+$VERBOSE = original_verbose
 
 OpenSSL.fips_mode = true
+
 ActiveSupport::Digest.hash_digest_class = OpenSSL::Digest::SHA1.new
 Sprockets::DigestUtils.module_eval do
   def digest_class
@@ -30,10 +44,3 @@ OpenIDConnect::Discovery::Provider::Config::Resource.module_eval do
     "swd:resource:opneid-conf:#{sha256}"
   end
 end
-
-# Remove pre-existing constants if they do exist to reduce the
-# amount of log spam and warnings.
-Digest.send(:remove_const, "SHA1") if Digest.const_defined?("SHA1")
-Digest.const_set("SHA1", OpenSSL::Digest::SHA1)
-OpenSSL::Digest.send(:remove_const, "MD5") if OpenSSL::Digest.const_defined?("MD5")
-OpenSSL::Digest.const_set("MD5", Digest::MD5)
