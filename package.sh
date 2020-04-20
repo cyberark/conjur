a2d96fcbd0cea56a11eee2d4ca3ad5a094710d07
Alon Carmel <alon.carmel@cyberark.com>
2020-04-20 22:37:42 +0300

    build dockerfile using rvm

--- package.sh
+++ package.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -ex
-export DEBIFY_IMAGE='registry.tld/conjurinc/debify:1.11'
+export DEBIFY_IMAGE='registry.tld/conjurinc/debify:1.11.4.12-01e0cfd'
 
 docker run --rm $DEBIFY_IMAGE config script > docker-debify
 chmod +x docker-debify
