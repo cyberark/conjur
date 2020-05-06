497873f90b056793b5e33522c647dbdb11dce315
Hadar Artzi <hadar.artzi@gmail.com>
2020-05-06 09:26:22 -0400

    add roles when using conjurctl load policy

--- lib/root_loader.rb
+++ lib/root_loader.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'json'
+
 # BootstrapLoader is used to load an initial "root" policy when the database is completely empty.
 class RootLoader
   class << self
@@ -17,25 +19,60 @@ class RootLoader
         admin = ::Role[admin_id] || ::Role.create(role_id: admin_id)
         if admin_password = ENV['CONJUR_ADMIN_PASSWORD']
           $stderr.puts "Setting 'admin' password"
-          admin_credentials = Credentials[role: admin] || Credentials.create(role: admin)
-          admin_credentials.password = admin_password
-          admin_credentials.save
+          set_admin_password(admin, admin_password)
         end
 
         root_policy_resource = Loader::Types.find_or_create_root_policy(account)
+        policy_ver = policy_version(
+          role: admin, 
+          policy: root_policy_resource, 
+          filename: filename
+        )
 
-        policy_version = PolicyVersion.new role: admin, policy: root_policy_resource, policy_text: File.read(filename)
-        policy_version.policy_filename = filename
-        policy_version.perform_automatic_deletion = true
-        policy_version.delete_permitted = true
-        policy_version.update_permitted = true
-        policy_version.save
+        acc_roles = accepted_roles(policy_ver)
+        created_roles = create_roles(acc_roles)
 
-        loader = Loader::Orchestrate.new policy_version
-        loader.load
+        $stderr.puts(
+          JSON.pretty_generate(
+            created_roles: created_roles,
+            version: policy_ver.version
+          )
+        )
+
+        end_t = Time.now
+        $stderr.puts "Loaded policy in #{end_t - start_t} seconds"
       end
-      end_t = Time.now
-      $stderr.puts "Loaded policy in #{end_t - start_t} seconds"
+    end
+
+    def set_admin_password(admin_role, password)
+      admin_credentials = Credentials[role: admin_role] || Credentials.create(role: admin_role)
+      admin_credentials.password = password
+      admin_credentials.save
+    end
+
+    def create_roles accepted_roles
+      accepted_roles.each_with_object({}) do |role, memo|
+        credentials = Credentials[role: role] || Credentials.create(role: role)
+        role_id = role.id
+        memo[role_id] = { id: role_id, api_key: credentials.api_key }
+      end
+    end
+
+    def accepted_roles policy_version
+      loader = Loader::Orchestrate.new policy_version
+      loader.load
+      loader.new_roles.select do |role|
+        %w(user host).member?(role.kind)
+      end
+    end
+
+    def policy_version role:, policy:, filename:
+      policy_version = PolicyVersion.new role: role, policy: policy, policy_text: File.read(filename)
+      policy_version.policy_filename = filename
+      policy_version.perform_automatic_deletion = true
+      policy_version.delete_permitted = true
+      policy_version.update_permitted = true
+      policy_version.save
     end
   end
 end
