1c19222bf50d74b40062c524748da9878425fe03
Nahum Cohen <nahum.cohen@cyberark.com>
2020-05-11 09:59:19 +0300

    remove Sprockets remove the Chef warning suppressing

--- config/initializers/fips.rb
+++ config/initializers/fips.rb
@@ -1,20 +1,8 @@
 require "openssl"
 require "digest"
-require "digest/md5"
-require "digest/sha1"
-require 'sprockets'
-require 'openid_connect'
 
 # Suppress warning messages
 original_verbose, $VERBOSE = $VERBOSE, nil
-
-# Remove pre-existing constants if they do exist to reduce the amount of log spam and warnings
-Digest.send(:remove_const, "MD5") if Digest.const_defined?("MD5")
-Digest.const_set("MD5", OpenSSL::Digest::MD5)
-Digest.send(:remove_const, "SHA1") if Digest.const_defined?("SHA1")
-Digest.const_set("SHA1", OpenSSL::Digest::SHA1)
-Digest.send(:remove_const, "SHA256") if Digest.const_defined?("SHA256")
-Digest.const_set("SHA256", OpenSSL::Digest::SHA256)
 
 # override the default Digest with OpenSSL::Digest
 Digest::SHA256 = OpenSSL::Digest::SHA256
@@ -33,16 +21,6 @@ OpenSSL.fips_mode = true
 
 # override ActiveSupport hash_digest_class with FIPS complaint method
 ActiveSupport::Digest.hash_digest_class = OpenSSL::Digest::SHA1.new
-
-# override Sprockets digest_class with FIPS complaint method
-Sprockets::DigestUtils.module_eval do
-  def digest_class
-    OpenSSL::Digest::SHA256
-  end
-end
-Sprockets.config = Sprockets.config.merge(
-    digest_class: OpenSSL::Digest::SHA256
-).freeze
 
 # override OpenIDConnect cache_key with FIPS complaint method
 OpenIDConnect::Discovery::Provider::Config::Resource.module_eval do
