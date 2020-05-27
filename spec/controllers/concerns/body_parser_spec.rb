af8d628e5f4812e8080446d3fc51a05215375da4
Micah Lee <micah.lee@cyberark.com>
2020-05-27 16:22:51 -0400

    Update body parser spec to use safe params
    Our body parser spec relied on the ability to use `each` to convert
    `ActionController::Parameters` to a hash. This was removed in version
    5.2.4.3 (https://github.com/rails/rails/blob/v5.2.4.3/actionpack/CHANGELOG.md#rails-5243-may-18-2020)
    
    Instead, we now use permitted parameters and the built in `to_h` to provide
    the params_hash in this spec.

--- spec/controllers/concerns/body_parser_spec.rb
+++ spec/controllers/concerns/body_parser_spec.rb
@@ -46,7 +46,7 @@ describe BodyParser do
 
   describe '#params' do
     it "merges the body params with others" do
-      params_hash = controller.params.each {|x| x }
+      params_hash = controller.params.permit(:get, :id).to_h
       expect(params_hash).to include({'get' => 'params', 'id' => 'foo'})
     end
   end
