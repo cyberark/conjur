56fd0f39fd6d33508817fd0328094d8a533c7e47
Micah Lee <micah.lee@cyberark.com>
2020-06-16 15:47:13 -0400

    Set default content type at start of Rack middleware chain
    This ensures that any subsequent middleware (e.g. Rack::MethodOverride) will
    use the expected content type for that request.

---
+++ config/initializers/rack_middleware.rb
@@ -0,0 +1,30 @@
+require 'rack/default_content_type'
+
+# This is where we introduce custom middleware that interacts with Rack
+# and Rails to change how requests are handled.
+Rails.application.configure do
+
+  # This configures which paths do and do not require token authentication.
+  # Token authentication is optional for authn routes, and it's not applied at
+  # all to authentication, host factories, or static assets (e.g. images, CSS)
+  config.middleware.use(Conjur::Rack::Authenticator,
+  optional: [
+    /^\/authn-[^\/]+\//,
+    /^\/authn\//,
+    /^\/public_keys\//
+  ],
+  except: [
+    /^\/authn-[^\/]+\/.*\/authenticate$/,
+    /^\/authn\/.*\/authenticate$/,
+    /^\/host_factories\/hosts$/,
+    /^\/assets\/.*/,
+    /^\/authenticators$/,
+    /^\/$/
+  ])
+
+  # We want to ensure requests have an expected content type
+  # before other middleware runs to make sure any body parsing
+  # attempts are handled correctly. So we add this middleware
+  # to the start of the Rack middleware chain.
+  config.middleware.insert_before(0, ::Rack::DefaultContentType)
+end
