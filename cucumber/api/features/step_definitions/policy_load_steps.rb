a730b8439964259cd9d5098e5dea1d1e62a45235
Micah Lee <micah.lee@cyberark.com>
2020-06-16 15:47:11 -0400

    Update failing test for special characters in policy
    To verify issue described in
    https://github.com/cyberark/conjur/issues/1621

--- cucumber/api/features/step_definitions/policy_load_steps.rb
+++ cucumber/api/features/step_definitions/policy_load_steps.rb
@@ -1,3 +1,7 @@
+# Let's you reference the global variables such as `$?` using less 
+# cryptic names like `$CHILD_STATUS`
+require 'English'
+
 When(/^I load a large policy with POST$/) do
   # Generate a large policy with 1000 variables, sampled from
   # an example PAS synchronizer policy
@@ -15,4 +19,40 @@ When(/^I load a large policy with POST$/) do
   try_request true do
     post_json path, policy_body
   end
+end
+
+When('I use curl to load a policy with special characters and no content type') do
+  # Policy with special characters, sampled from a PAS synchronizer test policy
+  policy_body = <<~POLICY
+    - !variable
+      id: AccountWithSpecialCharacters~!@#$%^&(){}[]-+=,Name/password
+  POLICY
+
+  auth_token = current_user_credentials.dig(:headers, :authorization)
+
+  url_path = '/policies/cucumber/policy/dev/db'
+
+  # `--no-buffer` allows to close the pipe in popen without getting a curl error
+  # `--output ...` prevents curl from trying to write to stdout, causing an io error
+  command = <<~COMMAND
+    curl --silent \
+      --no-buffer \
+      --fail \
+      --output /dev/null \
+      --header 'Content-Type:' \
+      --header 'Authorization: #{auth_token}' \
+      --data-binary '@-' \
+      #{full_conjur_url(url_path)}
+  COMMAND
+
+  IO.popen(command, "r+") do |pipe|
+    pipe.puts policy_body
+    pipe.close_write
+  end
+
+  @command_result = $CHILD_STATUS.success?
+end
+
+Then('the command is successful') do
+  expect(@command_result).to be(true)
 end
