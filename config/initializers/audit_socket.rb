f38be195c9cdb90a294c0589a094564f959ca170
Jonah <jonahx@gmail.com>
2020-06-24 16:18:40 -0400

    Use Syslog format for audit events in dev too
    This change allows us to write e2e tests for the format of Audit events
    -- and by extension for the Syslog formatter.
    
    It only affects Audit events, so that the rest of our logs will continue
    using the more human-friendly ConjurFormatter.

--- config/initializers/audit_socket.rb
+++ config/initializers/audit_socket.rb
@@ -25,10 +25,27 @@ require 'logger/formatter/rfc5424_formatter'
 #
 #    Formatter docs:
 #    https://ruby-doc.org/stdlib-2.5.1/libdoc/logger/rdoc/Logger.html#class-Logger-label-Format
-if path = Rails.application.config.try(:audit_socket)
+Audit.logger = if path = Rails.application.config.try(:audit_socket)
   # TODO: Add tests for this.  Our CI isn't verifying if we've broken anything.
-  Audit.logger = Audit::Log::SyslogAdapter.new(
+  Audit::Log::SyslogAdapter.new(
     Logger.new(UNIXSocket.open(path)).tap do |logger|
+      logger.formatter = Logger::Formatter::RFC5424Formatter
+    end
+  )
+# We don't want this to affect Conjur OSS production deploys
+elsif Rails.env.development?
+  # NOTE: Use Syslog format for audit events so we can write e2e tests against
+  # what actually appears in the log.  We don't want this to affect our other
+  # logging because the ConjurFormatter produces more human-friendly logs.
+  #
+  # NOTE: We need to use the relative path "./log/development.log"
+  # because in the CI environment (see "ci/docker-compose.yml") the
+  # cucumber and conjur containers mount the log volume at different
+  # absolute paths ("/src" vs "/opt").  Otoh, in LogHelpers::LOG_LOCATION
+  # -- which this code is coupled to -- we can use the absolute
+  # path "/src" since that runs in the cucumber container.
+  Audit::Log::SyslogAdapter.new(
+    Logger.new("./log/development.log").tap do |logger|
       logger.formatter = Logger::Formatter::RFC5424Formatter
     end
   )
