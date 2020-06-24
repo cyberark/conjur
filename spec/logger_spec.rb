ea7ed779e8d3f716710bb3911cfec4c0194f4f23
Jonah <jonahx@gmail.com>
2020-06-24 16:42:35 -0400

    Add unit test for Syslog formatter

--- spec/logger_spec.rb
+++ spec/logger_spec.rb
@@ -3,9 +3,18 @@
 require 'logger/formatter/rfc5424_formatter'
 
 describe Logger::Formatter::RFC5424Formatter do
-  it "can be given facility by object attribute" do
-    msg = double "message", facility: 40
-    expect(formatter.call(3, Time.now, nil, msg)).to start_with '<43>'
+  it "writes the expected log line" do
+    # Notes:
+    #   The "\\d+" represents a process id that changes each run.
+    #   "#{time_str}" avoids time of day / time zone issues.
+    expect(
+      formatter.call("_", known_time, progname, my_event)
+    ).to match(
+      Regexp.new(
+        "<43>1 #{time_str} - conjur \\d+ my_message_id " \
+        '\\[1 key1="11"\\]\\[8 key2="22"\\] my sample message'
+      )
+    )
   end
 
   describe "with structured data" do
@@ -31,4 +40,27 @@ describe Logger::Formatter::RFC5424Formatter do
   end
 
   subject(:formatter) { described_class }
+
+  let(:my_event) {
+    msg = double(
+      "message",
+      facility: 40,
+      message: message,
+      message_id: "my_message_id",
+      progname: progname,
+      severity: 3,
+      structured_data: {
+        1 => { key1: 11 },
+        8 => { key2: 22 }
+      },
+      to_s: message
+    )
+  }
+
+  let (:message) { "my sample message" }
+  let (:progname) { "conjur" }
+
+  # Time.parse('2020-06-22').utc.iso8601(3) => "2020-06-22T04:00:00.000Z"
+  let(:known_time) { Time.parse('2020-06-22') }
+  let(:time_str) { known_time.utc.iso8601(3) }
 end
