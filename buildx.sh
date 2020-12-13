832534c8a5bc864cc6215dddf09d7fc21048299d
Quincy Cheng <quincy.cheng@gmail.com>
2020-12-13 16:40:30 +0800

    arm64 support

--- buildx.sh
+++ buildx.sh
@@ -2,6 +2,6 @@
 
 DOCKER_USER=$1
 
-docker buildx build -t $DOCKER_USER/conjur:latest \
+docker buildx build -f Dockerfile.arm64 -t $DOCKER_USER/conjur:latest \
   --build-arg DOCKER_USER="$DOCKER_USER" \
   --platform linux/amd64,linux/arm64 .  --push
