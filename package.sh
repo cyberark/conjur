f38addfcc54e7aafaa9bb5d7664a0352d327ce74
Tamir Zheleznyak <73157235+tzheleznyak@users.noreply.github.com>
2020-12-17 14:34:03 +0200

    Use debify 2 (#1977)

--- package.sh
+++ package.sh
@@ -1,11 +1,24 @@
 #!/bin/bash -ex
-export DEBIFY_IMAGE='registry.tld/conjurinc/debify:1.11.5'
+export DEBIFY_IMAGE='registry.tld/conjurinc/debify:2.0.0'
 
 docker run --rm $DEBIFY_IMAGE config script > docker-debify
 chmod +x docker-debify
 
+# Create possum deb
 ./docker-debify package \
   --dockerfile=Dockerfile.fpm \
+  --output=deb \
   possum \
   -- \
-  --depends tzdata
+  --depends tzdata \
+
+# Create possum rpm
+# The '--rpm-rpmbuild-define' flag is added to avoid packaging build
+# files that are not needed and cause conflict with conjur-ui on install
+./docker-debify package \
+  --dockerfile=Dockerfile.fpm \
+  --output=rpm \
+  possum \
+  -- \
+  --depends tzdata \
+  --rpm-rpmbuild-define '_build_id_links none'
