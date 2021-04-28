451fb320433f3e6e459d1dc4ec0588184b7337aa
John ODonnell <john.odonnell@cyberark.com>
2021-04-28 09:37:26 -0400

    Add API Version to status page

--- config/initializers/status.rb
+++ config/initializers/status.rb
@@ -3,3 +3,4 @@
 require 'logs'
 
 ENV["CONJUR_VERSION_DISPLAY"] = File.read(File.expand_path("../../VERSION", File.dirname(__FILE__)))
+ENV["API_VERSION"] = File.read(File.expand_path("../../API_VERSION", File.dirname(__FILE__)))
