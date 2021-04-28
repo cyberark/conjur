8cf77e3a03e469612ebfef1d8f4bdc7f3f25b950
Avi Eyal <avi.eyal@cyberark.com>
2021-04-28 15:55:57 +0300

    Add dummy jwt authenticator and configuration factory (#2132)

--- spec/app/domain/authentication/authn_jwt/jwt_configuration_dummy_vendor_spec.rb
+++ spec/app/domain/authentication/authn_jwt/jwt_configuration_dummy_vendor_spec.rb
@@ -3,18 +3,18 @@ require 'spec_helper'
 RSpec.describe(Authentication::AuthnJwt::JWTConfigurationDummyVendor) do
 
   it "return cucumber on get_identity" do
-    dummy_jwt_configuration = ::Authentication::AuthnJwt::JWTConfigurationDummyVendor.new
-    expect(dummy_jwt_configuration.get_identity).to eq "cucumber"
+    dummy_jwt_configuration = ::Authentication::AuthnJwt::JWTConfigurationDummyVendor
+    expect(dummy_jwt_configuration.conjur_id).to eq "cucumber"
   end
 
   it "return true on validate_restrictions" do
-    dummy_jwt_configuration = ::Authentication::AuthnJwt::JWTConfigurationDummyVendor.new
+    dummy_jwt_configuration = ::Authentication::AuthnJwt::JWTConfigurationDummyVendor
     expect(dummy_jwt_configuration.validate_restrictions).to eq true
   end
 
-  it "return true on validate_and_decode" do
-    dummy_jwt_configuration = ::Authentication::AuthnJwt::JWTConfigurationDummyVendor.new
-    expect(dummy_jwt_configuration.validate_and_decode).to eq true
+  it "return true on validate_and_decode_token" do
+    dummy_jwt_configuration = ::Authentication::AuthnJwt::JWTConfigurationDummyVendor
+    expect(dummy_jwt_configuration.validate_and_decode_token("DummyToken")).to eq true
   end
 
 end
