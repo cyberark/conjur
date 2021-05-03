4725a07d8faf20f4c35d8f20d504b4dfcde28f1d
nessil <nessi.lahav@cyberark.com>
2021-05-03 10:26:01 +0300

    Implement fetch mandatory claims to validate logic

--- app/domain/authentication/authn_jwt/consts.rb
+++ app/domain/authentication/authn_jwt/consts.rb
@@ -6,5 +6,9 @@ module Authentication
     JWKS_URI_RESOURCE_NAME = "jwks-uri"
     ISSUER_RESOURCE_NAME = "issuer"
     IDENTITY_FIELD_VARIABLE = "token-app-property"
+    ISS_CLAIM_NAME = "iss"
+    EXP_CLAIM_NAME = "exp"
+    NBF_CLAIM_NAME = "nbf"
+    IAT_CLAIM_NAME = "iat"
   end
 end
