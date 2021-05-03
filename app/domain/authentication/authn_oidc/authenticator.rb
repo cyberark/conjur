dfc7d661b3dda2011030a0373845d05da615d5a0
orenbm <oren.benmeir@cyberark.com>
2021-05-03 16:15:35 +0300

    Verify ID token as part of the OIDC authenticator specific validations
    We actually don't have any specific validations for OIDC. We only verify
    that the ID token is valid but this is done while it is decoded (using
    a third-party). However, we want to verify that we verify the token no
    matter what so we run the validation again (even if it means that in most
    cases we will perform this action twice).

--- app/domain/authentication/authn_oidc/authenticator.rb
+++ app/domain/authentication/authn_oidc/authenticator.rb
@@ -10,12 +10,16 @@ module Authentication
 
       # We actually don't have any specific validations for OIDC. We only verify
       # that the ID token is valid but this is done while it is decoded (using
-      # a third-party). Thus, this method just returns true.
+      # a third-party). However, we want to verify that we verify the token no
+      # matter what so we run the validation again (even if it means that in most
+      # cases we will perform this action twice).
       #
       # The method is still defined because we need `valid?` to exist on the Authenticator
       # class so it is a valid Authenticator class
-      def valid?(_input)
-        true
+      def valid?(input)
+        Authentication::AuthnOidc::UpdateInputWithUsernameFromIdToken.new.(
+          authenticator_input: input
+        )
       end
 
       def status(authenticator_status_input:)
