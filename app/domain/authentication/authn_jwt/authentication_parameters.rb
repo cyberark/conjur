8ec736446136d9eedd61e160c8c0a28a8ce6ad2b
Tamir Zheleznyak <73157235+tzheleznyak@users.noreply.github.com>
2021-05-05 13:43:55 +0300

    Added get identity code (#2139)
    Implement IdentityFromUrlProvider and IdentityFromDecodedTokenProvider to fetch identity of the client requesting the jwt authentication

---
+++ app/domain/authentication/authn_jwt/authentication_parameters.rb
@@ -0,0 +1,23 @@
+module Authentication
+  module AuthnJwt
+    # Data class to store data regarding jwt token that is needed during the jwt authentication process
+    class AuthenticationParameters
+      attr_accessor :jwt_identity, :decoded_token
+      attr_reader :decoded_token, :authenticator_name, :service_id, :account, :username, :credentials, :client_ip, :request
+
+      def initialize(authentication_input)
+        @authenticator_name = authentication_input.authenticator_name
+        @service_id = authentication_input.service_id
+        @account = authentication_input.account
+        @username = authentication_input.username
+        @credentials = authentication_input.credentials
+        @client_ip = authentication_input.client_ip
+        @request = authentication_input.request
+      end
+
+      def authenticator_resource_id
+        "#{@account}:variable:conjur/#{@authenticator_name}/#{@service_id}"
+      end
+    end
+  end
+end
