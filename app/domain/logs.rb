6cb1489dc9bd9644b6e4f01998c578a1a17a70c4
nessil <nessi.lahav@cyberark.com>
2021-05-05 12:59:57 +0300

    Implement fetch mandatory claims to validate logic

--- app/domain/logs.rb
+++ app/domain/logs.rb
@@ -339,23 +339,24 @@ module LogMessages
         msg: "JWT authentication passed successfully",
         code: "CONJ00066D"
       )
-      FetchingJwtMandatoryClaimsToValidate = ::Util::TrackableLogMessageClass.new(
-        msg: "Fetching JWT mandatory claims to validate",
-        code: "CONJ00057D"
+
+      FetchingJwtClaimsToValidate = ::Util::TrackableLogMessageClass.new(
+        msg: "Fetching JWT claims to validate",
+        code: "CONJ00067D"
       )
 
-      FetchedJwtMandatoryClaimsToValidate = ::Util::TrackableLogMessageClass.new(
-        msg: "Fetched JWT mandatory claims to validate",
+      FetchedJwtClaimsToValidate = ::Util::TrackableLogMessageClass.new(
+        msg: "Fetched JWT claims to validate",
         code: "CONJ00068D"
       )
 
-      AddingJwtMandatoryClaimToValidate = ::Util::TrackableLogMessageClass.new(
-        msg: "Adding JWT mandatory claim '{0-claim-name}' to validate",
+      AddingJwtClaimToValidate = ::Util::TrackableLogMessageClass.new(
+        msg: "Adding JWT claim '{0-claim-name}' to validate",
         code: "CONJ00069D"
       )
 
-      ValidatingJwtOptionalClaimToValidate = ::Util::TrackableLogMessageClass.new(
-        msg: "Validating if JWT optional claim '{0-claim-name}' is mandatory to validate",
+      CheckingJwtClaimToValidate = ::Util::TrackableLogMessageClass.new(
+        msg: "Checking if JWT claim '{0-claim-name}' is mandatory to validate",
         code: "CONJ00070D"
       )
 
