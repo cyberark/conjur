fdc34f09750413a85f1d7466ac590d2ae45a6aa3
Tamir Zheleznyak <73157235+tzheleznyak@users.noreply.github.com>
2021-05-05 13:43:55 +0300

    Added get identity code (#2139)
    Implement IdentityFromUrlProvider and IdentityFromDecodedTokenProvider to fetch identity of the client requesting the jwt authentication

--- app/domain/authentication/authn_jwt/consts.rb
+++ app/domain/authentication/authn_jwt/consts.rb
@@ -2,11 +2,9 @@
 
 module Authentication
   module AuthnJwt
-
     PROVIDER_URI_RESOURCE_NAME = "provider-uri"
     JWKS_URI_RESOURCE_NAME = "jwks-uri"
     ISSUER_RESOURCE_NAME = "issuer"
-
+    IDENTITY_FIELD_VARIABLE = "token-app-property"
   end
 end
-
