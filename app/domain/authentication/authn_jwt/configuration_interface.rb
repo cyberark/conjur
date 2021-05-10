07a3b98d8e859047a312a1937d1c734a48030f03
Tamir Zheleznyak <73157235+tzheleznyak@users.noreply.github.com>
2021-05-10 20:41:14 +0300

    Add call to validate restrictions
    Add call to validate restrictions restrictions command class to the generic configuration
    
    Flow:
    
    1.Extracting annotations with ExtractResourceRestrictions
    2.Check there is at least one annotation using NotEmptyConstraint
    3.Validating them with the JWT token using ValidateRestrictionsOneToOne

--- app/domain/authentication/authn_jwt/configuration_interface.rb
+++ app/domain/authentication/authn_jwt/configuration_interface.rb
@@ -2,9 +2,9 @@ module Authentication
   module AuthnJwt
     # Interface containing JWT configuration functions to be implemented in JWTConfiguration class for a vendor
     class ConfigurationInterface
-      def self.jwt_identity(authentication_parameters); end
-      def self.validate_restrictions(authentication_parameters); end
-      def self.validate_and_decode_token(authentication_parameters); end
+      def jwt_identity(authentication_parameters); end
+      def validate_restrictions(authentication_parameters); end
+      def validate_and_decode_token(authentication_parameters); end
     end
   end
 end
