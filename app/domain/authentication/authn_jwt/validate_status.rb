5df9e1abb93c040319b6f85de83faebed9fa276b
Avi Eyal <avi.eyal@cyberark.com>
2021-05-18 19:06:07 +0300

    add validations logic

--- app/domain/authentication/authn_jwt/validate_status.rb
+++ app/domain/authentication/authn_jwt/validate_status.rb
@@ -4,47 +4,59 @@ module Authentication
     ValidateStatus = CommandClass.new(
       dependencies: {
         fetch_authenticator_secrets: Authentication::Util::FetchAuthenticatorSecrets.new,
-        discover_identity_provider: Authentication::OAuth::DiscoverIdentityProvider.new
+        discover_identity_provider: Authentication::OAuth::DiscoverIdentityProvider.new,
+        create_signing_key: Authentication::AuthnJwt::CreateSigningKeyInterface.new,
+        fetch_issuer_value: Authentication::AuthnJwt::FetchIssuerValue.new,
+        fetch_identity_from_token: Authentication::AuthnJwt::IdentityFromDecodedTokenProvider
       },
-      inputs: %i[account service_id]
+      inputs: %i[authenticator_status_input]
     ) do
       def call
+        create_authentication_parameters
         validate_service_id_exists
         validate_secrets
-        #validate_provider_is_responsive
       end
 
       private
 
       def validate_service_id_exists
-        raise Errors::Authentication::AuthnJwt::ServiceIdMissing unless @service_id
+        raise Errors::Authentication::AuthnJwt::ServiceIdMissing unless @authenticator_status_input.service_id
       end
 
       def validate_secrets
-        jwt_authenticator_secrets
+        validate_signing_key_secrets
+        validate_issuer
+        validate_identity_secrets
       end
 
-      def jwt_authenticator_secrets
-        @jwt_authenticator_secrets ||= @fetch_authenticator_secrets.(
-          service_id: @service_id,
-            conjur_account: @account,
-            authenticator_name: "authn-jwt",
-            required_variable_names: required_variable_names
-        )
+      def validate_signing_key_secrets
+        @create_signing_key.call(authentication_parameters: @authentication_parameters)
+      end
+
+      def validate_issuer
+        @fetch_issuer_value.call(authenticator_input: @authenticator_status_input)
+      end
+
+      def validate_identity_secrets
+        @fetch_identity_from_token.new(@authentication_parameters).identity_configured_properly?
       end
 
       def required_variable_names
-        @required_variable_names ||= %w[provider-uri id-token-user-property]
+        @required_variable_names ||= %w[provider-uri]
       end
 
-      def validate_provider_is_responsive
-        @discover_identity_provider.(
-          provider_uri: provider_uri
+      def create_authentication_parameters
+        authentication_input = Authentication::AuthenticatorInput.new(
+          authenticator_name: @authenticator_status_input.authenticator_name,
+          service_id: @authenticator_status_input.service_id,
+          account: @authenticator_status_input.account,
+          username: @authenticator_status_input.username,
+          client_ip: @authenticator_status_input.client_ip,
+          credentials: nil,
+          request: nil
         )
-      end
 
-      def provider_uri
-        @jwt_authenticator_secrets["provider-uri"]
+        @authentication_parameters ||= Authentication::AuthnJwt::AuthenticationParameters.new(authentication_input)
       end
     end
   end
