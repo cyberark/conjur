98adac9be89771cc1ccbb6377cebcc0f63f835e1
Matthew Felgate <matthew.felgate@cyberark.com>
2021-05-20 15:49:14 -0400

    fixup! Remove the Conjur API dependency from the policy cucumber tests

--- cucumber/policy/features/support/env.rb
+++ cucumber/policy/features/support/env.rb
@@ -2,6 +2,7 @@
 
 require 'aruba'
 require 'aruba/cucumber'
+require 'conjur-api'
 
 Conjur.configuration.appliance_url = ENV['CONJUR_APPLIANCE_URL'] || 'http://conjur'
 Conjur.configuration.account = ENV['CONJUR_ACCOUNT'] || 'cucumber'
