2d049f3382899869ab5ec5445fa3651b66d9cb14
Sasha Chernomordik <sashac@cyberark.com>
2021-05-31 12:36:33 +0300

    JWT-JWKs basic sanity scenario (#2208)
    * JWT Basic sanity cucumber scenario
    * Add JWT automation to ci test suite
    * Adding JWT cucumber to dev env
    * Add JWT Automation to pipeline

---
+++ cucumber/authenticators_jwt/features/step_definitions/authn_jwt_steps.rb
@@ -0,0 +1,11 @@
+Given(/I successfully set authn-jwt jwks-uri variable with value of "([^"]*)" endpoint/) do |filename|
+  create_jwt_secret("jwks-uri", "#{JwtJwksHelper::JWKS_BASE_URI}/#{filename}")
+end
+
+Given(/I successfully set authn-jwt token-app-property variable to value "([^"]*)"/) do |value|
+  create_jwt_secret("token-app-property", value)
+end
+
+When(/I authenticate via authn-jwt with the JWT token/) do
+  authenticate_jwt_token(jwt_token)
+end
