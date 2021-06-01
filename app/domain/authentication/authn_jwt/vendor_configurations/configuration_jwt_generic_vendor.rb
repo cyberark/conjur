db1957f7333adbf50e4cf953bad549ad58236025
Tamir Zheleznyak <Tamir.Zheleznyak@CyberArk.com>
2021-06-01 12:36:53 +0300

    Refactor dependencies command classes in validate resource restrictions
    Call for new function from dependencies injection of command classes

--- app/domain/authentication/authn_jwt/vendor_configurations/configuration_jwt_generic_vendor.rb
+++ app/domain/authentication/authn_jwt/vendor_configurations/configuration_jwt_generic_vendor.rb
@@ -5,14 +5,13 @@ module Authentication
       class ConfigurationJWTGenericVendor < ConfigurationInterface
 
         def initialize(authenticator_input)
-          @authentication_parameters_class = Authentication::AuthnJwt::AuthenticationParameters
-          @extract_token_from_credentials = Authentication::AuthnJwt::InputValidation::ExtractTokenFromCredentials.new
-          @validate_and_decode_token_class = Authentication::AuthnJwt::ValidateAndDecode::ValidateAndDecodeToken
-          @identity_provider_factory = Authentication::AuthnJwt::IdentityProviders::CreateIdentityProvider
-          @validate_resource_restrictions_class = Authentication::ResourceRestrictions::ValidateResourceRestrictions
-          @create_identity_provider_class = Authentication::AuthnJwt::IdentityProviders::CreateIdentityProvider
           @logger = Rails.logger
-          init_authentication_parameters(authenticator_input)
+
+          @logger.debug(LogMessages::Authentication::AuthnJwt::CREATING_AUTHENTICATION_PARAMETERS_OBJECT.new)
+          @authentication_parameters = authentication_parameters_class.new(
+            authentication_input: authenticator_input,
+            jwt_token: jwt_token(authenticator_input)
+          )
         end
 
         def jwt_identity
@@ -20,14 +19,13 @@ module Authentication
         end
 
         def validate_restrictions
-          initialize_validate_restrictions
-          @validate_resource_restrictions.call(
+          validate_resource_restrictions.call(
             authenticator_name: @authentication_parameters.authenticator_name,
             service_id: @authentication_parameters.service_id,
             account: @authentication_parameters.account,
             role_name: jwt_identity,
-            constraints: @constraints,
-            authentication_request: @restriction_validator.new(
+            constraints: constraints,
+            authentication_request: restriction_validator_class.new(
               decoded_token: @authentication_parameters.decoded_token
             )
           )
@@ -41,29 +39,24 @@ module Authentication
 
         private
 
-        def init_authentication_parameters(authenticator_input)
-          @logger.debug(LogMessages::Authentication::AuthnJwt::CREATING_AUTHENTICATION_PARAMETERS_OBJECT.new)
-          jwt_token = @extract_token_from_credentials.call(
+        def jwt_token(authenticator_input)
+          extract_token_from_credentials.call(
             credentials: authenticator_input.request.body.read
-          )
-          @authentication_parameters = @authentication_parameters_class.new(
-            authentication_input: authenticator_input,
-            jwt_token: jwt_token
           )
         end
 
         def jwt_identity_from_request
-          @jwt_identity_from_request ||= identity_provider.jwt_identity
+          @jwt_identity_from_request = identity_provider.jwt_identity
         end
 
         def identity_provider
-          @identity_provider ||= @create_identity_provider_class.new.call(
+          @identity_provider = create_identity_provider.call(
             authentication_parameters: @authentication_parameters
           )
         end
 
         def validate_and_decode_token_instance
-          @validate_and_decode_token_instance ||= @validate_and_decode_token_class.new(
+          @validate_and_decode_token_instance ||= validate_and_decode_token_class.new(
             fetch_signing_key: fetch_signing_key,
             fetch_jwt_claims_to_validate: fetch_jwt_claims_to_validate
           )
@@ -76,7 +69,7 @@ module Authentication
         def fetch_cached_signing_key
           @fetch_cached_signing_key ||= ::Util::ConcurrencyLimitedCache.new(
             ::Util::RateLimitedCache.new(
-              ::Authentication::AuthnJwt::SigningKey::FetchCachedSigningKey.new(
+              fetch_cached_signing_key_class.new(
                 fetch_singing_key_interface: fetch_singing_key_interface
               ),
               refreshes_per_interval: CACHE_REFRESHES_PER_INTERVAL,
@@ -88,10 +81,34 @@ module Authentication
           )
         end
 
+        def authentication_parameters_class
+          @authentication_parameters_class ||= Authentication::AuthnJwt::AuthenticationParameters
+        end
+
+        def extract_token_from_credentials
+          @extract_token_from_credentials ||= Authentication::AuthnJwt::InputValidation::ExtractTokenFromCredentials.new
+        end
+
+        def validate_and_decode_token_class
+          @validate_and_decode_token_class ||= Authentication::AuthnJwt::ValidateAndDecode::ValidateAndDecodeToken
+        end
+
+        def validate_resource_restrictions_class
+          @validate_resource_restrictions_class ||= Authentication::ResourceRestrictions::ValidateResourceRestrictions
+        end
+
+        def create_identity_provider
+          @create_identity_provider ||= Authentication::AuthnJwt::IdentityProviders::CreateIdentityProvider.new
+        end
+
         def fetch_singing_key_interface
           @fetch_singing_key_interface ||= create_signing_key_interface.call(
             authentication_parameters: @authentication_parameters
           )
+        end
+
+        def fetch_cached_signing_key_class
+          @fetch_cached_signing_key_class ||= Authentication::AuthnJwt::SigningKey::FetchCachedSigningKey
         end
 
         def create_signing_key_interface
@@ -102,17 +119,29 @@ module Authentication
           @fetch_jwt_claims_to_validate ||= ::Authentication::AuthnJwt::ValidateAndDecode::FetchJwtClaimsToValidate.new
         end
 
-        def initialize_validate_restrictions
-          @restriction_validator = Authentication::AuthnJwt::RestrictionValidators::ValidateRestrictionsOneToOne
-          @restrictions_from_annotations_class = Authentication::ResourceRestrictions::GetServiceSpecificRestrictionFromAnnotation
-          @extract_resource_restrictions = Authentication::ResourceRestrictions::ExtractResourceRestrictions.new(
-            get_restriction_from_annotation: @restrictions_from_annotations_class,
+        def restriction_validator_class
+          @restriction_validator_class ||= Authentication::AuthnJwt::RestrictionValidators::ValidateRestrictionsOneToOne
+        end
+
+        def restrictions_from_annotations
+          @restrictions_from_annotations ||= Authentication::ResourceRestrictions::GetServiceSpecificRestrictionFromAnnotation.new
+        end
+
+        def extract_resource_restrictions
+          @extract_resource_restrictions ||= Authentication::ResourceRestrictions::ExtractResourceRestrictions.new(
+            get_restriction_from_annotation: restrictions_from_annotations,
             ignore_empty_annotations: false
           )
-          @validate_resource_restrictions = @validate_resource_restrictions_class.new(extract_resource_restrictions: @extract_resource_restrictions)
-          @constraints = Authentication::Constraints::MultipleConstraint.new(
+        end
+
+        def constraints
+          @constraints ||= Authentication::Constraints::MultipleConstraint.new(
             Authentication::Constraints::NotEmptyConstraint.new
           )
+        end
+
+        def validate_resource_restrictions
+          @validate_resource_restrictions ||= validate_resource_restrictions_class.new(extract_resource_restrictions: extract_resource_restrictions)
         end
       end
     end
