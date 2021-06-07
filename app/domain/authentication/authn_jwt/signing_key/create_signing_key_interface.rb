866ee14bc8170e31cdf48e1a6d2b406de8b584a0
Nessi Lahav <Nessi.Lahav@cyberark.com>
2021-06-07 16:46:19 +0300

    Enhance and fix log messages (#2244)
    * Enhance and fix log messages
    
    * Update app/domain/logs.rb
    
    Co-authored-by: Shuli Finkelstein <64148158+shulifink@users.noreply.github.com>
    
    * Update app/domain/logs.rb
    
    Co-authored-by: Shuli Finkelstein <64148158+shulifink@users.noreply.github.com>
    
    * Enhance and fix log messages
    
    * Change messages to info
    
    * Fix messages
    
    * Update app/domain/logs.rb
    
    Co-authored-by: Shuli Finkelstein <64148158+shulifink@users.noreply.github.com>
    
    * Update app/domain/logs.rb
    
    Co-authored-by: Shuli Finkelstein <64148158+shulifink@users.noreply.github.com>
    
    * Update app/domain/logs.rb
    
    Co-authored-by: Shuli Finkelstein <64148158+shulifink@users.noreply.github.com>
    
    * Fix messages
    
    * fix memoization issue
    
    Co-authored-by: Shuli Finkelstein <64148158+shulifink@users.noreply.github.com>

--- app/domain/authentication/authn_jwt/signing_key/create_signing_key_interface.rb
+++ app/domain/authentication/authn_jwt/signing_key/create_signing_key_interface.rb
@@ -17,19 +17,22 @@ module Authentication
         private
 
         def create
-          @logger.debug(LogMessages::Authentication::AuthnJwt::ValidatingJwtSigningKeyConfiguration.new)
+          @logger.debug(LogMessages::Authentication::AuthnJwt::SelectingSigningKeyInterface.new)
           validate_key_configuration
 
           if provider_uri_has_valid_configuration?
-            @logger.debug(LogMessages::Authentication::AuthnJwt::FetchingProviderUriSigningKey.new)
+            @logger.info(
+              LogMessages::Authentication::AuthnJwt::SelectedSigningKeyInterface.new(PROVIDER_URI_INTERFACE_NAME))
             fetch_provider_uri_signing_key
           elsif jwks_uri_has_valid_configuration?
-            @logger.debug(LogMessages::Authentication::AuthnJwt::FetchingJwksUriSigningKey.new)
+            @logger.info(
+              LogMessages::Authentication::AuthnJwt::SelectedSigningKeyInterface.new(JWKS_URI_INTERFACE_NAME))
             fetch_jwks_uri_signing_key
           end
         end
 
         def validate_key_configuration
+          @logger.debug(LogMessages::Authentication::AuthnJwt::ValidatingJwtSigningKeyConfiguration.new)
           if (provider_uri_has_valid_configuration? && jwks_uri_has_valid_configuration?) ||
               (!provider_uri_has_valid_configuration? && !jwks_uri_has_valid_configuration?)
             raise Errors::Authentication::AuthnJwt::InvalidUriConfiguration.new(
@@ -37,13 +40,16 @@ module Authentication
               JWKS_URI_RESOURCE_NAME
             )
           end
+          @logger.debug(LogMessages::Authentication::AuthnJwt::ValidatedSigningKeyConfiguration.new)
         end
 
         def provider_uri_has_valid_configuration?
+          return @provider_uri_has_valid_configuration unless @provider_uri_has_valid_configuration.nil?
           @provider_uri_has_valid_configuration ||= fetch_provider_uri_signing_key.valid_configuration?
         end
 
         def jwks_uri_has_valid_configuration?
+          return @jwks_uri_has_valid_configuration unless @jwks_uri_has_valid_configuration.nil?
           @jwks_uri_has_valid_configuration ||= fetch_jwks_uri_signing_key.valid_configuration?
         end
 
