6bb9d16ae27f1b051c0591980fd9e9e068cc1225
Adam Ouamani <adam.ouamani@cyberark.com>
2021-10-22 16:27:52 -0400

    Updated conjurctl_steps.rb

--- cucumber/api/features/step_definitions/conjurctl_steps.rb
+++ cucumber/api/features/step_definitions/conjurctl_steps.rb
@@ -1,14 +1,18 @@
 require 'open3'
 
 When(/^I retrieve an API key for user "([^"]*)" using conjurctl$/) do |user_id|
-  command = "conjurctl role retrieve-key #{user_id} > admin-key.txt"    
+  command = "conjurctl role retrieve-key #{user_id} > ~/admin-key.txt"    
   #command = "conjurctl role retrieve-key #{user_id}"  
   @conjurctl_stdout, @conjurctl_stderr, = Open3.capture3(command)
-  @conjurctl_stdout = File.read("admin-key.txt")
+  @conjurctl_stdout = File.read("~/admin-key.txt")
+  puts "SIMPLECOV_DEBUG1-@conjurctl_stdout: #{@conjurctl_stdout}"
 end
 
 Then(/^the API key is correct$/) do
-  expect(@conjurctl_stdout).to eq("#{Credentials['cucumber:user:admin'].api_key}\n")
+  #@conjurctl_stdout = File.read("~/admin-key.txt") #added ao
+  adam_stdout = File.read("~/admin-key.txt") #added ao
+  puts "SIMPLECOV_DEBUG2-@conjurctl_stdout: #{adam_stdout}"
+  expect(adam_stdout).to eq("#{Credentials['cucumber:user:admin'].api_key}\n")
 end
 
 Then(/^the stderr includes the error "([^"]*)"$/) do |error|
