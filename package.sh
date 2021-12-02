3c64ce5b6495f58634c6b921128b0f83177e8392
Sasha Chernomordik <sashac@cyberark.com>
2021-12-02 14:38:53 +0200

    Test new debify image

--- package.sh
+++ package.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -ex
-export DEBIFY_IMAGE='registry.tld/conjurinc/debify:2.0.0'
+export DEBIFY_IMAGE='registry.tld/conjurinc/debify:one-step-to-ruby3'
 
 docker run --rm $DEBIFY_IMAGE config script > docker-debify
 chmod +x docker-debify
