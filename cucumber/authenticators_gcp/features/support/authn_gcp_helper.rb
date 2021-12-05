c582e382705eaca552d7bb0b12733225b13cea8d
Semyon Estrin <semyon.estrin@cyberark.com>
2021-12-05 16:30:10 +0200

    Missing require in GCP cucumber

--- cucumber/authenticators_gcp/features/support/authn_gcp_helper.rb
+++ cucumber/authenticators_gcp/features/support/authn_gcp_helper.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 require 'jwt'
-
+require 'cucumber/_authenticators_common/features/support/authenticator_helpers'
 module AuthnGcpHelper
   include AuthenticatorHelpers
 
