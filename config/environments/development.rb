ad3b2c1df15a1c54cacd2936faf4eea7dd39beec
Semyon Estrin <semyon.estrin@cyberark.com>
2022-02-07 14:38:14 +0200

    Update CHANGELOG NOTICES etc
    Co-Authored-By: Sasha Chernomordik <sashac@cyberark.com>

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -10,8 +10,9 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = true
 
-  # Whitelist one hostname
-  puts "Adding conjur machine to whitelist"
+  # Whitelist conjur hostname for tests
+  # For more information, refer to:
+  # https://guides.rubyonrails.org/configuring.html#actiondispatch-hostauthorization
   config.hosts << "conjur"
 
   # eager_load needed to make authentication work without the hacky
