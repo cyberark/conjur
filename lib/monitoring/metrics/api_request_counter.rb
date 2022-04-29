fd95ffa4b0e9a4bef9b4c5f9c06208821aa35fda
Glen Johnson <glen.johnson@cyberark.com>
2022-04-29 09:53:14 -0400

    Define request metrics, operations, and tests

---
+++ lib/monitoring/metrics/api_request_counter.rb
@@ -0,0 +1,28 @@
+module Monitoring
+  module Metrics
+    class ApiRequestCounter
+      attr_reader :registry, :pubsub, :metric_name, :docstring, :labels, :sub_event_name
+
+      def setup(registry, pubsub)
+        @registry = registry
+        @pubsub = pubsub
+        @metric_name = :conjur_http_server_requests_total
+        @docstring = 'The total number of HTTP requests handled by Conjur.'
+        @labels = %i[code operation]
+        @sub_event_name = 'conjur.request'
+
+        # Create/register the metric
+        Metrics.create_metric(self, :counter)
+      end
+
+      def update(payload)
+        metric = @registry.get(@metric_name)
+        update_labels = {
+          code: payload[:code],
+          operation: payload[:operation]
+        }
+        metric.increment(labels: update_labels)
+      end
+    end
+  end
+end
---
+++ lib/monitoring/metrics/api_request_histogram.rb
@@ -0,0 +1,27 @@
+module Monitoring
+  module Metrics
+    class ApiRequestHistogram
+      attr_reader :registry, :pubsub, :metric_name, :docstring, :labels, :sub_event_name
+
+      def setup(registry, pubsub)
+        @registry = registry
+        @pubsub = pubsub
+        @metric_name = :conjur_http_server_request_duration_seconds
+        @docstring = 'The HTTP response duration of requests handled by Conjur.'
+        @labels = %i[operation]
+        @sub_event_name = 'conjur.request'
+
+        # Create/register the metric
+        Metrics.create_metric(self, :histogram)
+      end
+
+      def update(payload)
+        metric = @registry.get(@metric_name)
+        update_labels = {
+          operation: payload[:operation]
+        }
+        metric.observe(payload[:duration], labels: update_labels)
+      end
+    end
+  end
+end
