c88ee57c9e6d824a283eea62aab7c375bc752b02
Glen Johnson <glen.johnson@cyberark.com>
2022-05-25 13:03:24 -0400

    Add policy resource metric and pub/sub events

---
+++ lib/monitoring/metrics/policy_resouce_gauge.rb
@@ -0,0 +1,30 @@
+module Monitoring
+  module Metrics
+    class PolicyResourceGauge
+      attr_reader :registry, :pubsub, :metric_name, :docstring, :labels, :sub_event_name, :throttle
+
+      def setup(registry, pubsub)
+        @registry = registry
+        @pubsub = pubsub
+        @metric_name = :conjur_resource_count
+        @docstring = 'Number of resources in Conjur database'
+        @labels = %i[kind]
+        @sub_event_name = 'conjur.resource_count_update'
+        @throttle = true
+        
+        # Create/register the metric
+        Metrics.create_metric(self, :gauge)
+
+        # Run update to set the initial counts on startup
+        update
+      end
+
+      def update(*payload)
+        metric = @registry.get(@metric_name)
+        Monitoring::QueryHelper.instance.policy_resource_counts.each do |kind, value|
+          metric.set(value, labels: { kind: kind })
+        end
+      end
+    end
+  end
+end
