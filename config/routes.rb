74eb700f0e7d5ff701faa67c81ca630c271f7dec
Matthew Felgate <matthew.felgate@cyberark.com>
2022-05-26 12:28:56 -0400

    Add oidc endpoint to list authenticators

--- config/routes.rb
+++ config/routes.rb
@@ -63,6 +63,8 @@ Rails.application.routes.draw do
       get     "/resources/:account"                   => "resources#index"
       get     "/resources"                            => "resources#index"
 
+      get     "/authn-oidc/:account/providers"              => "authenticators/oidc/providers#index"
+
       # NOTE: the order of these routes matters: we need the expire
       #       route to come first.
       post    "/secrets/:account/:kind/*identifier" => "secrets#expire",
