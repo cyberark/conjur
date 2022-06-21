e8fb32e76f01940a04feb36a5e4b9fee006889de
Ofira Burstein <ofira.burstein@cyberark.com>
2022-06-21 11:15:17 +0300

    ApiKey annotation mandatory for hosts

--- spec/models/loader/types.rb
+++ spec/models/loader/types.rb
@@ -85,3 +85,67 @@ describe Loader::Types::User do
     end
   end
 end
+
+describe Loader::Types::Host do
+  let(:host) do
+    host = Conjur::PolicyParser::Types::Host.new
+    host.id = resource_id
+    if api_key != ''
+      host.annotations =  { "authn/api-key" => api_key }
+    end
+    Loader::Types.wrap(host, self)
+  end
+
+  describe '.verify' do
+    context 'when CONJUR_AUTHN_API_KEY_DEFAULT is true' do
+      before do
+        allow(ENV).to receive(:[]).with('CONJUR_AUTHN_API_KEY_DEFAULT').and_return('true')
+        Rails.application.config.conjur_config.authn_api_key_default = true
+      end
+
+      context 'when creating host with api-key annotation true' do
+        let(:resource_id) { 'myhost@admin' }
+        let(:api_key) { true }
+        it { expect { host.verify }.to_not raise_error }
+      end
+
+      context 'when creating host with api-key annotation false' do
+        let(:resource_id) { 'myhost@cyberark' }
+        let(:api_key) { false }
+        it { expect { host.verify }.to_not raise_error(Exceptions::InvalidPolicyObject) }
+      end
+
+      context 'when creating host without api-key annotation' do
+        let(:resource_id) { 'myhost@cyberark' }
+        let(:api_key) { '' }
+        it { expect { host.verify }.to_not raise_error(Exceptions::InvalidPolicyObject) }
+      end
+    end
+
+    context 'when CONJUR_AUTHN_API_KEY_DEFAULT is false' do
+      before do
+        allow(ENV).to receive(:[]).with('CONJUR_AUTHN_API_KEY_DEFAULT').and_return('false')
+        Rails.application.config.conjur_config.authn_api_key_default = false
+      end
+
+      context 'when creating host with api-key annotation true' do
+        let(:resource_id) { 'myhost@admin' }
+        let(:api_key) { true }
+        it { expect { host.verify }.to_not raise_error }
+      end
+
+      context 'when creating host with api-key annotation false' do
+        let(:resource_id) { 'alice@cyberark' }
+        let(:api_key) { false }
+        it { expect { host.verify }.to raise_error }
+      end
+
+      context 'when creating host without api-key annotation' do
+        let(:resource_id) { 'alice@cyberark' }
+        let(:api_key) { '' }
+        it { expect { host.verify }.to raise_error }
+      end
+    end
+
+  end
+end
