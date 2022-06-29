3c43507ad66197b8e84e7ef6e7656acbbf477885
Kumbirai Tanekha <kumbirai.tanekha@gmail.com>
2022-06-29 10:23:14 +0100

    Fix Dockerfile.appliance

--- build-and-publish-internal-appliance.sh
+++ build-and-publish-internal-appliance.sh
@@ -4,5 +4,24 @@ set -euo pipefail
 
 IMAGE="registry.tld/conjur-appliance:eval-authn-k8s-label-selector"
 
+echo "FROM registry.tld/conjur-appliance:5.0-stable
+
+# Copy new source files
+$(
+    echo "
+app/domain/authentication/authn_k8s/authentication_request.rb
+app/domain/authentication/authn_k8s/consts.rb
+app/domain/authentication/authn_k8s/k8s_object_lookup.rb
+app/domain/authentication/authn_k8s/k8s_resource_validator.rb
+app/domain/authentication/constraints/required_exclusive_constraint.rb
+app/domain/errors.rb
+" |  docker run --rm -i --entrypoint="" ruby:2-alpine ruby -e '
+files = STDIN.read.split("\n").reject(&:empty?)
+puts files.map {|file| "COPY #{file} /opt/conjur/possum/#{file}"}.join("\n")
+'
+)
+
+RUN chown -R conjur:conjur /opt/conjur/possum/app" > Dockerfile.appliance
+
 docker build -f ./Dockerfile.appliance -t "${IMAGE}" .
 docker push "${IMAGE}"
