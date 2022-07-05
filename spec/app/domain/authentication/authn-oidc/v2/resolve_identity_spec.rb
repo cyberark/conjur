1a66feb87f1914481c94fe3f874e593ca031f6c9
Jason Vanderhoof <jvanderhoof@gmail.com>
2022-07-05 14:03:44 -0600

    Cleanup Resolve Identity class and tests

--- spec/app/domain/authentication/authn-oidc/v2/resolve_identity_spec.rb
+++ spec/app/domain/authentication/authn-oidc/v2/resolve_identity_spec.rb
@@ -3,95 +3,160 @@
 require 'spec_helper'
 
 RSpec.describe(' Authentication::AuthnOidc::V2::ResolveIdentity') do
-
-  let(:alice) { "rspec:user:alice" }
-  let(:bob) { "rspec:user:bob" }
-  let(:chad) { "rspec:user:chad" }
-
-  let(:allowed_roles) do
-    [user_alice, user_chad, user_dave, user_edward]
-  end
-
-  let(:user_alice) do
-    instance_double(::Role).tap do |double|
-      allow(double).to receive(:id).and_return(alice)
-      allow(double).to receive(:resource?).and_return(true)
-      allow(double).to receive(:resource).and_return(alice_resource)
-    end
-  end
-  let(:user_dave) do
-    instance_double(::Role).tap do |double|
-      allow(double).to receive(:id).and_return(alice)
-      allow(double).to receive(:resource?).and_return(true)
-      allow(double).to receive(:resource).and_return(same_annotation)
-    end
-  end
-  let(:user_edward) do
-    instance_double(::Role).tap do |double|
-      allow(double).to receive(:id).and_return(alice)
-      allow(double).to receive(:resource?).and_return(true)
-      allow(double).to receive(:resource).and_return(same_annotation)
-    end
-  end
-  let(:user_chad) do
-    instance_double(::Role).tap do |double|
-      allow(double).to receive(:id).and_return(chad)
-      allow(double).to receive(:resource?).and_return(true)
-      allow(double).to receive(:resource).and_return(chad_resource)
-    end
-  end
-
-  let(:chad_resource) do
-    instance_double(::Resource).tap do |double|
-      allow(double).to receive(:annotation).with('authn-oidc/identity').and_return("chad.example")
-    end
-  end
-
-  let(:same_annotation) do
-    instance_double(::Resource).tap do |double|
-      allow(double).to receive(:annotation).with('authn-oidc/identity').and_return("bad.example")
-    end
-  end
-
-  let(:alice_resource) do
-    instance_double(::Resource).tap do |double|
-      allow(double).to receive(:annotation).with('authn-oidc/identity').and_return("alice.example")
-    end
-  end
-
   let(:resolve_Identity) do
-    Authentication::AuthnOidc::V2::ResolveIdentity.new()
+    Authentication::AuthnOidc::V2::ResolveIdentity.new
   end
 
   describe('#call') do
-    context 'when a role_id matches the identity exist' do
-      it 'returns the role' do
-        expect(resolve_Identity.call(account: 'rspec', identity: "alice", allowed_roles: allowed_roles).id)
-          .to eq(alice)
+    # context 'when a role_id matches the provided identity' do
+    let(:valid_role) do
+      instance_double(::Role).tap do |double|
+        allow(double).to receive(:id).and_return('rspec:user:alice')
+        allow(double).to receive(:resource?).and_return(true)
       end
     end
 
-    context 'when the identity does not match a role or annotation' do
+    context 'when identity matches a role ID' do
+      it 'returns the matching role' do
+        expect(
+          resolve_Identity.call(
+            account: 'rspec',
+            identity: "alice",
+            allowed_roles: [ valid_role ]
+          ).id
+        ).to eq('rspec:user:alice')
+      end
+
+      context 'when it includes non-resources' do
+        it 'returns the matching role' do
+          expect(
+            resolve_Identity.call(
+              account: 'rspec',
+              identity: "alice",
+              allowed_roles: [
+                instance_double(::Role).tap do |double|
+                  allow(double).to receive(:id).and_return('rspec:user:alice')
+                  allow(double).to receive(:resource?).and_return(false)
+                end,
+                valid_role
+              ]
+            ).id
+          ).to eq('rspec:user:alice')
+        end
+      end
+
+      context 'when the accounts are different' do
+        it 'returns the matching role' do
+          expect(
+            resolve_Identity.call(
+              account: 'rspec',
+              identity: "alice",
+              allowed_roles: [
+                instance_double(::Role).tap do |double|
+                  allow(double).to receive(:id).and_return('foo:user:alice')
+                  allow(double).to receive(:resource?).and_return(true)
+                end,
+                valid_role
+              ]
+            ).id
+          ).to eq('rspec:user:alice')
+        end
+      end
+    end
+
+    context 'when the provided identity does not match a role or annotation' do
       it 'raises the error RoleNotFound' do
-        expect { resolve_Identity.call(account: 'rspec', identity: "bob", allowed_roles: allowed_roles) }
-          .to raise_error(Errors::Authentication::Security::RoleNotFound, /CONJ00007E 'bob' not found/)
+        expect {
+          resolve_Identity.call(
+            account: 'rspec',
+            identity: "bob",
+            allowed_roles: []
+          )
+        }.to raise_error(
+          Errors::Authentication::Security::RoleNotFound,
+          /CONJ00007E 'bob' not found/
+        )
       end
     end
 
-    context 'when the identity does not matches an annotation' do
-      it 'returns the role of the user' do
-        expect(resolve_Identity.call(account: 'rspec', identity: "chad.example", allowed_roles: allowed_roles).id)
-          .to eq(chad)
+    context 'when the identity matches an annotation' do
+      context 'when a single role matches' do
+        it 'returns the role of the user' do
+          expect(
+            resolve_Identity.call(
+              account: 'rspec',
+              identity: "chad.example",
+              allowed_roles: [
+                instance_double(::Role).tap do |double|
+                  allow(double).to receive(:id).and_return('rspec:user:chad')
+                  allow(double).to receive(:resource?).and_return(true)
+                  allow(double).to receive(:resource).and_return(
+                    instance_double(::Resource).tap do |resource|
+                      allow(resource).to receive(:annotation).with('authn-oidc/identity').and_return('chad.example')
+                    end
+                  )
+                end
+              ]
+            ).id
+          ).to eq('rspec:user:chad')
+        end
+      end
+
+      context 'when multiple roles match' do
+        let(:resource) do
+          instance_double(::Resource).tap do |resource|
+            allow(resource).to receive(:annotation).with('authn-oidc/identity').and_return('chad.example')
+          end
+        end
+        it 'raises an error' do
+          expect do
+            resolve_Identity.call(
+              account: 'rspec',
+              identity: 'chad.example',
+              allowed_roles: [
+                instance_double(::Role).tap do |double|
+                  allow(double).to receive(:id).and_return('rspec:user:chad')
+                  allow(double).to receive(:resource?).and_return(true)
+                  allow(double).to receive(:resource).and_return(resource)
+                end,
+                instance_double(::Role).tap do |double|
+                  allow(double).to receive(:id).and_return('rspec:user:brad')
+                  allow(double).to receive(:resource?).and_return(true)
+                  allow(double).to receive(:resource).and_return(resource)
+                end
+              ]
+            )
+          end.to raise_error(
+            Errors::Authentication::Security::MultipleRoleMatchesFound,
+            /CONJ00009E 'chad.example' matched multiple roles/
+          )
+        end
+      end
+
+      context 'when the account does not match the role' do
+        it 'raises the error RoleNotFound' do
+          expect do
+            resolve_Identity.call(
+              account: 'rspec',
+              identity: 'chad.example',
+              allowed_roles: [
+                instance_double(::Role).tap do |double|
+                  allow(double).to receive(:id).and_return('foo:user:chad')
+                  allow(double).to receive(:resource?).and_return(true)
+                  allow(double).to receive(:resource).and_return(
+                    instance_double(::Resource).tap do |resource|
+                      allow(resource).to receive(:annotation).with('authn-oidc/identity').and_return('chad.example')
+                    end
+                  )
+                end
+              ]
+            )
+          end.to raise_error(
+            Errors::Authentication::Security::RoleNotFound,
+            /CONJ00007E 'chad.example' not found/
+          )
+        end
       end
     end
-
-    context 'when the identity matches an annotation, but multiple roles have the same annotation' do
-      it 'raises the an error' do
-        expect { resolve_Identity.call(account: 'rspec', identity: "bad.example", allowed_roles: allowed_roles) }
-          .to raise_error("Multiple annotations match identity: bad.example")
-      end
-    end
-
   end
 end
-
