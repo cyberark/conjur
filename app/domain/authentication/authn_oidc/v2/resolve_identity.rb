e1b2c87a1404e7e9b143cc724128bf9d81d43185
Jason Vanderhoof <jvanderhoof@gmail.com>
2022-07-05 14:03:44 -0600

    Cleanup Resolve Identity class and tests

--- app/domain/authentication/authn_oidc/v2/resolve_identity.rb
+++ app/domain/authentication/authn_oidc/v2/resolve_identity.rb
@@ -3,26 +3,19 @@ module Authentication
     module V2
       class ResolveIdentity
         def call(identity:, account:, allowed_roles:)
-          # binding.pry
           # make sure role has a resource (ex. user, host)
-          roles = allowed_roles.select { |role| role.resource? }
+          roles = allowed_roles.select(&:resource?)  # { |role| role.resource? }
 
           roles.each do |role|
             role_account, _, role_id = role.id.split(':')
             return role if role_account == account && identity == role_id
           end
 
-          annotated_roles = []
-          roles.each do |role|
-            role_account = role.id.split(':').first
-            next unless role_account == account
-
-            # Don't love the performance...
-            if role.resource.annotation('authn-oidc/identity').to_s.downcase == identity.to_s.downcase
-              annotated_roles.push(role)
-            end
+          annotated_roles = roles.select do |role|
+            role.resource.annotation('authn-oidc/identity').to_s.downcase == identity.to_s.downcase &&
+              role.id.split(':').first == account
           end
-          raise "Multiple annotations match identity: #{identity}" if annotated_roles.length > 1
+          raise Errors::Authentication::Security::MultipleRoleMatchesFound, identity if annotated_roles.length > 1
           raise Errors::Authentication::Security::RoleNotFound, identity unless  annotated_roles.first
 
           annotated_roles.first
