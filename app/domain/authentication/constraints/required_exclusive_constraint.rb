64f65668786215ee81f7ac3aa330769c1ac78887
John ODonnell <john.odonnell@cyberark.com>
2022-07-22 10:13:14 -0400

    Add unit tests for new XOR constraint

--- app/domain/authentication/constraints/required_exclusive_constraint.rb
+++ app/domain/authentication/constraints/required_exclusive_constraint.rb
@@ -1,6 +1,8 @@
 module Authentication
   module Constraints
 
+    # This constraint is initialized with an array of strings.
+    # They represent resource restrictions where exactly one is required.
     class RequiredExclusiveConstraint
 
       def initialize(required_exclusive:)
@@ -9,7 +11,7 @@ module Authentication
 
       def validate(resource_restrictions:)
         restrictions_found = resource_restrictions & @required_exclusive
-        raise Errors::Authentication::Constraints::IllegalExclusiveRequiredCombination, @required_exclusive unless restrictions_found.length == 1
+        raise Errors::Authentication::Constraints::IllegalRequiredExclusiveCombination.new(@required_exclusive, restrictions_found) unless restrictions_found.length == 1
       end
 
     end
