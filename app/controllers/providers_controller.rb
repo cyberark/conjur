0ca31ba5be2f18b8f28cd619a5feaeefe545e4df
Jason Vanderhoof <jvanderhoof@gmail.com>
2022-08-05 16:08:38 -0600

    Remove authenticator for list authenticators

--- app/controllers/providers_controller.rb
+++ app/controllers/providers_controller.rb
@@ -1,10 +1,6 @@
 # frozen_string_literal: true
 
-class ProvidersController < RestController
-  include FindResource
-  include AssumedRole
-  include CurrentUser
-
+class ProvidersController < ApplicationController
   def index
     namespace = Authentication::Util::NamespaceSelector.select(
       authenticator_type: params[:authenticator]
@@ -16,14 +12,8 @@ class ProvidersController < RestController
         ).find_all(
           account: params[:account],
           type: params[:authenticator]
-        ).select { |authenticator| role&.allowed_to?(:read, ::Resource[authenticator.resource_id]) }
+        )
       )
     )
-  end
-
-  # The v5 API currently sends +acting_as+ when listing resources
-  # for a role other than the current user.
-  def role
-    assumed_role(params[:role].presence) || assumed_role(params[:acting_as].presence)
   end
 end
