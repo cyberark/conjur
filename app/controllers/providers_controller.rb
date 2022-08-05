dfa79962d4c0e7f3355be7893ba1bb9e5ec40bae
Jason Vanderhoof <jvanderhoof@gmail.com>
2022-08-05 16:08:38 -0600

    Remove OIDC provider list from authentication

--- app/controllers/providers_controller.rb
+++ app/controllers/providers_controller.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
-class ProvidersController < RestController
-  include FindResource
-  include AssumedRole
-  include CurrentUser
+class ProvidersController < ApplicationController
+  # include FindResource
+  # include AssumedRole
+  # include CurrentUser
 
   def index
     namespace = Authentication::Util::NamespaceSelector.select(
@@ -16,14 +16,15 @@ class ProvidersController < RestController
         ).find_all(
           account: params[:account],
           type: params[:authenticator]
-        ).select { |authenticator| role&.allowed_to?(:read, ::Resource[authenticator.resource_id]) }
+        )
+        # .select { |authenticator| role&.allowed_to?(:read, ::Resource[authenticator.resource_id]) }
       )
     )
   end
 
   # The v5 API currently sends +acting_as+ when listing resources
   # for a role other than the current user.
-  def role
-    assumed_role(params[:role].presence) || assumed_role(params[:acting_as].presence)
-  end
+  # def role
+  #   assumed_role(params[:role].presence) || assumed_role(params[:acting_as].presence)
+  # end
 end
