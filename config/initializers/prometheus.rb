d6a955fa95c590779885a946d1803c0d0d02109d
Glen Johnson <glen.johnson@cyberark.com>
2022-09-14 10:37:51 -0600

    Add authenticator metric and tests
    (cherry picked from commit be2d9ef85b6c288991751c01be9e2bf3a049ebcc)

--- config/initializers/prometheus.rb
+++ config/initializers/prometheus.rb
@@ -6,13 +6,17 @@ if Rails.application.config.conjur_config.telemetry_enabled
   # Require all defined metrics
   Dir.glob(Rails.root + 'lib/monitoring/metrics/*.rb', &method(:require))
 
+  # Load the authentication module early so that telemetry can see which authenticators are installed on startup
+  Dir.glob(Rails.root + 'app/domain/authentication/**/*.rb', &method(:require))
+
   # Register new metrics and setup the Prometheus client store
   metrics = [
     Monitoring::Metrics::ApiRequestCounter.new,
     Monitoring::Metrics::ApiRequestHistogram.new,
     Monitoring::Metrics::ApiExceptionCounter.new,
     Monitoring::Metrics::PolicyResourceGauge.new,
-    Monitoring::Metrics::PolicyRoleGauge.new
+    Monitoring::Metrics::PolicyRoleGauge.new,
+    Monitoring::Metrics::AuthenticatorGauge.new,
   ]
   Monitoring::Prometheus.setup(metrics: metrics)
 
