3f21db074ffaced9e54182b696dda10ece4afda8
Jason Vanderhoof <jvanderhoof@gmail.com>
2022-11-21 11:24:26 -0700

    Revert "Update and cleanup Cucumber tests for OIDC"
    This revert is part of the removal of PKCE support. This specific
    commit reverts commit 218b6ca1e35c90669f8b5f20366d0c031cfe5d31.

--- cucumber/_authenticators_common/features/support/hooks.rb
+++ cucumber/_authenticators_common/features/support/hooks.rb
@@ -4,41 +4,6 @@ Before('@skip') do
   skip_this_scenario
 end
 
-# The following is a context object. It allows data to be shared between
-# Scenario steps. The use of this context should help reduce the current
-# practice of setting and retrieving instance variables in different Scenario
-# Steps.
-
-# An example of when a Context might be used is when retriving a user's Conjur
-# Authorization Token on one step, and validating the  token on another step.
-module Scenario
-  class Context
-    def initialize(logger: Rails.logger, **kwargs)
-      @store = {}
-      @logger = logger
-      set(**kwargs) if kwargs
-    end
-
-    def get(key)
-      @store[key]
-    end
-
-    def set(**kwargs)
-      kwargs.each do |key, value|
-        @store[key] = value
-      end
-    end
-
-    def keys
-      @store.keys
-    end
-
-    def unset
-      @store = {}
-    end
-  end
-end
-
 # Reset the DB between each test
 #
 # Prior to this hook, our tests had hidden coupling.  This ensures each test is
@@ -46,7 +11,6 @@ end
 Before do
   @user_index = 0
   @host_index = 0
-  @context = Scenario::Context.new(account: 'cucumber')
 
   Role.truncate(cascade: true)
   Secret.truncate
@@ -57,7 +21,7 @@ Before do
       Slosilo.send(:keystore).adapter.model[k].delete
     end
   end
-
+  
   Account.find_or_create_accounts_resource
   admin_role = Role.create(role_id: "cucumber:user:admin")
   creds = Credentials.new(role: admin_role)
@@ -78,5 +42,4 @@ After do
   @env.each do |key, value|
     ENV[key] = value
   end
-  @context.unset
 end
