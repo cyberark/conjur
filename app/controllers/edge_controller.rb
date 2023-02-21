16d1740c7549c461eb4e5bdea3fcabc5ec5ef6bd
amosmintzcyberark <amos.mintz@cyberark.com>
2023-02-21 16:28:18 +0200

    ONYX-32182: Secret Retrieval endpoint - product

--- app/controllers/edge_controller.rb
+++ app/controllers/edge_controller.rb
@@ -8,8 +8,6 @@ class EdgeController < RestController
       .slice(*allowed_params).to_h.symbolize_keys
     begin
       verify_edge_host(options)
-      #scope = Resource.visible_to(current_user).search(options)
-      #scope = Resource.visible_to(current_user).search
       offset = options[:offset]
       limit = options[:limit]
       scope = Resource.where(:resource_id.like(options[:account]+":variable:data%"))
@@ -48,14 +46,20 @@ class EdgeController < RestController
     allowed_params = %i[account limit offset]
     options = params.permit(*allowed_params)
                     .slice(*allowed_params).to_h.symbolize_keys
-    verify_edge_host(options)
-    offset = options[:offset]
-    limit = options[:limit]
-    scope = Role.where(:role_id.like(options[:account]+":host:data%"))
-    scope = scope.order(:role_id).limit(
-      (limit || 1000).to_i,
-      (offset || 0).to_i
-    )
+    begin
+      verify_edge_host(options)
+      offset = options[:offset]
+      limit = options[:limit]
+      scope = Role.where(:role_id.like(options[:account]+":host:data%"))
+      scope = scope.order(:role_id).limit(
+        (limit || 1000).to_i,
+        (offset || 0).to_i
+      )
+    rescue ApplicationController::Forbidden
+      raise
+    rescue ArgumentError => e
+      raise ApplicationController::UnprocessableEntity, e.message
+    end
     if params[:count] == 'true'
       results = { count: scope.count('*'.lit) }
       render(json: results)
@@ -76,7 +80,8 @@ class EdgeController < RestController
   private
 
   def verify_edge_host(options)
-    raise Forbidden, 'Requestor is not a host' unless current_user.kind == 'host'
+    raise Forbidden unless current_user.kind == 'host'
+    raise Forbidden unless current_user.role_id.include? "host:edge/edge"
     role = Role[options[:account] + ':group:edge/edge-admins']
     raise Forbidden unless role && role.ancestor_of?(current_user)
   end
