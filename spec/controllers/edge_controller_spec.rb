4ea88b819f29a1704fbf50bc041cb07838928e52
amosmintzcyberark <amos.mintz@cyberark.com>
2023-04-19 11:42:17 +0300

    edge hmac_api_key + test
    edge hmac_api_key + test
    
    edge hmac_api_key + test

--- spec/controllers/edge_controller_spec.rb
+++ spec/controllers/edge_controller_spec.rb
@@ -5,14 +5,20 @@ require 'spec_helper'
 describe EdgeController, :type => :request do
   let(:account) { "rspec" }
   let(:host_id) {"#{account}:host:edge/edge"}
- 
+  let(:other_host_id) {"#{account}:host:data/other"}
+
   before do
     Slosilo["authn:#{account}"] ||= Slosilo::Key.new
     @current_user = Role.find_or_create(role_id: host_id)
+    @other_user = Role.find_or_create(role_id: other_host_id)
   end
-  
+
   let(:update_slosilo_keys_url) do
     "/edge/slosilo_keys/#{account}"
+  end
+
+  let(:get_hosts) do
+    "/edge/hosts/#{account}"
   end
 
   let(:token_auth_header) do
@@ -31,8 +37,8 @@ describe EdgeController, :type => :request do
       #get the Slosilo key the URL request
       get(update_slosilo_keys_url, env: token_auth_header)
       expect(response.code).to eq("200")
-     
-      #get the Slosilo key from DB 
+
+      #get the Slosilo key from DB
       key = Slosilo["authn:#{account}"]
       private_key = key.to_der.unpack("H*")[0]
       fingerprint = key.fingerprint
@@ -56,6 +62,25 @@ describe EdgeController, :type => :request do
       #get the Slosilo key the URL request
       get(update_slosilo_keys_url, env: token_auth_header)
       expect(response.code).to eq("403")
+    end
+  end
+
+  context "Host" do
+    it "Check HMAC" do
+      #add edge-hosts to edge/edge-hosts group
+      Role.create(role_id: "#{account}:group:edge/edge-hosts")
+      RoleMembership.create(role_id: "#{account}:group:edge/edge-hosts", member_id: host_id, admin_option: false, ownership:false)
+      get(get_hosts, env: token_auth_header)
+      expect(response.code).to eq("200")
+      expect(response).to be_ok
+      expect(response.body).to include("api_key".strip)
+      expect(response.body).to include("salt".strip)
+      @result = JSON.parse(response.body)
+      encoded_api_key = @result['hosts'][0]['api_key']
+      encoded_salt = @result['hosts'][0]['salt']
+      salt = Base64.strict_decode64(encoded_salt)
+      test_api_key =  Base64.strict_encode64(EdgeController.new.hmac_api_key(@other_user.credentials.api_key, salt))
+      expect(test_api_key).to eq(encoded_api_key)
     end
   end
 end
