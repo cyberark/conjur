21d88f30640e6f59b6bf0ae84df46b74badbecd2
Neil King <neil.king@cyberark.com>
2023-05-08 09:44:27 -0700

    Support running cucumber tests in parallel:
     - Spawn multiple processes to execute cucumber tests in parallel. Each process contains it's own indipendent conjur instance and postgresql database to avoid colisions.
     - Dynamically alter Ruby ENV variables based on the process executing the cucumber feature tests.
     - Dockerfile has been altered to support replicated services for parallel tests (this has not been altered to be dynamic and consists of static changes).
     - Alter cucumber.yml profiles to support parallel_tests ruby gem cmd usage
    
    The following tests have not been parallelized:
     - authenticators_k8s
     - rspec tests

--- cucumber/rotators/features/support/env.rb
+++ cucumber/rotators/features/support/env.rb
@@ -1,5 +1,16 @@
 # frozen_string_literal: true
 
+parallel_cuke_vars = {}
+parallel_cuke_vars['CONJUR_APPLIANCE_URL'] = "http://conjur#{ENV['TEST_ENV_NUMBER']}"
+parallel_cuke_vars['DATABASE_URL'] = "postgres://postgres@pg#{ENV['TEST_ENV_NUMBER']}/postgres"
+parallel_cuke_vars['CONJUR_AUTHN_API_KEY'] = ENV["CONJUR_AUTHN_API_KEY#{ENV['TEST_ENV_NUMBER']}"]
+
+parallel_cuke_vars.each do |key, value|
+  if ENV[key].nil? || ENV[key].empty?
+    ENV[key] = value
+  end
+end
+
 # so that we can require relative to the project root
 $LOAD_PATH.unshift(Dir.pwd)
 require 'config/environment'
@@ -7,5 +18,5 @@ require 'config/environment'
 ENV['RAILS_ENV'] ||= 'test'
 ENV['CONJUR_LOG_LEVEL'] ||= 'debug'
 
-Conjur.configuration.appliance_url = ENV['CONJUR_APPLIANCE_URL'] || 'http://conjur'
+Conjur.configuration.appliance_url = ENV['CONJUR_APPLIANCE_URL'] || "http://conjur#{ENV['TEST_ENV_NUMBER']}"
 Conjur.configuration.account = ENV['CONJUR_ACCOUNT'] || 'cucumber'
