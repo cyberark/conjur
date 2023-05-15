452aa60cbfa0da36161fab46155fec10326bfe43
Neil King <neil.king@cyberark.com>
2023-05-15 08:08:40 -0700

    working rspec_audit

--- engines/conjur_audit/spec/dummy/config/environments/test.rb
+++ engines/conjur_audit/spec/dummy/config/environments/test.rb
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
 
+ENV['CONJUR_APPLIANCE_URL'] = "http://conjur#{ENV['TEST_ENV_NUMBER']}"
+ENV['DATABASE_URL'] = "postgres://postgres@pg#{ENV['TEST_ENV_NUMBER']}/postgres"
+
+api_string = "CONJUR_AUTHN_API_KEY#{ENV['TEST_ENV_NUMBER']}"
+ENV['CONJUR_AUTHN_API_KEY'] = ENV[api_string]
+
 Rails.application.configure do
   config.cache_classes = true
   config.eager_load = false
