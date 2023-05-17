9e65a4bb25e734be01232ac33f6dc0585b8f1d3c
sofia.dimant <sofia.dimant@cyberark.com>
2023-05-17 12:59:02 +0300

    Change token factory to sign with host/user signing key

--- app/domain/token_factory.rb
+++ app/domain/token_factory.rb
@@ -12,18 +12,19 @@ class TokenFactory < Dry::Struct
   MAXIMUM_AUTHENTICATION_TOKEN_EXPIRATION = 5.hours
   MINIMUM_AUTHENTICATION_TOKEN_EXPIRATION = 0
 
-  def signing_key(account)
-    slosilo["authn:#{account}".to_sym] || raise(NoSigningKey, account)
+  def signing_key(username, account)
+    sloislo_key = host?(username) ? Account.token_key(account, "host") : Account.token_key(account, "user")
+    sloislo_key || raise(NoSigningKey, Account.token_id(account, host?(username) ? "host" : "user"))
   end
-
+  
   def signed_token(account:,
                    username:,
                    host_ttl: Rails.application.config.conjur_config.host_authorization_token_ttl,
                    user_ttl: Rails.application.config.conjur_config.user_authorization_token_ttl)
-    signing_key(account).issue_jwt(
+    signing_key(username, account).issue_jwt(
       sub: username,
       exp: Time.now + offset(
-        ttl: username.starts_with?('host/') ? host_ttl : user_ttl
+        ttl: host?(username) ? host_ttl : user_ttl
       )
     )
   end
@@ -41,5 +42,9 @@ class TokenFactory < Dry::Struct
     return ttl.to_i if ttl.to_i.to_s == ttl.to_s
     # Attempt to coerce a string into integer
     ttl.to_s.to_i
+  end
+  
+  def host?(username)
+    username.start_with?('host/')
   end
 end
