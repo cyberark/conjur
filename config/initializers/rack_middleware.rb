6116c5787105ab46d3d5f4578e206f6e6084ffc4
Jason Vanderhoof <jvanderhoof@gmail.com>
2023-06-15 15:35:05 -0600

    Update rack initializer with relevant information from original

--- config/initializers/rack_middleware.rb
+++ config/initializers/rack_middleware.rb
@@ -3,24 +3,6 @@ require 'rack/default_content_type'
 # This is where we introduce custom middleware that interacts with Rack
 # and Rails to change how requests are handled.
 Rails.application.configure do
-  # This configures which paths do and do not require token authentication.
-  # Token authentication is optional for authn routes, and it's not applied at
-  # all to authentication, host factories, or static assets (e.g. images, CSS)
-  config.middleware.use(Conjur::Rack::Authenticator,
-                        optional: [
-                          %r{^/authn-[^/]+/},
-                          %r{^/authn/},
-                          %r{^/public_keys/}
-                        ],
-                        except: [
-                          %r{^/authn-oidc/.*/providers},
-                          %r{^/authn-[^/]+/.*/authenticate$},
-                          %r{^/authn/.*/authenticate$},
-                          %r{^/host_factories/hosts$},
-                          %r{^/assets/.*},
-                          %r{^/authenticators$},
-                          %r{^/$}
-                        ])
 
   # We want to ensure requests have an expected content type
   # before other middleware runs to make sure any body parsing
--- config/initializers/token_authentication.rb
+++ config/initializers/token_authentication.rb
@@ -3,8 +3,9 @@
 require 'rack/token_authentication'
 
 Rails.application.configure do
-  # Defines the routes which do not require an auth token (`except`) and the
-  # routes which may utilize an auth token (`optional`).
+  # This configures which paths do and do not require token authentication.
+  # Token authentication is optional for authn routes, and it's not applied at
+  # all to authentication, host factories, or static assets (e.g. images, CSS)
   config.middleware.use(Rack::TokenAuthentication, {
     optional: [
       %r{^/authn-[^/]+/},
