4806f5d76604c183a35b86a26cb618c8b7c49afb
sofia.dimant <sofia.dimant@cyberark.com>
2023-06-21 15:53:23 +0300

    Add update slosilo key

---
+++ spec/rack/slosilo.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+describe "Slosilo key" do
+  before(:all) {
+    init_slosilo_keys("rspec")
+  }
+  context "Update existing key" do
+    it "possible to update existing key" do
+      new_key = Slosilo::Key.new
+      Slosilo["authn:rspec:host:current"] = new_key
+      current_key = Slosilo["authn:rspec:host:current"]
+      expect(current_key.to_der.unpack("H*")[0]).to eq(new_key.to_der.unpack("H*")[0])
+      expect(current_key.fingerprint).to eq(new_key.fingerprint)
+    end
+  end
+end
