35259f89259544e86739be9b3c51fd1b7826167f
egvili <eyal.gvili@cyberark.com>
2023-06-29 12:22:18 +0300

    List Edges

--- app/controllers/edge_controller.rb
+++ app/controllers/edge_controller.rb
@@ -122,7 +122,6 @@ class EdgeController < RestController
                     .slice(*allowed_params).to_h.symbolize_keys
     begin
       verify_edge_host(options)
-      record_edge_access(current_user.role_id)
       scope = Role.where(:role_id.like(options[:account] + ":host:data/%"))
       if params[:count] == 'true'
         sumItems = scope.count('*'.lit)
@@ -166,9 +165,12 @@ class EdgeController < RestController
   end
 
   def all_edges
-    #TODO: verify role is in Conjur_Admins?
-    render(json: Edge.all.map{|edge| {name: edge.name, ip: edge.ip, last_sync: edge.last_sync,
-                                      version:edge.version, installation_date: edge.installation_date}})
+    allowed_params = %i[account]
+    options = params.permit(*allowed_params).to_h.symbolize_keys
+    verify_conjur_admins(options)
+    render(json: Edge.all.map{|edge|
+      {name: edge.name, ip: edge.ip, last_sync: edge.last_sync,
+       version:edge.version, installation_date: edge.installation_date}})
   end
 
   private
@@ -232,21 +234,31 @@ class EdgeController < RestController
     end
   end
 
+  def verify_conjur_admins(options)
+    admins_group = Role[options[:account] + ':group:Conjur_Cloud_Admins']
+    unless admins_group&.ancestor_of?(current_user)
+      logger.error(
+        Errors::Authorization::EndpointNotVisibleToRole.new(
+          "Curren user is: #{current_user}. should be member of #{admins_group}"
+        )
+      )
+      raise Forbidden
+    end
+  end
+
   def numeric? val
     val == val.to_i.to_s
   end
 
-  def record_edge_access(host_name)
-    edge_record = Edge.get_by_hostname(host_name)
-    unless edge_record
-      logger.error(Errors::Conjur::RequestedResourceNotFound.new("Edge instance", host_name))
-      return
-    end
+  #TODO: call from future endpoint for data
+  def record_edge_access(host_name, version, sync_time)
+    edge_record = Edge.get_by_hostname(host_name) || raise(RecordNotFound, host_name)
     edge_record.ip = request.ip
-    edge_record.version = request.headers["Edge-Version"]
-    now = Time.now
-    edge_record.last_sync = now
-    edge_record.installation_date = now unless edge_record.installation_date
+    edge_record.version = version
+    if sync_time
+      edge_record.last_sync = sync_time
+      edge_record.installation_date = sync_time unless edge_record.installation_date
+    end
     edge_record.save
   end
 end
