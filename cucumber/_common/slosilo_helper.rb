8b1c286e5bec95da67930e27594deac606d4f262
sofia.dimant <sofia.dimant@cyberark.com>
2023-07-05 09:26:28 +0300

    modify slosilo endpoint to return current and prev keys

--- cucumber/_common/slosilo_helper.rb
+++ cucumber/_common/slosilo_helper.rb
@@ -1,10 +1,11 @@
 
-def token_id(account, role)
-  "authn:#{account}:#{role}:current"
+def token_id(account, role, tag = "current")
+  "authn:#{account}:#{role}:#{tag}"
 end
 
 def init_slosilo_keys
-  slosilo_ids = [token_id("rspec", "host"), token_id("rspec", "user"), token_id("cucumber", "host"), token_id("cucumber", "user")]
+  slosilo_ids = [token_id("rspec", "host"), token_id("rspec", "user"), token_id("cucumber", "host"), token_id("cucumber", "user"),
+                 token_id("cucumber", "user", "previous"),token_id("cucumber", "host", "previous")  ]
   Slosilo.each do |k, v|
     unless slosilo_ids.member?(k)
       Slosilo.send(:keystore).adapter.model[k].delete
