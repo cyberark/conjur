6d3b9feb5209ac74ed996dad25961464beebfe61
egvili <eyal.gvili@cyberark.com>
2023-07-11 12:06:34 +0300

    Extend data endpoint

--- app/models/edge.rb
+++ app/models/edge.rb
@@ -13,20 +13,8 @@ class Edge < Sequel::Model
       Edge.insert(**values)
     end
 
-    def record_edge_access(host_name, data, ip)
-      edge_record = Edge.get_by_hostname(host_name) || raise(Exceptions::RecordNotFound)
-      edge_record.ip = ip
-      edge_record.version = data['edge_version'] if data['edge_version']
-      sync_time = Time.at(data['edge_statistics']['last_synch_time']) # This field is required
-      edge_record.last_sync = sync_time
-      edge_record.installation_date = sync_time unless edge_record.installation_date
-      edge_record.platform = data['edge_container_type'] if data['edge_container_type']
-
-      edge_record.save
-    end
-
     def get_by_hostname(hostname)
-      Edge.where(id: hostname_to_id(hostname)).first
+      Edge.where(id: hostname_to_id(hostname)).first || raise(Exceptions::RecordNotFound)
     end
 
     def hostname_to_id(hostname)
@@ -37,5 +25,15 @@ class Edge < Sequel::Model
     def id_to_hostname(id)
       EDGE_HOST_PREFIX + id
     end
+  end
+
+  def record_edge_access(data, ip)
+    self.ip = ip
+    self.version = data['edge_version'] if data['edge_version']
+    sync_time = Time.at(data['edge_statistics']['last_synch_time']) # This field is required
+    self.last_sync = sync_time
+    self.platform = data['edge_container_type'] if data['edge_container_type']
+
+    self.save
   end
 end
