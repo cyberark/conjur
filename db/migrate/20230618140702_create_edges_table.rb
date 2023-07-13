9b5ebe973b8cd18522953922d76698e15e8ad05e
egvili <eyal.gvili@cyberark.com>
2023-07-13 17:14:26 +0300

    Revert single edge migration

--- db/migrate/20230618140702_create_edges_table.rb
+++ db/migrate/20230618140702_create_edges_table.rb
@@ -5,13 +5,6 @@ end
 
 Sequel.migration do
   change do
-    def migrate_single_edge
-      single_host_id = ::DB::Preview::SingleEdgeToMulti.new.find_single_host_id
-      if single_host_id
-        self[:edges].insert(name: "first_edge", id: single_host_id, version: "1.0.2")
-      end
-    end
-
     create_table :edges do
       String :id, primary_key: true
       String :name, unique: true, null: false
@@ -21,7 +14,5 @@ Sequel.migration do
       Timestamp :last_sync, null: true
       Timestamp :installation_date, null: true
     end
-
-    migrate_single_edge
   end
 end
