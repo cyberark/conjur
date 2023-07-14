546126bdabe2a4e420ab07914550e0c7e31bcaa7
cezary tarnowski <cezary.tarnowski@cyberark.com>
2023-07-14 12:27:17 +0200

    display fips mode status in the UI, requires temporary fix for openssl gem

--- config/initializers/fips.rb
+++ config/initializers/fips.rb
@@ -1,5 +1,6 @@
 require "openssl"
 require "digest"
+require "ffi"
 
 # Suppress warning messages
 original_verbose = $VERBOSE
@@ -12,9 +13,27 @@ Digest::SHA1 = OpenSSL::Digest::SHA1
 # Activate warning messages again
 $VERBOSE = original_verbose
 
-# by default FIPS mode is enabled
-# disable FIPS mode only if OPENSSL_FIPS_ENABLED environment variable is present and has false value
-# OpenSSL.fips_mode = !(ENV["OPENSSL_FIPS_ENABLED"].present? && ENV["OPENSSL_FIPS_ENABLED"] == 'false')
+# This is a temporary workaround to support OpenSSL v3 until ruby openssl gem properly handles fips mode state
+# https://github.com/ruby/openssl/issues/369
+if OpenSSL::OPENSSL_LIBRARY_VERSION.start_with?("OpenSSL 3")
+  module OpenSSL
+    extend FFI::Library
+      ffi_lib 'libssl.so'
+      attach_function :EVP_default_properties_is_fips_enabled, [:pointer], :int
+
+    def self.fips_mode
+      EVP_default_properties_is_fips_enabled(nil) == 1
+    end
+
+    def self.fips_mode=(mode)
+      raise "Changing FIPS state in OpenSSL 3 needs to be done with OpenSSL configuration"
+    end
+  end
+else
+  # by default FIPS mode is enabled
+  # disable FIPS mode only if OPENSSL_FIPS_ENABLED environment variable is present and has false value
+  OpenSSL.fips_mode = !(ENV.fetch('OPENSSL_FIPS_ENABLED', 'true') == 'false')
+end
 
 # each of the following 3rd party overridden is required since a non FIPS complaint encryption method is used
 # if a non-complaint FIPS method like MD5 is used or a direct use of Digest::encryption-method
--- config/initializers/status.rb
+++ config/initializers/status.rb
@@ -4,3 +4,4 @@ require 'logs'
 
 ENV["CONJUR_VERSION_DISPLAY"] = File.read(File.expand_path("../../VERSION", File.dirname(__FILE__)))
 ENV["API_VERSION"] = File.read(File.expand_path("../../API_VERSION", File.dirname(__FILE__)))
+ENV["FIPS_MODE_STATUS"] = OpenSSL.fips_mode ? "enabled" : "disabled"
