d1758a7b2d463f15ada25c6346be648d0a89293f
egvili <eyal.gvili@cyberark.com>
2023-07-18 14:37:52 +0300

    Add strict validation for report data params

--- app/models/edge_logic/data_handlers/ongoing_handler.rb
+++ app/models/edge_logic/data_handlers/ongoing_handler.rb
@@ -3,6 +3,8 @@
 module EdgeLogic
   module DataHandlers
     class OngoingHandler
+
+      include ParamsValidator
       def initialize(logger)
         @logger = logger
       end
@@ -12,6 +14,7 @@ module EdgeLogic
         allowed_params = [:account, :edge_version, :edge_container_type, edge_statistics: [:last_synch_time, cycle_requests:
           [:get_secret, :apikey_authenticate, :jwt_authenticate, :redirect]]]
         options = params.permit(*allowed_params).to_h
+        validate_params(options, ->(k, v) {v.is_a?(Numeric) || (v.is_a?(String) && v.length <= 20)})
 
         edge = Edge.get_by_hostname(hostname)
         edge.record_edge_access(options, ip)
