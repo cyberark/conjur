1d886067551324d5c0ba6904f9761c3283bec541
Rafi Schwarz <rafi.schwarz@cyberark.com>
2023-10-16 18:32:41 +0300

    Added tests and improved the logic

--- app/controllers/locks_controller.rb
+++ app/controllers/locks_controller.rb
@@ -8,15 +8,23 @@ class LocksController < RestController
   before_action :current_user
 
   LOCK_NOT_FOUND = "Lock not found"
+  ID_FIELD_ALLOWED_CHARACTERS = /\A[a-zA-Z0-9+\-_]+\z/
 
   def create
-    logger.info("POST /locks/:account started")
+    logger.debug("POST /locks/:account started")
     authorize(:create)
+
+    unless params[:ttl].is_a? Integer and params[:ttl] > 0
+      raise ApplicationController::BadRequest, "invalid 'ttl' parameter. Must be a positive number"
+    end
+
+    unless params[:id].match?(ID_FIELD_ALLOWED_CHARACTERS)
+      raise ApplicationController::BadRequest, "invalid 'id' parameter. Only the following characters are supported: A-Z, a-z, 0-9, +, -, and _"
+    end
 
     delete_lock_if_expired(params[:account], params[:id])
 
-    current_timestamp = Time.now
-    expires_at = current_timestamp + params[:ttl]
+    expires_at = Lock.db.get(Sequel.lit("CURRENT_TIMESTAMP + interval ?", "#{params[:ttl]} second"))
     lock = Lock.new(lock_id: params[:id], account: params[:account],
                     owner: params[:owner],
                     modified_at: Sequel::CURRENT_TIMESTAMP,
@@ -24,18 +32,20 @@ class LocksController < RestController
     lock.save
 
     render(json: lock.as_json, status: :created)
-    logger.info("POST /locks/:account ended successfully")
+    logger.debug("POST /locks/:account ended successfully")
   rescue Sequel::UniqueConstraintViolation => e
     logger.error("The lock [#{params[:id]}] already exists")
+    logger.debug("POST /locks/:account ended successfully")
     raise Exceptions::RecordExists.new("lock", params[:id])
   end
 
   def get
-    logger.info("GET /locks/:account/*identifier started")
+    logger.debug("GET /locks/:account/*identifier started")
     authorize(:read)
 
+    delete_lock_if_expired(params[:account], params[:identifier])
     lock = get_lock_from_db(params[:account], params[:identifier])
-    if lock and delete_lock_if_expired(params[:account], params[:identifier]) == 0
+    if lock
       render(json: lock.as_json, status: :ok)
     else
       render(json: {
@@ -52,18 +62,18 @@ class LocksController < RestController
       }, status: :not_found)
     end
 
-    logger.info("GET /locks/:account/*identifier ended successfully")
+    logger.debug("GET /locks/:account/*identifier ended successfully")
   end
 
   def update
-    logger.info("PATCH /locks/:account/*identifier started")
+    logger.debug("PATCH /locks/:account/*identifier started")
     authorize(:update)
 
+    delete_lock_if_expired(params[:account], params[:identifier])
     lock = get_lock_from_db(params[:account], params[:identifier])
-    if lock and delete_lock_if_expired(params[:account], params[:identifier]) == 0
-      current_timestamp = Time.now
-      expires_at = current_timestamp + params[:ttl]
-      Lock.where(account: account, lock_id: params[:identifier]).update(modified_at: current_timestamp, expires_at: expires_at)
+    if lock
+      expires_at = Lock.db.get(Sequel.lit("CURRENT_TIMESTAMP + interval ?", "#{params[:ttl]} second"))
+      Lock.where(account: account, lock_id: params[:identifier]).update(modified_at: Sequel::CURRENT_TIMESTAMP, expires_at: expires_at)
       render(json: lock.as_json, status: :ok)
     else
       render(json: {
@@ -79,15 +89,16 @@ class LocksController < RestController
         }
       }, status: :not_found)
     end
-    logger.info("PATCH /locks/:account/*identifier ended successfully")
+    logger.debug("PATCH /locks/:account/*identifier ended successfully")
   end
 
   def delete
-    logger.info("DELETE /locks/:account/*identifier started")
+    logger.debug("DELETE /locks/:account/*identifier started")
     authorize(:delete)
 
+    delete_lock_if_expired(params[:account], params[:identifier])
     lock = get_lock_from_db(params[:account], params[:identifier])
-    if lock and delete_lock_if_expired(params[:account], params[:identifier]) == 0
+    if lock
       lock.delete
       head :ok
     else
@@ -104,7 +115,7 @@ class LocksController < RestController
         }
       }, status: :not_found)
     end
-    logger.info("DELETE /locks/:account/*identifier ended successfully")
+    logger.debug("DELETE /locks/:account/*identifier ended successfully")
   end
 
   private
---
+++ spec/controllers/locks_controller_spec.rb
@@ -0,0 +1,362 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+DatabaseCleaner.strategy = :truncation
+
+describe LocksController, type: :request do
+  before do
+    Slosilo["authn:rspec"] ||= Slosilo::Key.new
+    # Load the base conjur/locks policies into Conjur
+
+    put(
+      '/policies/rspec/policy/root',
+      env: token_auth_header(role: admin_user).merge(
+        'RAW_POST_DATA' => data_locks_policy
+      )
+    )
+    assert_response :success
+
+  end
+
+  let(:data_locks_policy) do
+    <<~POLICY
+      - !policy
+        id: conjur/locks
+        body:
+        - !webservice my-lock
+        - !webservice my-id$#@
+        - !webservice non-existing-lock
+    POLICY
+  end
+
+  let(:admin_user) { Role.find_or_create(role_id: 'rspec:user:admin') }
+  let(:current_user) { Role.find_or_create(role_id: current_user_id) }
+  let(:current_user_id) { 'rspec:user:admin' }
+  let(:alice_user) { Role.find_or_create(role_id: alice_user_id) }
+  let(:alice_user_id) { 'rspec:user:alice' }
+
+  describe "#create" do
+    context "when a user creates a lock" do
+      let(:payload_create_lock) do
+        <<~BODY
+          {
+            "id": "my-lock",
+            "owner": "my-lock-owner",
+            "ttl": 5
+          }
+        BODY
+      end
+      it 'returns created' do
+        post("/locks/rspec",
+             env: token_auth_header(role: admin_user).merge(
+               'RAW_POST_DATA' => payload_create_lock,
+               'CONTENT_TYPE' => "application/json"
+             ))
+        assert_response :created
+      end
+    end
+
+    context "when a user creates a lock with invalid id" do
+      let(:payload_create_lock) do
+        <<~BODY
+          {
+            "id": "my-id$#@",
+            "owner": "my-lock-owner",
+            "ttl": 5
+          }
+        BODY
+      end
+      it 'returns bad request' do
+        post("/locks/rspec",
+             env: token_auth_header(role: admin_user).merge(
+               'RAW_POST_DATA' => payload_create_lock,
+               'CONTENT_TYPE' => "application/json"
+             ))
+        assert_response :bad_request
+      end
+    end
+
+    context "when a user creates a lock with negative ttl" do
+      let(:payload_create_lock) do
+        <<~BODY
+          {
+            "id": "my-lock",
+            "owner": "my-lock-owner",
+            "ttl": -1
+          }
+        BODY
+      end
+      it 'returns bad request' do
+        post("/locks/rspec",
+             env: token_auth_header(role: admin_user).merge(
+               'RAW_POST_DATA' => payload_create_lock,
+               'CONTENT_TYPE' => "application/json"
+             ))
+        assert_response :bad_request
+      end
+    end
+
+    context "when a user creates a lock with string ttl" do
+      let(:payload_create_lock) do
+        <<~BODY
+          {
+            "id": "my-lock",
+            "owner": "my-lock-owner",
+            "ttl": "10"
+          }
+        BODY
+      end
+      it 'returns bad request' do
+        post("/locks/rspec",
+             env: token_auth_header(role: admin_user).merge(
+               'RAW_POST_DATA' => payload_create_lock,
+               'CONTENT_TYPE' => "application/json"
+             ))
+        assert_response :bad_request
+      end
+    end
+
+    context "when a user creates a lock with float ttl" do
+      let(:payload_create_lock) do
+        <<~BODY
+          {
+            "id": "my-lock",
+            "owner": "my-lock-owner",
+            "ttl": 1.5
+          }
+        BODY
+      end
+      it 'returns bad request' do
+        post("/locks/rspec",
+             env: token_auth_header(role: admin_user).merge(
+               'RAW_POST_DATA' => payload_create_lock,
+               'CONTENT_TYPE' => "application/json"
+             ))
+        assert_response :bad_request
+      end
+    end
+
+    context "when a user creates a lock without a webservice" do
+      let(:payload_create_lock) do
+        <<~BODY
+          {
+            "id": "non-existing-webservice-lock",
+            "owner": "my-lock-owner",
+            "ttl": 10
+          }
+        BODY
+      end
+      it 'returns not found' do
+        post("/locks/rspec",
+             env: token_auth_header(role: admin_user).merge(
+               'RAW_POST_DATA' => payload_create_lock,
+               'CONTENT_TYPE' => "application/json"
+             ))
+        assert_response :not_found
+      end
+    end
+
+    context "when a user tries to create a lock that already exists" do
+      let(:payload_create_lock) do
+        <<~BODY
+          {
+            "id": "my-lock",
+            "owner": "my-lock-owner",
+            "ttl": 60
+          }
+        BODY
+      end
+      it 'returns conflict' do
+        post("/locks/rspec",
+             env: token_auth_header(role: admin_user).merge(
+               'RAW_POST_DATA' => payload_create_lock,
+               'CONTENT_TYPE' => "application/json"
+             ))
+        assert_response :created
+        post("/locks/rspec",
+             env: token_auth_header(role: admin_user).merge(
+               'RAW_POST_DATA' => payload_create_lock,
+               'CONTENT_TYPE' => "application/json"
+             ))
+        assert_response :conflict
+      end
+    end
+  end
+
+  describe "#delete" do
+    context "when a user deletes a lock that does not exist" do
+      it 'it returns not found' do
+        delete("/locks/rspec/non-existing-lock",
+               env: token_auth_header(role: admin_user))
+        assert_response :not_found
+        expect(response.body).to eq("{\"error\":{\"code\":\"not_found\",\"message\":\"Lock not found\",\"target\":\"lock\",\"details\":{\"code\":\"not_found\",\"target\":\"id\",\"message\":\"non-existing-lock\"}}}")
+      end
+    end
+
+    context "when a user deletes a lock that does not have a webservice exist" do
+      it 'it returns not found' do
+        delete("/locks/rspec/non-existing-lock",
+               env: token_auth_header(role: admin_user))
+        assert_response :not_found
+        expect(response.body).to eq("{\"error\":{\"code\":\"not_found\",\"message\":\"Lock not found\",\"target\":\"lock\",\"details\":{\"code\":\"not_found\",\"target\":\"id\",\"message\":\"non-existing-lock\"}}}")
+      end
+    end
+
+    context "when a user deletes an existing lock" do
+      let(:payload_create_lock) do
+        <<~BODY
+          {
+            "id": "my-lock",
+            "owner": "my-lock-owner",
+            "ttl": 10
+          }
+        BODY
+      end
+      it 'it is deleted successfully' do
+        post("/locks/rspec",
+             env: token_auth_header(role: admin_user).merge(
+               'RAW_POST_DATA' => payload_create_lock,
+               'CONTENT_TYPE' => "application/json"
+             ))
+        assert_response :created
+
+        delete("/locks/rspec/my-lock", env: token_auth_header(role: admin_user))
+        assert_response :success
+      end
+
+      context "when a user deletes a non existing lock without permissions" do
+        it 'it returns not found' do
+          delete("/locks/rspec/non-existing-lock", env: token_auth_header(role: alice_user))
+          assert_response :not_found
+          expect(response.body).to eq("{\"error\":{\"code\":\"not_found\",\"message\":\"Webservice 'conjur/locks/non-existing-lock' not found in account 'rspec'\",\"target\":\"webservice\",\"details\":{\"code\":\"not_found\",\"target\":\"id\",\"message\":\"rspec:webservice:conjur/locks/non-existing-lock\"}}}")
+        end
+      end
+    end
+
+    context "when a user tries to delete a lock that expired" do
+      let(:payload_create_lock) do
+        <<~BODY
+          {
+            "id": "my-lock",
+            "owner": "my-lock-owner",
+            "ttl": 1
+          }
+        BODY
+      end
+      it 'returns not found' do
+        post("/locks/rspec",
+             env: token_auth_header(role: admin_user).merge(
+               'RAW_POST_DATA' => payload_create_lock,
+               'CONTENT_TYPE' => "application/json"
+             ))
+        assert_response :created
+        sleep(2)
+        delete("/locks/rspec/my-lock",
+            env: token_auth_header(role: admin_user))
+        assert_response :not_found
+        expect(response.body).to eq("{\"error\":{\"code\":\"not_found\",\"message\":\"Lock not found\",\"target\":\"lock\",\"details\":{\"code\":\"not_found\",\"target\":\"id\",\"message\":\"my-lock\"}}}")
+      end
+    end
+  end
+
+  describe "#get" do
+    context "when a user gets a lock that exists" do
+      let(:payload_create_lock) do
+        <<~BODY
+          {
+            "id": "my-lock",
+            "owner": "my-lock-owner",
+            "ttl": 10
+          }
+        BODY
+      end
+      it 'the lock is returned' do
+
+        post("/locks/rspec",
+             env: token_auth_header(role: admin_user).merge(
+               'RAW_POST_DATA' => payload_create_lock,
+               'CONTENT_TYPE' => "application/json"
+             ))
+        assert_response :created
+
+        get("/locks/rspec/my-lock",
+            env: token_auth_header(role: admin_user))
+        assert_response :success
+        parsed_body = JSON.parse(response.body)
+        expect(parsed_body["id"]).to eq("my-lock")
+        expect(parsed_body["owner"]).to eq("my-lock-owner")
+        expect(response.body).to include("\"created_at\"")
+        expect(response.body).to include("\"modified_at\"")
+        expect(response.body).to include("\"expires_at\"")
+      end
+    end
+
+    context "when a user gets a lock that does not exist" do
+      it 'the response is not found' do
+        get("/locks/rspec/non-existing-lock",
+            env: token_auth_header(role: admin_user))
+        assert_response :not_found
+        expect(response.body).to eq("{\"error\":{\"code\":\"not_found\",\"message\":\"Lock not found\",\"target\":\"lock\",\"details\":{\"code\":\"not_found\",\"target\":\"id\",\"message\":\"non-existing-lock\"}}}")
+      end
+    end
+
+    context "when a user gets a lock that its webservice does not exist" do
+      it 'the response is not found' do
+        get("/locks/rspec/non-existing-webservice-lock",
+            env: token_auth_header(role: admin_user))
+        assert_response :not_found
+        expect(response.body).to eq("{\"error\":{\"code\":\"not_found\",\"message\":\"Webservice 'conjur/locks/non-existing-webservice-lock' not found in account 'rspec'\",\"target\":\"webservice\",\"details\":{\"code\":\"not_found\",\"target\":\"id\",\"message\":\"rspec:webservice:conjur/locks/non-existing-webservice-lock\"}}}")
+      end
+    end
+
+    context "when a user that does not have permissions on lock webservice, gets a lock" do
+      let(:payload_create_lock) do
+        <<~BODY
+          {
+            "id": "my-lock",
+            "owner": "my-lock-owner",
+            "ttl": 10
+          }
+        BODY
+      end
+      it 'the response is not found' do
+        post("/locks/rspec",
+             env: token_auth_header(role: admin_user).merge(
+               'RAW_POST_DATA' => payload_create_lock,
+               'CONTENT_TYPE' => "application/json"
+             ))
+        assert_response :created
+
+        get("/locks/rspec/my-lock",
+            env: token_auth_header(role: alice_user))
+        assert_response :not_found
+        expect(response.body).to eq("{\"error\":{\"code\":\"not_found\",\"message\":\"Webservice 'conjur/locks/my-lock' not found in account 'rspec'\",\"target\":\"webservice\",\"details\":{\"code\":\"not_found\",\"target\":\"id\",\"message\":\"rspec:webservice:conjur/locks/my-lock\"}}}")
+      end
+    end
+
+    context "when a user tries to get a lock that expired" do
+      let(:payload_create_lock) do
+        <<~BODY
+          {
+            "id": "my-lock",
+            "owner": "my-lock-owner",
+            "ttl": 1
+          }
+        BODY
+      end
+      it 'returns not found' do
+        post("/locks/rspec",
+             env: token_auth_header(role: admin_user).merge(
+               'RAW_POST_DATA' => payload_create_lock,
+               'CONTENT_TYPE' => "application/json"
+             ))
+        assert_response :created
+        sleep(2)
+        get("/locks/rspec/my-lock",
+            env: token_auth_header(role: admin_user))
+        assert_response :not_found
+        expect(response.body).to eq("{\"error\":{\"code\":\"not_found\",\"message\":\"Lock not found\",\"target\":\"lock\",\"details\":{\"code\":\"not_found\",\"target\":\"id\",\"message\":\"my-lock\"}}}")
+      end
+    end
+  end
+end
