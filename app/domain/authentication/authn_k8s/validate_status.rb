8fd15682e5154e4dc5318581cc1904ffeb02a0e9
Micah Lee <micah.lee@cyberark.com>
2023-11-14 16:37:47 -0500

    Validate syntax of k8s authenticator configuration variables

--- app/domain/authentication/authn_k8s/validate_status.rb
+++ app/domain/authentication/authn_k8s/validate_status.rb
@@ -3,14 +3,113 @@ module Authentication
     # AuthnK8s::ValidateStatus raises an exception if the Kubernetes
     # authenticator is not configured properly or with inadequate permissions.
     ValidateStatus = CommandClass.new(
-      dependencies: {},
+      dependencies: {
+        fetch_authenticator_secrets: Authentication::Util::FetchAuthenticatorSecrets.new
+      },
       inputs: %i[account service_id]
     ) do
       def call
-        # Kubernetes specific validations will be added here in follow up
-        # stories. Creating a blank ValidateStatus allows the status endpoint
-        # to function for Kubernetes authenticators and provide some shared
-        # capabilities (e.g. is the authenticator enabled).
+        validate_configuration_values
+      end
+
+      protected
+
+      def validate_configuration_values
+        # Validate configuration values for k8s API access
+        validate_k8s_service_account_token
+        validate_k8s_ca_certificate
+        validate_k8s_api_url
+
+        # Validate configuration values for issuing authentication certificates
+        validate_conjur_ca_certificate
+        validate_conjur_ca_private_key
+      end
+
+      def validate_k8s_service_account_token
+        k8s_service_account_token
+      rescue JWT::DecodeError => e
+        raise Errors::Authentication::AuthnK8s::InvalidServiceAccountToken,
+              "Unable to decode JWT: #{e.message}"
+      end
+
+      def validate_k8s_ca_certificate
+        k8s_ca_certificate
+      rescue OpenSSL::X509::CertificateError => e
+        raise Errors::Authentication::AuthnK8s::InvalidApiCert,
+              "Unable to read certificate: #{e.message}"
+      end
+
+      def validate_k8s_api_url
+        # URI#parse will gracefully handle an empty string, so we check for that
+        # specifically here.
+        return unless k8s_api_url.to_s == ''
+
+        raise Errors::Authentication::AuthnK8s::InvalidApiUrl, k8s_api_url
+      rescue URI::InvalidURIError
+        raise Errors::Authentication::AuthnK8s::InvalidApiUrl, \
+              authenticator_secrets['kubernetes/api-url']
+      end
+
+      def validate_conjur_ca_certificate
+        conjur_ca_certificate
+      rescue OpenSSL::X509::CertificateError => e
+        raise Errors::Authentication::AuthnK8s::InvalidSigningCert,
+              "Unable to read certificate: #{e.message}"
+      end
+
+      def validate_conjur_ca_private_key
+        conjur_ca_private_key
+      rescue OpenSSL::PKey::RSAError
+        # We don't bubble up the internal error message to avoid leaking any
+        # accidental information about the private key.
+        raise Errors::Authentication::AuthnK8s::InvalidSigningKey,
+              "Unable to read private key"
+      end
+
+      def authenticator_secrets
+        @authenticator_secrets ||= @fetch_authenticator_secrets.call(
+          service_id: @service_id,
+          conjur_account: @account,
+          authenticator_name: AUTHENTICATOR_NAME,
+          required_variable_names: REQUIRED_VARIABLE_NAMES
+        )
+      end
+
+      def k8s_service_account_token
+        @k8s_service_account_token ||= \
+          JWT.decode(
+            authenticator_secrets['kubernetes/service-account-token'],
+            nil,
+            false
+          )
+      end
+
+      def k8s_ca_certificate
+        @k8s_ca_certificate ||= \
+          OpenSSL::X509::Certificate.new(
+            authenticator_secrets['kubernetes/ca-cert']
+          )
+      end
+
+      def k8s_api_url
+        @k8s_api_url ||= \
+          URI.parse(
+            authenticator_secrets['kubernetes/api-url']
+          )
+      end
+
+      def conjur_ca_certificate
+        @conjur_ca_certificate ||= \
+          OpenSSL::X509::Certificate.new(
+            authenticator_secrets['ca/cert']
+          )
+      end
+
+      def conjur_ca_private_key
+        @conjur_ca_private_key ||= \
+          OpenSSL::PKey::RSA.new(
+            authenticator_secrets['ca/key']
+          )
       end
     end
   end
