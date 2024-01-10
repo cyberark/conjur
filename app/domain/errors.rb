40eee7aa8c867ce13d846ded4fba79f5a8884e02
ltsirulnik <luba.tsirulnik@cyberark.com>
2024-01-10 10:46:06 +0200

    ONYX-49995 : update errors

--- app/domain/errors.rb
+++ app/domain/errors.rb
@@ -27,12 +27,12 @@ module Errors
     )
 
     ParameterValueInvalid = ::Util::TrackableErrorClass.new(
-      msg: "{0} parameter value is not valid",
+      msg: "The value in the '{0}' parameter is not valid. Error: {1}",
       code: "CONJ00191E"
     )
 
     ParameterTypeInvalid = ::Util::TrackableErrorClass.new(
-      msg: "The '{0}' parameter must be a {1}",
+      msg: "The '{0}' parameter must be of 'type={1}'",
       code: "CONJ00192E"
     )
 
