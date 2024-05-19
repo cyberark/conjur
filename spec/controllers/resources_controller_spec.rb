9dc622966d39fe79306b1b127353123fa5761a40
ltsirulnik <luba.tsirulnik@cyberark.com>
2024-05-19 08:57:37 +0300

    ONYX-56923: add tests to resources controller

--- spec/controllers/resources_controller_spec.rb
+++ spec/controllers/resources_controller_spec.rb
@@ -11,6 +11,46 @@ describe ResourcesController, type: :request do
   end
 
   let(:current_user) { Role.find_or_create(role_id: 'rspec:user:admin') }
+  let(:alice_user) { Role.find_or_create(role_id: 'rspec:user:alice') }
+
+  describe "resource permissions" do
+    let(:test_policy) do
+      <<~POLICY
+        - !user alice
+        - !variable perm_secret
+        - !permit
+          resource: !variable perm_secret
+          privilege: [ update ]
+          role: !user alice
+      POLICY
+    end
+    before do
+      # Load the test policy into Conjur
+      put(
+        '/policies/rspec/policy/root',
+        env: token_auth_header(role: current_user).merge(
+          { 'RAW_POST_DATA' => test_policy }
+        )
+      )
+      assert_response :success
+    end
+    context 'when validating check permission request' do
+      it "should return 204 when permission exists" do
+        get(
+          "/resources/rspec/variable/perm_secret?check=true&role=rspec:user:alice&privilege=update",
+          env: token_auth_header(role: current_user)
+        )
+        expect(response.code).to eq("204")
+      end
+      it "should return 404 when permission not exists" do
+        get(
+          "/resources/rspec/variable/perm_secret?check=true&role=rspec:user:alice&privilege=execute",
+          env: token_auth_header(role: current_user)
+        )
+        expect(response.code).to eq("404")
+      end
+    end
+  end
 
   describe '#post' do
 
@@ -27,12 +67,13 @@ describe ResourcesController, type: :request do
       '/policies/rspec/policy/root'
     end
 
-    def list_resources(limit: nil, offset: nil, count: false, short: false)
+    def list_resources(limit: nil, offset: nil, count: false, short: false, kind: false)
       params = {}
       params.merge!({ :limit => limit }) if limit
       params.merge!({ :offset => offset }) if offset
       params.merge!({ :count => count }) if count
       params.merge!({ :short => short }) if short
+      params.merge!({ :kind => kind }) if kind
       get(
         resources_url,
         env: token_auth_header(role: current_user),
@@ -46,6 +87,19 @@ describe ResourcesController, type: :request do
 
     def load_variables()
       payload = '[!variable a, !variable b, !variable c, !variable d, !host a, !host b, !host c, !host d, !layer a, !layer b, !layer c]'
+      put(
+        policies_url,
+        env: token_auth_header(role: current_user).merge({ 'RAW_POST_DATA' => payload })
+      )
+    end
+
+    def load_many_variables(num_of_variables)
+      payload = "[!host a, ".dup
+      (1..num_of_variables).each { |i|
+        payload += "!variable a#{i}, "
+      }
+      payload[-1] = "]"
+
       put(
         policies_url,
         env: token_auth_header(role: current_user).merge({ 'RAW_POST_DATA' => payload })
@@ -87,7 +141,6 @@ describe ResourcesController, type: :request do
           end
         end
       end
-
       context 'with limit query param defined' do
         before(:each) do
           list_resources(limit: 5)
@@ -109,7 +162,6 @@ describe ResourcesController, type: :request do
           end
         end
       end
-
       context 'with offset query param defined' do
         before(:each) do
           list_resources(offset: 1)
@@ -136,6 +188,21 @@ describe ResourcesController, type: :request do
             next if idx == 0
             expect(resource["id"]).to be > @resources[idx-1]["id"]
           end
+        end
+      end
+      context 'with max limit query param defined' do
+        before(:each) do
+          load_many_variables(1001)
+          list_resources(limit: 1000, kind: "variable")
+          @resources = JSON.parse(response.body)
+        end
+
+        it 'should return a 200 status code' do
+          expect(response.code).to eq("200")
+        end
+
+        it 'should list resources according to the provided limit' do
+          expect(@resources.size).to eq(1000)
         end
       end
     end
