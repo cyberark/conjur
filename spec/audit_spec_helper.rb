bcbdcb81924182a1197038ebea3abf48b52c6a44
Hubert Dąbrowski <hubert.dabrowski@cyberark.com>
2025-09-16 14:38:36 +0200

    CONJSE-2047: Remove policy factory code

--- spec/audit_spec_helper.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-def expect_audit(message:, result:, operation:)
-  expect(log_output.string).to include("result=\"#{result}\"")
-  expect(log_output.string).to include("operation=\"#{operation}\"")
-  expect(log_output.string).to include(message)
-end
